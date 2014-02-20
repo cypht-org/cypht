eaf05718a05310ffd496aed923e21b7b4ca470e1
jasonmunro <jason@hastymail.org>
2014-02-20 12:23:00 -0600

    add an ajax data handler and reformat the page a bit for readability

--- module/module_map.php
+++ module/module_map.php
@@ -1,19 +1,20 @@
 <?php
 
-/* Homepage modules */
+/* Homepage data handler modules */
 Hm_Handler_Modules::add('home', 'language',  true);
 Hm_Handler_Modules::add('home', 'title', true);
 Hm_Handler_Modules::add('home', 'date', true);
 Hm_Handler_Modules::add('home', 'logout', true);
 Hm_Handler_Modules::add('home', 'imap_delete', true);
 Hm_Handler_Modules::add('home', 'imap_setup', true);
-Hm_Handler_Modules::add('home', 'login', false);
 Hm_Handler_Modules::add('home', 'imap_setup_display', true);
 Hm_Handler_Modules::add('home', 'imap_connect', true);
 Hm_Handler_Modules::add('home', 'http_headers', true);
 
+/* Homepage output modules */
 Hm_Output_Modules::add('home', 'header', false);
 Hm_Output_Modules::add('home', 'css', false);
+Hm_Output_Modules::add('home', 'jquery', true);
 Hm_Output_Modules::add('home', 'logout', true);
 Hm_Output_Modules::add('home', 'login', false);
 Hm_Output_Modules::add('home', 'title', true);
@@ -22,10 +23,14 @@ Hm_Output_Modules::add('home', 'date', true);
 Hm_Output_Modules::add('home', 'imap_setup', true);
 Hm_Output_Modules::add('home', 'imap_setup_display', true);
 Hm_Output_Modules::add('home', 'imap_debug', true);
-Hm_Output_Modules::add('home', 'jquery', true);
+Hm_Output_Modules::add('home', 'js', true);
 Hm_Output_Modules::add('home', 'footer', true);
+
+/* ajax callback */
+Hm_Handler_Modules::add('ajax_imap_debug', 'imap_connect', true);
 
 /* Not found page modules */
 Hm_Handler_Modules::add('notfound', 'title', true);
 Hm_Output_Modules::add('notfound', 'title', true);
+
 ?>
