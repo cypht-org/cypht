4fab4ec85bb056b9b818ceaf41db32a10f78416d
jasonmunro <jason@hastymail.org>
2014-02-20 12:17:44 -0600

    adding some default js actions

---
+++ js/site.js
@@ -0,0 +1,35 @@
+Hm_Ajax = {
+
+    callback: false,
+
+    request: function(args, callback) {
+        console.log(args);
+        Hm_Ajax.callback = callback;
+        $.post('', args )
+        .done(Hm_Ajax.done)
+        .fail(Hm_Ajax.fail)
+        .always(Hm_Ajax.always);
+        return false;
+    },
+
+    done: function(res) {
+        res = jQuery.parseJSON(res);
+        if (Hm_Ajax.callback) {
+            Hm_Ajax.callback(res);
+        }
+    },
+
+    fail: function(res) {
+        Hm_Notices.show({0: 'An error occured communicating with the server'});
+    },
+
+    always: function(res) {
+    }
+}
+Hm_Notices = {
+
+    show: function(msgs) {
+        var msg_list = $.map(msgs, function(v) { return v; });
+        $('.sys_messages').html(msg_list.join('<br />'));
+    }
+}
