6e070acac9c1a400c3347cf3790d8ae65fa848f5
jasonmunro <jason@hastymail.org>
2014-02-24 15:41:53 -0600

    add data prep support for ajax callback data in two output modules

--- module/output_modules.php
+++ module/output_modules.php
@@ -118,7 +118,7 @@ class Hm_Output_imap_setup_display extends Hm_Output_Module {
                     ' <input type="submit" value="Delete" id="imap_delete'.$index.'" />'.
                     ' <input type="hidden" value="ajax_imap_debug" name="hm_ajax_hook" /></form><script type="text/javascript">'.
                     '$("#imap_delete'.$index.'").on("click", function() {'.
-                        '$(".imap_debug").empty(); '.
+                        '$(".imap_debug_data").empty(); '.
                         'event.preventDefault(); Hm_Ajax.request( $( this ).parent().serializeArray(), function(res) {'.
                         'Hm_Notices.show(res.router_user_msgs); if (res.deleted_server_id > -1 ) {$("#imap_server'.$index.'").remove();}},'.
                         '{"imap_delete": 1});});'.
@@ -142,15 +142,20 @@ class Hm_Output_imap_setup_display extends Hm_Output_Module {
 if (!class_exists('Hm_Output_imap_debug')) {
 class Hm_Output_imap_debug extends Hm_Output_Module {
     protected function output($input, $format) {
-        $res = '';
         if ($format == 'HTML5') {
             $res = '<div class="imap_debug"><div class="subtitle">IMAP Debug</div><pre class="imap_debug_data">';
             if (isset($input['imap_debug'])) {
-                $res .= $this->html_safe(print_r($input['imap_debug'], true));
+                $res .= $this->html_safe($input['imap_debug']);
             }
             $res .= '</pre></div>';
+            return $res;
         }
-        return $res;
+        elseif ($format == 'JSON') {
+            if (isset($input['imap_debug'])) {
+                $input['imap_debug'] = $this->html_safe($input['imap_debug']);
+            }
+            return $input;
+        }
     }
 }}
 
@@ -170,18 +175,23 @@ class Hm_Output_imap_setup extends Hm_Output_Module {
 
 if (!class_exists('Hm_Output_imap_folders')) {
 class Hm_Output_imap_folders extends Hm_Output_Module {
-    protected function output($input, $format) {
-        $res = '';
+    protected function output($input, $format, $lang_str=false) {
         if ($format == 'HTML5') {
-            $res .= '<div class="imap_folders"><div class="subtitle">Top Level Folders</div><div class="imap_folder_data">';
+            $res = '<div class="imap_folders"><div class="subtitle">Top Level Folders</div><div class="imap_folder_data">';
             if (isset($input['imap_folders'])) {
                 foreach (array_keys($input['imap_folders']) as $folder) {
                     $res .= '<div class="folder">'.$this->html_safe($folder).'</div>';
                 }
             }
             $res .= '</div></div>';
+            return $res;
         }
-        return $res;
+        elseif ($format == 'JSON') {
+            if (isset($input['imap_folders'])) {
+                $input['imap_folders'] = array_map(function($v) { return $this->html_safe($v); }, array_keys($input['imap_folders']));
+            }
+            return $input;
+        }
     }
 }}
 
