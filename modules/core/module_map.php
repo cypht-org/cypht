f1f4a8600154d304747e2bd57bea7cea85ffc0f8
jasonmunro <jason@hastymail.org>
2014-03-07 22:49:21 -0600

    remove imap debug code moved to it's own module. update the default config file

--- modules/core/module_map.php
+++ modules/core/module_map.php
@@ -1,54 +1,14 @@
 <?php
 
-/*
- * This is the default mapping for core data and output handlers in HM3.
- * both types of handlers are executed in the order they are defined, so 
- * be careful about mixing them up!.
- *
- * There are two handler assignment methods, both take the same arguments. The
- * two methods are:
- *
- * Hm_Handler_Modules::add  add a data handler process to a page or AJAX request. These
- *                          methods fetch data based on user input.
- *
- * Hm_Output_Modules::add   add an output module to a page. These methods output parts
- *                          of the response "page". This could be HTML, JSON, or even
- *                          terminal formatted content.
- *
- * The required arguments in order are "page", "module name", "logged in":
- *
- *  page          string valid page name or ajax callback
- *  module        string module name of the module associated with the request 
- *                page/ajax hook
- *  logged in     bool flag indicating if the user needs to be logged in for the 
- *                module to be active
- *  
- *  There are three optional arguments:
- *
- *  marker        string name of another module to insert this one before or 
- *                after
- *  placement     string either before or after. only matters if marker is set
- *  module_args   array list of arguments to make available to the module
- *
- */
-
-
-
-/* Homepage */
-
-/* data modules */
+/* homepage data */
 Hm_Handler_Modules::add('home', 'login', false);
-Hm_Handler_Modules::add('home', 'load_imap_servers',  true);
 Hm_Handler_Modules::add('home', 'language',  true);
 Hm_Handler_Modules::add('home', 'title', true);
 Hm_Handler_Modules::add('home', 'date', true);
 Hm_Handler_Modules::add('home', 'logout', true);
-Hm_Handler_Modules::add('home', 'imap_setup', true);
-Hm_Handler_Modules::add('home', 'imap_setup_display', true);
 Hm_Handler_Modules::add('home', 'http_headers', true);
-Hm_Handler_Modules::add('home', 'save_imap_servers',  true);
 
-/* output modules */
+/* homepage output */
 Hm_Output_Modules::add('home', 'header', false);
 Hm_Output_Modules::add('home', 'css', false);
 Hm_Output_Modules::add('home', 'jquery', true);
@@ -57,35 +17,15 @@ Hm_Output_Modules::add('home', 'login', false);
 Hm_Output_Modules::add('home', 'title', true);
 Hm_Output_Modules::add('home', 'msgs', false);
 Hm_Output_Modules::add('home', 'date', true);
-Hm_Output_Modules::add('home', 'imap_setup', true);
-Hm_Output_Modules::add('home', 'imap_setup_display', true);
-Hm_Output_Modules::add('home', 'imap_folders', true);
-Hm_Output_Modules::add('home', 'imap_debug', true);
 Hm_Output_Modules::add('home', 'js', true);
 Hm_Output_Modules::add('home', 'footer', true);
 
-
-/* Ajax debug callback */
-
-/* data modules */
-Hm_Handler_Modules::add('ajax_imap_debug', 'login', false);
-Hm_Handler_Modules::add('ajax_imap_debug', 'load_imap_servers',  true);
-Hm_Handler_Modules::add('ajax_imap_debug', 'imap_connect', true);
-Hm_Handler_Modules::add('ajax_imap_debug', 'imap_delete', true);
-Hm_Handler_Modules::add('ajax_imap_debug', 'save_imap_cache',  true);
-Hm_Handler_Modules::add('ajax_imap_debug', 'save_imap_servers',  true);
-
-/* output modules */
-Hm_Output_Modules::add('ajax_imap_debug', 'imap_folders',  true);
-Hm_Output_Modules::add('ajax_imap_debug', 'imap_debug',  true);
-
-/* Not found page modules */
+/* not-found page data and output */
 Hm_Handler_Modules::add('notfound', 'title', true);
 Hm_Output_Modules::add('notfound', 'title', true);
 
-/**
- * Core input definitions. Other module sets can add to these but cannot overwrite them.
- */
+
+/* allowed input */
 return array(
     'allowed_pages' => array(
         'home',
@@ -105,26 +45,14 @@ return array(
 
     'allowed_get' => array(
         'page' => FILTER_SANITIZE_STRING,
-        'imap_server_id' => FILTER_VALIDATE_INT,
     ),
 
     'allowed_post' => array(
         'logout' => FILTER_VALIDATE_BOOLEAN,
-        'tls' => FILTER_VALIDATE_BOOLEAN,
-        'server_port' => FILTER_VALIDATE_INT,
-        'server' => FILTER_SANITIZE_STRING,
         'username' => FILTER_SANITIZE_STRING,
         'password' => FILTER_SANITIZE_STRING,
-        'new_imap_server' => FILTER_SANITIZE_STRING,
-        'new_imap_port' => FILTER_VALIDATE_INT,
-        'imap_server_id' => FILTER_VALIDATE_INT,
-        'imap_user' => FILTER_SANITIZE_STRING,
-        'imap_pass' => FILTER_SANITIZE_STRING,
-        'imap_delete' => FILTER_SANITIZE_STRING,
-        'submit_server' => FILTER_SANITIZE_STRING,
-        'imap_connect' => FILTER_SANITIZE_STRING,
         'hm_ajax_hook' => FILTER_SANITIZE_STRING,
-        'imap_remember' => FILTER_VALIDATE_INT,
     )
 );
+
 ?>
--- modules/core/output_modules.php
+++ modules/core/output_modules.php
@@ -61,107 +61,6 @@ class Hm_Output_msgs extends Hm_Output_Module {
     }
 }}
 
-if (!class_exists('Hm_Output_imap_setup_display')) {
-class Hm_Output_imap_setup_display extends Hm_Output_Module {
-    protected function output($input, $format) {
-        $res = '';
-        if ($format == 'HTML5') {
-            $res = '<div class="configured_servers"><div class="subtitle">Configured Servers</div>';
-            foreach ($input['imap_servers'] as $index => $vals) {
-
-                $user_row = '<span id="user_row'.$index.'"> '.$this->trans('Username').': <input type="text" name="imap_user" value=""></span>';
-                $pass_row = '<span id="pass_row'.$index.'"> '.$this->trans('Password').': <input type="password" name="imap_pass"></span>';
-
-                $res .= '<div class="configured_server" id="imap_server'.$index.'">';
-                $res .= sprintf("Server: %s<br />Port: %d<br />TLS: %s<br /><br />", $this->html_safe($vals['server']),
-                    $this->html_safe($vals['port']), $vals['tls'] ? 'true' : 'false' );
-                $res .= ' <form class="imap_connect" method="POST">'.
-                    '<input type="hidden" id="imap_server_id'.$index.'" name="imap_server_id" value="'.$this->html_safe($index).'" />';
-                if (!isset($vals['user'])) {
-                    $res .= $user_row.$pass_row;
-                }
-                $res .= ' Remember: <input type="checkbox" '. (isset($vals['user']) ? 'checked="checked" ' : '') . 'value="1" name="imap_remember" /><br /><br />'.
-                    ' <input type="submit" value="Test Connection" id="imap_connect'.$index.'" />'.
-                    ' <input type="submit" value="Delete" id="imap_delete'.$index.'" />'.
-                    ' <input type="hidden" value="ajax_imap_debug" name="hm_ajax_hook" /></form><script type="text/javascript">'.
-                    '$("#imap_delete'.$index.'").on("click", function() {'.
-                        '$(".imap_debug_data").empty(); '.
-                        'event.preventDefault(); Hm_Ajax.request( $( this ).parent().serializeArray(), function(res) {'.
-                        'Hm_Notices.show(res.router_user_msgs); if (res.deleted_server_id > -1 ) {$("#imap_server'.$index.'").remove();}},'.
-                        '{"imap_delete": 1});});'.
-                    '$("#imap_connect'.$index.'").on("click", function() {'.
-                        '$(this).attr("disabled", true); $(".imap_debug_data").empty(); '.
-                        'event.preventDefault(); form = $(this).parent(); Hm_Ajax.request( $(this).parent().serializeArray(), function(res) {'.
-                        'Hm_Notices.show(res.router_user_msgs); '.
-                        'if (res.just_saved_credentials) { $("#pass_row'.$index.'").remove(); $("#user_row'.$index.'").remove(); } '.
-                        'if (res.just_forgot_credentials) { $(\''.$pass_row.'\').insertAfter("#imap_server_id'.$index.'"); '.
-                        '$(\''.$user_row.'\').insertAfter("#imap_server_id'.$index.'"); } '.
-                        '$("#imap_connect'.$index.'").attr("disabled", false); Hm_Folders.show(res.imap_folders); $(".imap_debug_data").html(res.imap_debug); },'.
-                        '{"imap_connect": 1});});'.
-                    '</script></div>';
-            }
-            $res .= '</div>';
-        }
-        return $res;
-    }
-}}
-
-if (!class_exists('Hm_Output_imap_debug')) {
-class Hm_Output_imap_debug extends Hm_Output_Module {
-    protected function output($input, $format) {
-        if ($format == 'HTML5') {
-            $res = '<div class="imap_debug"><div class="subtitle">IMAP Debug</div><pre class="imap_debug_data">';
-            if (isset($input['imap_debug'])) {
-                $res .= $this->html_safe($input['imap_debug']);
-            }
-            $res .= '</pre></div>';
-            return $res;
-        }
-        elseif ($format == 'JSON') {
-            if (isset($input['imap_debug'])) {
-                $input['imap_debug'] = $this->html_safe($input['imap_debug']);
-            }
-            return $input;
-        }
-    }
-}}
-
-if (!class_exists('Hm_Output_imap_setup')) {
-class Hm_Output_imap_setup extends Hm_Output_Module {
-    protected function output($input, $format) {
-        if ($format == 'HTML5') {
-            return '<form class="add_server" method="POST">'.
-                '<div class="subtitle">Add a mail server</div>'.
-                'Server name or address: <input type="text" name="new_imap_server" value=""/><br />'.
-                'Server Port: <input type="text" name="new_imap_port" value="143"><br />'.
-                'Use TLS: <input type="checkbox" name="tls" value="1" /><br />'.
-                '<input type="submit" value="Add" onclick="$( this ).css(\'visibility\', \'hidden\'); return true;" name="submit_server" /></form>';
-        }
-    }
-}}
-
-if (!class_exists('Hm_Output_imap_folders')) {
-class Hm_Output_imap_folders extends Hm_Output_Module {
-    protected function output($input, $format, $lang_str=false) {
-        if ($format == 'HTML5') {
-            $res = '<div class="imap_folders"><div class="subtitle">Top Level Folders</div><div class="imap_folder_data">';
-            if (isset($input['imap_folders'])) {
-                foreach (array_keys($input['imap_folders']) as $folder) {
-                    $res .= '<div class="folder">'.$this->html_safe($folder).'</div>';
-                }
-            }
-            $res .= '</div></div>';
-            return $res;
-        }
-        elseif ($format == 'JSON') {
-            if (isset($input['imap_folders'])) {
-                $input['imap_folders'] = array_map(function($v) { return $this->html_safe($v); }, array_keys($input['imap_folders']));
-            }
-            return $input;
-        }
-    }
-}}
-
 if (!class_exists('Hm_Output_header')) {
 class Hm_Output_header extends Hm_Output_Module {
     protected function output($input, $format) {
@@ -212,7 +111,9 @@ class Hm_Output_css extends Hm_Output_Module {
 if (!class_exists('Hm_Output_js')) {
     class Hm_Output_js extends Hm_Output_Module {
         protected function output($input, $format) {
-            return '<script type="text/javascript" src="js/site.js"></script>';
+            if ($format == 'HTML5') {
+                return '<script type="text/javascript" src="js/site.js"></script>';
+            }
         }
     }
 }
