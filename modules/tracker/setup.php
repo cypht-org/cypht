e4d20f1441735f35400155511425396a30b20732
jasonmunro <jason@hastymail.org>
2014-03-13 13:48:00 -0500

    update layout and setup for changes in core mods

--- modules/tracker/setup.php
+++ modules/tracker/setup.php
@@ -3,8 +3,9 @@
 Hm_Handler_Modules::add('home', 'tracker', false, 'http_headers', 'after');
 Hm_Handler_Modules::add('ajax_imap_debug', 'tracker', false, 'save_imap_servers', 'after');
 
-Hm_Output_Modules::add('home', 'tracker', false, 'logout', 'after');
-Hm_Output_Modules::add('home', 'show_debug', false, 'footer', 'before');
+Hm_Output_Modules::add('home', 'tracker', false, 'msgs', 'after');
+Hm_Output_Modules::add('home', 'show_debug', false, 'tracker', 'after');
+
 Hm_Output_Modules::add('ajax_imap_debug', 'tracker', false);
 Hm_Output_Modules::add('ajax_imap_debug', 'show_debug', false, 'imap_debug', 'after');
 
