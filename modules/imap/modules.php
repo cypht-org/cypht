dfcc7a7a4bc007f2dba1eb574f3d2c517e1dc10b
jasonmunro <jason@hastymail.org>
2014-03-13 23:47:43 -0500

    clean up debug mode formatting and hide the imap debug bits on the imap setup page with DEBUG_MODE

--- modules/imap/modules.php
+++ modules/imap/modules.php
@@ -327,19 +327,21 @@ class Hm_Output_imap_setup_display extends Hm_Output_Module {
 
 class Hm_Output_imap_debug extends Hm_Output_Module {
     protected function output($input, $format) {
-        if ($format == 'HTML5') {
-            $res = '<div class="imap_debug"><div class="subtitle">IMAP Debug</div><pre class="imap_debug_data">';
-            if (isset($input['imap_debug'])) {
-                $res .= $this->html_safe($input['imap_debug']);
+        if (DEBUG_MODE) {
+            if ($format == 'HTML5') {
+                $res = '<div class="imap_debug"><div class="subtitle">IMAP Debug</div><pre class="imap_debug_data">';
+                if (isset($input['imap_debug'])) {
+                    $res .= $this->html_safe($input['imap_debug']);
+                }
+                $res .= '</pre></div>';
+                return $res;
             }
-            $res .= '</pre></div>';
-            return $res;
-        }
-        elseif ($format == 'JSON') {
-            if (isset($input['imap_debug'])) {
-                $input['imap_debug'] = $this->html_safe($input['imap_debug']);
+            elseif ($format == 'JSON') {
+                if (isset($input['imap_debug'])) {
+                    $input['imap_debug'] = $this->html_safe($input['imap_debug']);
+                }
+                return $input;
             }
-            return $input;
         }
     }
 }
@@ -359,21 +361,23 @@ class Hm_Output_imap_setup extends Hm_Output_Module {
 
 class Hm_Output_imap_folders extends Hm_Output_Module {
     protected function output($input, $format, $lang_str=false) {
-        if ($format == 'HTML5') {
-            $res = '<div class="imap_folders"><div class="subtitle">Top Level Folders</div><div class="imap_folder_data">';
-            if (isset($input['imap_folders'])) {
-                foreach (array_keys($input['imap_folders']) as $folder) {
-                    $res .= '<div class="folder">'.$this->html_safe($folder).'</div>';
+        if (DEBUG_MODE) {
+            if ($format == 'HTML5') {
+                $res = '<div class="imap_folders"><div class="subtitle">Top Level Folders</div><div class="imap_folder_data">';
+                if (isset($input['imap_folders'])) {
+                    foreach (array_keys($input['imap_folders']) as $folder) {
+                        $res .= '<div class="folder">'.$this->html_safe($folder).'</div>';
+                    }
                 }
+                $res .= '</div></div>';
+                return $res;
             }
-            $res .= '</div></div>';
-            return $res;
-        }
-        elseif ($format == 'JSON') {
-            if (isset($input['imap_folders'])) {
-                $input['imap_folders'] = array_map(function($v) { return $this->html_safe($v); }, array_keys($input['imap_folders']));
+            elseif ($format == 'JSON') {
+                if (isset($input['imap_folders'])) {
+                    $input['imap_folders'] = array_map(function($v) { return $this->html_safe($v); }, array_keys($input['imap_folders']));
+                }
+                return $input;
             }
-            return $input;
         }
     }
 }
