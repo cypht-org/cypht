456f7df588cbd5e8f57d0c0172637976b66a27cd
jasonmunro <jason@hastymail.org>
2014-03-24 17:32:40 -0500

    update tracker dialogs to prepend new results

--- modules/tracker/site.js
+++ modules/tracker/site.js
@@ -4,15 +4,15 @@ $(document).ajaxSuccess(function(event, xhr, settings) {
         return;
     }
     if (response.module_debug) {
-        $(".module_list").html(response.module_debug);
+        $(".module_list").prepend(response.module_debug);
     }
     if (response.hm3_debug) {
-        $(".hm3_debug").html(response.hm3_debug);
+        $(".hm3_debug").prepend(response.hm3_debug);
     }
     if (response.imap_summary_debug) {
-        $(".hm3_imap_debug").html(response.imap_summary_debug);
+        $(".hm3_imap_debug").prepend(response.imap_summary_debug);
     }
     if (response.pop3_summary_debug) {
-        $(".hm3_pop3_debug").html(response.pop3_summary_debug);
+        $(".hm3_pop3_debug").prepend(response.pop3_summary_debug);
     }
 });
