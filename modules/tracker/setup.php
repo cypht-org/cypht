b3e3a2fcfcb9e100af31565107f2bd7a470c5a8e
jasonmunro <jason@hastymail.org>
2014-04-13 12:15:11 -0500

    remove unused tracker mods

--- modules/tracker/setup.php
+++ modules/tracker/setup.php
@@ -25,16 +25,6 @@ add_handler('ajax_imap_debug', 'imap_tracker', true, 'tracker', 'imap_connect', 
 add_output('ajax_imap_debug', 'tracker', false, 'tracker');
 add_output('ajax_imap_debug', 'show_debug', false, 'tracker');
 
-add_handler('ajax_imap_summary', 'imap_tracker', true, 'tracker', 'prep_imap_summary_display', 'after');
-add_handler('ajax_imap_summary', 'tracker', false, 'tracker', 'prep_imap_summary_display', 'after');
-add_output('ajax_imap_summary', 'show_debug', false, 'tracker');
-add_output('ajax_imap_summary', 'tracker', false, 'tracker');
-
-add_handler('ajax_pop3_summary', 'pop3_tracker', true, 'tracker', 'prep_pop3_summary_display', 'after');
-add_handler('ajax_pop3_summary', 'tracker', false, 'tracker', 'prep_pop3_summary_display', 'after');
-add_output('ajax_pop3_summary', 'show_debug', false, 'tracker');
-add_output('ajax_pop3_summary', 'tracker', false, 'tracker');
-
 add_handler('ajax_imap_unread', 'tracker', false, 'tracker', 'imap_unread', 'after');
 add_handler('ajax_imap_unread', 'imap_tracker', true, 'tracker', 'imap_unread', 'after');
 add_output('ajax_imap_unread', 'tracker', false, 'tracker');
