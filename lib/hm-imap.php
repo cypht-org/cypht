cfc4ae4e74d7804faf9e6dec318f2ec4429a690b
jasonmunro <jason@hastymail.org>
2014-05-19 14:33:27 -0500

    remove debug

--- lib/hm-imap.php
+++ lib/hm-imap.php
@@ -890,7 +890,6 @@ class Hm_IMAP extends Hm_IMAP_Cache {
                 $res = mb_convert_encoding($res, 'UTF-8', $struct['charset']);
             }
         }
-error_log(print_r($res,true));
         if ($status) {
             return $this->cache_return_val($res, $cache_command);
         }
