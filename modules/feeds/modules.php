1606a4a357f4d0144a3969eb0b9b2f7361cc0f83
jasonmunro <jason@hastymail.org>
2014-07-18 14:32:05 -0500

    fix copy paste bug in folder list highlighting for feeds

--- modules/feeds/modules.php
+++ modules/feeds/modules.php
@@ -293,7 +293,7 @@ class Hm_Output_filter_feed_list_data extends Hm_Output_Module {
                         $url .= '&amp;list_parent=combined_inbox';
                     }
                     else {
-                        $url .= '&amp;list_parent=pop3_'.$this->html_safe($item['server_id']);
+                        $url .= '&amp;list_parent=feeds_'.$this->html_safe($item['server_id']);
                     }
                     $from = isset($item['author']) ? $this->html_safe($item['author']) : '';
                     $from = !$from && isset($item['dc:creator']) ? $this->html_safe($item['dc:creator']) : $from;
