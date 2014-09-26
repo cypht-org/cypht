5e2da865dd8f966e5a6d921b1bbcbe8808fd975c
jasonmunro <jason@hastymail.org>
2014-09-26 17:51:12 -0500

    include zepto in the site.js build

--- scripts/config_gen.php
+++ scripts/config_gen.php
@@ -3,14 +3,15 @@
 define("DEBUG_MODE", false);
 
 /* command that takes js from stdin and outputs compressed results
- * example: 'java -jar /usr/local/lib/yuicompressor-2.4.8.jar --type js';
+ * examples: 'java -jar /usr/local/lib/yuicompressor-2.4.8.jar --type js'
+ *           'uglifyjs -nc --unsafe -c -v'
  */
 $js_compress = false;
 
 /* command that takes css from stdin and outputs compressed results
  * example: 'java -jar /usr/local/lib/yuicompressor-2.4.8.jar --type css';
  */
-$css_compress = false; 
+$css_compress = false;
 
 require 'lib/framework.php';
 
@@ -21,7 +22,7 @@ if (isset($options['ini_file'])) {
     $settings = parse_ini_file($options['ini_file']);
 }
 if (!empty($settings)) {
-    $js = '';
+    $js = file_get_contents("lib/zepto.min.js");
     $css = '';
     $mod_map = array();
     $filters = array('allowed_get' => array(), 'allowed_cookie' => array(), 'allowed_post' => array(), 'allowed_server' => array(), 'allowed_pages' => array());
