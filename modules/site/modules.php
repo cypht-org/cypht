b2a48b4e62f5e2ebaf8f185e1e915e3fb2b45b51
jasonmunro <jason@hastymail.org>
2014-09-25 22:20:44 -0500

    site module for custom overrides.

---
+++ modules/site/modules.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!defined('DEBUG_MODE')) { die(); }
+
+/*
+ *  class Hm_Handler_site_http_headers extends Hm_Handler_Module {
+ *      public function process($data) {
+ *          if (array_key_exists('language', $data)) {
+ *              $data['http_headers'][] = 'Content-Language: '.substr($data['language'], 0, 2);
+ *          }
+ *          if ($this->request->tls) {
+ *              $data['http_headers'][] = 'Strict-Transport-Security: max-age=31536000';
+ *          }
+ *          return $data;
+ *      }
+ *  }
+ */
+
+?>
