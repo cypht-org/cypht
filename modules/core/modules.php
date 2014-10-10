36090846a30d5974915e8c76bc37fe2f4e4f1912
Jason <jason@hastymail.org>
2014-10-10 14:48:11 -0500

    make sure we set a long past expire date for browser caching on dynamic pages

--- modules/core/modules.php
+++ modules/core/modules.php
@@ -27,7 +27,7 @@ class Hm_Handler_http_headers extends Hm_Handler_Module {
         }
         $data['http_headers'][] = 'X-XSS-Protection: 1; mode=block';
         $data['http_headers'][] = 'X-Content-Type-Options: nosniff';
-        $data['http_headers'][] = 'Expires: '.gmdate('D, d M Y H:i:s \G\M\T', strtotime('-1 minute'));
+        $data['http_headers'][] = 'Expires: '.gmdate('D, d M Y H:i:s \G\M\T', strtotime('-1 year'));
         $data['http_headers'][] = "Content-Security-Policy: default-src 'none'; script-src 'self' 'unsafe-inline'; connect-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline';";
         return $data;
     }
