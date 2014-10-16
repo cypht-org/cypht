7b51c58dbfa8e4bba3a44e6695bc37484f1d590f
Jason <jason@hastymail.org>
2014-10-16 16:46:10 -0500

    autofocus on the swipe 2fa dialogs

--- modules/swipe_2fa/modules.php
+++ modules/swipe_2fa/modules.php
@@ -134,7 +134,7 @@ class Hm_Output_swipe_2fa_dialog extends Hm_Output_Module {
                     '<div class="swipe_txt">Register your number for Swipeidentity two factor authentication. '.
                     'The number must include a country code. Only enter numbers and no spaces or delimiters</div>'.
                     '<input type="hidden" name="hm_nonce" value="'.$input['2fa_nonce'].'" />'.
-                    '<input type="text" name="sms_number" value="" placeholder="Phone number to SMS to" />'.
+                    '<input autofocus required type="text" name="sms_number" value="" placeholder="Phone number to SMS to" />'.
                     '<input type="submit" name="submit_swipe_number" value="Submit" />'.
                     '</form>';
             }
@@ -145,7 +145,7 @@ class Hm_Output_swipe_2fa_dialog extends Hm_Output_Module {
                     $error.
                     '<div class="swipe_txt">Enter the 5 digit SMS code you just received</div>'.
                     '<input type="hidden" name="hm_nonce" value="'.$input['2fa_nonce'].'" />'.
-                    '<input type="text" name="2fa_sms_response" value="" placeholder="Login code" />'.
+                    '<input autofocus required type="text" name="2fa_sms_response" value="" placeholder="Login code" />'.
                     '<input type="submit" name="submit_2fa_response" value="Submit" />'.
                     '</form>';
             }
