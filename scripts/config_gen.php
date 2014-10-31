2486e251d53de9147bccd342c8cb958d8ffa1dbf
Jason <jason@hastymail.org>
2014-10-30 22:28:22 -0500

    refactor the configuration script

--- scripts/config_gen.php
+++ scripts/config_gen.php
@@ -1,18 +1,78 @@
 <?php
 
+/**
+ * CLI script to build the site configuration
+ */
+
+/* debug mode has to be set to something or include files will die() */
 define('DEBUG_MODE', false);
+
+/* determine current absolute path used for require statements */
 define('APP_PATH', dirname(dirname(__FILE__)).'/');
 
+/* get the framework */
 require APP_PATH.'lib/framework.php';
-$settings = '';
-$error = '';
 
-$settings = parse_ini_file(APP_PATH.'hm3.ini');
+/* create site */
+build_config();
 
-if (!empty($settings)) {
-    $js = '';
-    $css = '';
-    $mod_map = array();
+/**
+ * Entry point into the configuration process
+ *
+ * @return void
+ */
+function build_config() {
+    /* get the site settings */
+    $settings = parse_ini_file(APP_PATH.'hm3.ini');
+
+    if (is_array($settings) && !empty($settings)) {
+
+        /* determine compression commands */
+        list($js_compress, $css_compress) = compress_methods($settings);
+
+        /* get module detail */
+        list($js, $css, $filters) = get_module_assignments($settings);
+
+        /* combined and compress page assets */
+        combine_includes($js, $js_compress, $css, $css_compress);
+
+        /* write out the hm3.rc file */
+        write_config_file($settings, $filters);
+
+        /* create the production version */
+        create_production_site();
+    }
+    else {
+        printf("\nNo settings found in ini file\n");
+    }
+}
+
+/**
+ * Compress a string
+ *
+ * @param $string string content to compress
+ * @param $command string command to do the compression
+ *
+ * @return string compressed string
+ */
+function compress($string, $command) {
+    if ($command) {
+        exec("echo ".escapeshellarg($string)." | $command", $output);
+        return join('', $output);
+    }
+    else {
+        return $string;
+    }
+}
+
+/**
+ * Check for site specific compression commands
+ *
+ * @param $settings array site settings list
+ *
+ * @return array compression methods or false for none
+ */
+function compress_methods($settings) {
     $js_compress = false;
     $css_compress = false;
     if (isset($settings['js_compress']) && $settings['js_compress']) {
@@ -21,7 +81,25 @@ if (!empty($settings)) {
     if (isset($settings['css_compress']) && $settings['css_compress']) {
         $css_compress = $settings['css_compress'];
     }
-    $filters = array('allowed_output' => array(), 'allowed_get' => array(), 'allowed_cookie' => array(), 'allowed_post' => array(), 'allowed_server' => array(), 'allowed_pages' => array());
+    return array($js_compress, $css_compress);
+}
+
+/**
+ * Get module content and filters. This function has a side effect of setting
+ * up all the module assignments in Hm_Output_Modules and Hm_Handler_Modules.
+ * (this happens when the module set's setup.php file is included).
+ * These will be recorded later in the write_config_file function
+ *
+ * @param $settings array site settings list
+ *
+ * @return array js and css blobs, and the combined filers array
+ */
+function get_module_assignments($settings) {
+    $js = '';
+    $css = '';
+    $filters = array('allowed_output' => array(), 'allowed_get' => array(), 'allowed_cookie' => array(),
+        'allowed_post' => array(), 'allowed_server' => array(), 'allowed_pages' => array());
+
     if (isset($settings['modules'])) {
         foreach (explode(',', $settings['modules']) as $mod) {
             printf("scanning module %s ...\n", $mod);
@@ -36,6 +114,20 @@ if (!empty($settings)) {
             }
         }
     }
+    return array($js, $css, $filters);
+}
+
+/**
+ * Write out combined javascript and css files
+ *
+ * @param $js string combined javascript from all modules
+ * @param $js_compress string command to compress the js
+ * @param $css string combined css from all modules
+ * @param $css_compress string command to compress the css
+ *
+ * @return void
+ */
+function combine_includes($js, $js_compress, $css, $css_compress) {
     if ($css) {
         file_put_contents('site.css', compress($css, $css_compress));
         printf("site.css file created\n");
@@ -45,7 +137,17 @@ if (!empty($settings)) {
         file_put_contents('site.js', $js_lib.compress($js, $js_compress));
         printf("site.js file created\n");
     }
+}
 
+/**
+ * Write the hm3.rc file to disk
+ *
+ * @param $settings array site settings list
+ * @param $filters array combined list of filters from all modules
+ * 
+ * @return void
+ */
+function write_config_file($settings, $filters) {
     Hm_Handler_Modules::process_all_page_queue();
     Hm_Output_Modules::process_all_page_queue();
     $settings['handler_modules'] = Hm_Handler_Modules::dump();
@@ -53,7 +155,15 @@ if (!empty($settings)) {
     $settings['input_filters'] = $filters;
     file_put_contents('hm3.rc', serialize($settings));
     printf("hm3.rc file written\n");
+}
 
+/**
+ * Copies the site.js and site.css files to the site/ directory, and creates
+ * a production version of the index.php file.
+ *
+ * @return void
+ */
+function create_production_site() {
     if (!is_readable('site/')) {
         mkdir('site');
     }
@@ -65,20 +175,6 @@ if (!empty($settings)) {
     $index_file = preg_replace("/CACHE_ID', ''/", "CACHE_ID', '".urlencode(Hm_Crypt::unique_id(32))."'", $index_file);
     $index_file = preg_replace("/DEBUG_MODE', true/", "DEBUG_MODE', false", $index_file);
     file_put_contents('site/index.php', $index_file);
-}
-else {
-    printf("\nNo settings found in ini file\n");
-}
-
-function compress($string, $command) {
-
-    if ($command) {
-        exec("echo ".escapeshellarg($string)." | $command", $output);
-        return join('', $output);
-    }
-    else {
-        return $string;
-    }
 }
 
 ?>
