4b6801a12e748c57af639eaca3c8919d232f4523
jason <jason@hastymail.org>
2015-01-04 13:15:32 -0600

    add comment to the db tests

--- tests/db.php
+++ tests/db.php
@@ -8,6 +8,8 @@ class Hm_Test_DB extends PHPUnit_Framework_TestCase {
     public function setUp() {
         $this->config = new Hm_Mock_Config();
     }
+
+    /* test for Hm_DB */
     public function test_connect() {
         $this->assertFalse(Hm_DB::connect($this->config));
         setup_db($this->config);
