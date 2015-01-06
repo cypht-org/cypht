60221619cc3bd7c27ed005ddced2d460492a73e4
Jason <jason@hastymail.org>
2015-01-05 19:29:15 -0600

    server unit test updates

--- tests/servers.php
+++ tests/servers.php
@@ -10,6 +10,7 @@ class Hm_Server_Wrapper {
 }
 class Hm_Test_Servers extends PHPUnit_Framework_TestCase {
 
+    /* tests for Hm_Server_List trait */
     public function test_add() {
         Hm_Server_Wrapper::add(array('user' => 'testuser', 'pass' => 'testpass', 'name' => 'test', 'server' => 'test', 'port' => 0, 'tls' => 1));
         $this->assertEquals(array(array('user' => 'testuser', 'name' => 'test', 'server' => 'test', 'port' => 0, 'tls' => 1)), Hm_Server_Wrapper::dump());
@@ -33,7 +34,9 @@ class Hm_Test_Servers extends PHPUnit_Framework_TestCase {
         $this->assertTrue(Hm_Server_Wrapper::service_connect(0, 'test', 'testuser', 'testpass', false));
     }
     public function test_connect() {
-        $this->assertTrue(Hm_Server_Wrapper::connect(0, false, 'test', 'testuser', 'testpass') !== false);
+        $this->assertTrue(Hm_Server_Wrapper::connect(0) !== false);
+        Hm_Server_Wrapper::add(array('user' => 'testuser', 'pass' => 'testpass', 'name' => 'test2', 'server' => 'test2', 'port' => 0, 'tls' => 1));
+        $this->assertTrue(Hm_Server_Wrapper::connect(1) !== false);
     }
     public function test_cleanup() {
         $this->assertTrue(Hm_Server_Wrapper::service_connect(0, 'test', 'testuser', 'testpass', false));
@@ -41,7 +44,6 @@ class Hm_Test_Servers extends PHPUnit_Framework_TestCase {
         $atts = Hm_Server_Wrapper::dump(0, true);
         $this->assertFalse($atts['connected']);
     }
-
 }
 
 ?>
