6f2771857918a4d4d9f0a7e46041f1bd60585a1f
jason <jason@hastymail.org>
2015-01-06 00:04:43 -0600

    moar unit test

--- tests/request.php
+++ tests/request.php
@@ -1,6 +1,14 @@
 <?php
 
 class Hm_Test_Request extends PHPUnit_Framework_TestCase {
+
+    /* tests for the Hm_Request object */
+    public function test_request() {
+        $_SERVER['REQUEST_URI'] = 'test';
+        $_GET['foo'] = 'bar';
+        $_POST['bar'] = 1;
+        $req = new Hm_Request(filters());
+    }
 }
 
 ?>
