ebc5955a92978b069c0803a7cd94ba6d29cc056f
jason <jason@hastymail.org>
2015-01-08 18:46:54 -0600

    moar coverage

--- tests/servers.php
+++ tests/servers.php
@@ -63,6 +63,7 @@ class Hm_Test_Server_List extends PHPUnit_Framework_TestCase {
         $this->assertTrue(Hm_Server_Wrapper::connect(0) !== false);
         Hm_Server_Wrapper::add(array('user' => 'testuser', 'pass' => 'testpass', 'name' => 'test2', 'server' => 'test2', 'port' => 0, 'tls' => 1), 1);
         $this->assertTrue(Hm_Server_Wrapper::connect(1, false, false, false, true) !== false);
+        $this->assertTrue(Hm_Server_Wrapper::connect(1, false, false, false, true) !== false);
         Hm_Server_Wrapper::add(array('pass' => 'testpass', 'name' => 'test2', 'server' => 'test2', 'port' => 0, 'tls' => 1), 2);
         $this->assertFalse(Hm_Server_Wrapper::connect(2));
         $this->assertFalse(Hm_Server_Wrapper::connect(234));
@@ -73,9 +74,10 @@ class Hm_Test_Server_List extends PHPUnit_Framework_TestCase {
      */
     public function test_cleanup() {
         $this->assertTrue(Hm_Server_Wrapper::service_connect(0, 'test', 'testuser', 'testpass', false));
-        Hm_Server_Wrapper::clean_up(0);
-        $atts = Hm_Server_Wrapper::dump(0, true);
+        Hm_Server_Wrapper::clean_up(3);
+        $atts = Hm_Server_Wrapper::dump(3, true);
         $this->assertFalse($atts['connected']);
+        Hm_Server_Wrapper::clean_up(0);
     }
 }
 
