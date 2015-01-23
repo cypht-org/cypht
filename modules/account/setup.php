85d0699516516efb4ebd883f6b22edc3bdcb431d
Jason <jason@hastymail.org>
2015-01-23 14:14:27 -0600

    comment out account creation for now

--- modules/account/setup.php
+++ modules/account/setup.php
@@ -6,15 +6,15 @@ handler_source('account');
 output_source('account');
 
 /* create account page */
-setup_base_page('create_account', 'core');
-replace_module('output', 'login', 'no_login', 'create_account');
-add_handler('create_account', 'process_create_account', false, 'account', 'login', 'before');
-add_handler('create_account', 'check_internal_users', false, 'account', 'login', 'before');
-add_output('create_account', 'create_form', false, 'account', 'content_section_start', 'after');
+//setup_base_page('create_account', 'core');
+//replace_module('output', 'login', 'no_login', 'create_account');
+//add_handler('create_account', 'process_create_account', false, 'account', 'login', 'before');
+//add_handler('create_account', 'check_internal_users', false, 'account', 'login', 'before');
+//add_output('create_account', 'create_form', false, 'account', 'content_section_start', 'after');
 
 /* home page */
 add_handler('home', 'check_internal_users', false, 'account', 'login', 'before');
-add_output('home', 'create_account_link', false, 'account', 'login', 'after');
+//add_output('home', 'create_account_link', false, 'account', 'login', 'after');
 
 /* settings page for password change */
 add_handler('settings', 'process_change_password', true, 'account', 'date', 'after');
