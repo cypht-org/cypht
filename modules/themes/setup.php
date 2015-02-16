e92c69e05a9ada931d203b0473c03403fb54ad1f
Jason Munro <jason@hastymail.org>
2015-02-16 12:21:31 -0600

    more theme stuff

--- modules/themes/setup.php
+++ modules/themes/setup.php
@@ -8,6 +8,7 @@ output_source('themes');
 add_module_to_all_pages('handler', 'load_theme', true, 'themes', 'load_user_data', 'after');
 add_module_to_all_pages('output', 'theme_css', true, 'themes', 'header_css', 'after');
 
+add_handler('ajax_hm_folders', 'load_theme', true, 'themes', 'load_user_data', 'after');
 add_handler('settings', 'process_theme_setting', true, 'themes', 'save_user_settings', 'before'); 
 add_output('settings', 'theme_setting', true, 'feeds', 'language_setting', 'after');
 
