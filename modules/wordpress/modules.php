99e634f0a6220a800d4045f66015ac4d924a5eba
Jason Munro <jason@hastymail.org>
2015-02-24 20:16:12 -0600

    wordpress updates

--- modules/wordpress/modules.php
+++ modules/wordpress/modules.php
@@ -7,6 +7,50 @@
  */
 if (!defined('DEBUG_MODE')) { die(); }
 
+class Hm_Handler_process_wordpress_authorization extends Hm_Handler_Module {
+    public function process() {
+        if (array_key_exists('state', $this->request->get) && $this->request->get['state'] == 'wp_authorization') {
+            if (array_key_exists('code', $this->request->get)) {
+                $details = wp_connect_details($this->config);
+                $oauth2 = new Hm_Oauth2($details['client_id'], $details['client_secret'], $details['redirect_uri']);
+                $result = $oauth2->request_token($details['token_url'], $this->request->get['code']);
+                if (!empty($result) && array_key_exists('access_token', $result)) {
+                    Hm_Msgs::add('WordPress.com connection established');
+                    $this->user_config->set('wp_connect_details', $result);
+                    $user_data = $this->user_config->dump();
+                    if (!empty($user_data)) {
+                        $this->session->set('user_data', $user_data);
+                    }
+                    $this->session->record_unsaved('WordPress.com connection');
+                    $this->session->secure_cookie($this->request, 'hm_reload_folders', '1');
+                    $this->session->close_early();
+                }
+                else {
+                    Hm_Msgs::add('ERRAn Error Occured');
+                }
+            }
+            elseif (array_key_exists('error', $this->request->get)) {
+                Hm_Msgs::add('ERR'.ucwords(str_replace('_', ' ', $this->request->get['error'])));
+            }
+            else {
+                Hm_Msgs::add('ERRAn Error Occured');
+            }
+            $msgs = Hm_Msgs::get();
+            $this->session->secure_cookie($this->request, 'hm_msgs', base64_encode(serialize($msgs)), 0);
+            Hm_Router::page_redirect('?page=servers');
+        }
+    }
+}
+
+class Hm_Handler_setup_wordpress_connect extends Hm_Handler_Module {
+    public function process() {
+        $details = wp_connect_details($this->config);
+        $oauth2 = new Hm_Oauth2($details['client_id'], $details['client_secret'], $details['redirect_uri']);
+        $this->out('wp_auth_url', $oauth2->request_authorization_url($details['auth_url'], 'global', 'wp_authorization'));
+        $this->out('wp_connect_details', $this->user_config->get('wp_connect_details', array()));
+    }
+}
+
 /**
  * @subpackage wordpress/output
  */
@@ -19,6 +63,34 @@ class Hm_Output_wordpress_folders extends Hm_Output_Module {
         Hm_Page_Cache::add('wordPress_folders', $res, true);
         return '';
     }
+}
+
+class Hm_Output_wordpress_connect_section extends Hm_Output_Module {
+    protected function output() {
+        elog($this->get('wp_connect_details', array()));
+        return '<div class="wordpress_connect"><div data-target=".wordpress_connect_section" class="server_section">'.
+            '<img src="'.Hm_Image_Sources::$key.'" alt="" width="16" height="16" /> '.
+            $this->trans('WordPress.com Connect').'</div><div class="wordpress_connect_section">'.
+            'Connect to WordPress.com to view your notifications.<br /><a href="'.$this->get('wp_auth_url', '').
+            '">Enable</a></div></div>';
+    }
+}
+
+function wp_connect_details($config) {
+    $details = array (
+        'auth_url' => 'https://public-api.wordpress.com/oauth2/authorize',
+        'token_url' => 'https://public-api.wordpress.com/oauth2/token',
+    );
+    $ini_file = rtrim($config->get('app_data_dir', ''), '/').'/wordpress.ini';
+    if (is_readable($ini_file)) {
+        $settings = parse_ini_file($ini_file);
+        if (!empty($settings)) {
+            $details['client_id'] = $settings['client_id'];
+            $details['client_secret'] = $settings['client_secret'];
+            $details['redirect_uri'] = $settings['client_uri'];
+        }
+    }
+    return $details;
 }
 
 ?>
