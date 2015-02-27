02cad74ce3a5114e681907dd721561f76a3a9807
Jason Munro <jason@hastymail.org>
2015-02-27 12:38:53 -0600

    more wordpress module set stuff

--- modules/wordpress/setup.php
+++ modules/wordpress/setup.php
@@ -5,30 +5,61 @@ if (!defined('DEBUG_MODE')) { die(); }
 handler_source('wordpress');
 output_source('wordpress');
 
-setup_base_page('wordpress_notifications', 'core');
-add_output('wordpress_notifications', 'wp_notice_heading', true, 'core', 'content_section_start', 'after');
-add_output('wordpress_notifications', 'message_list_start', true, 'core', 'wp_notice_heading', 'after');
-add_output('wordpress_notifications', 'wp_notice_end', true, 'core', 'message_list_start', 'after');
+setup_base_page('wordpress', 'core');
 
+replace_module('handler', 'message_list_type', 'wordpress_list_type', 'wordpress');
+add_output('wordpress', 'wp_notice_heading', true, 'core', 'content_section_start', 'after');
+add_output('wordpress', 'message_list_start', true, 'core', 'wp_notice_heading', 'after');
+add_output('wordpress', 'wp_notice_end', true, 'core', 'message_list_start', 'after');
+
+add_handler('message_list', 'wp_load_sources', true, 'wordpress', 'load_user_data', 'after');
 add_output('ajax_hm_folders', 'wordpress_folders',  true, 'wordpress', 'folder_list_content_start', 'before');
+
+add_handler('message', 'wordpress_list_type', true, 'wordpress', 'load_user_data', 'after');
 
 add_handler('servers', 'setup_wordpress_connect', true, 'wordpress', 'load_user_data', 'after');
 add_output('servers', 'wordpress_connect_section', true, 'wordpress', 'server_content_end', 'before');
+
 add_handler('home', 'process_wordpress_authorization', true, 'wordpress', 'load_user_data', 'after');
 
 add_handler('ajax_wordpess_notifications', 'login', false, 'core');
 add_handler('ajax_wordpess_notifications', 'load_user_data', true, 'core');
 add_handler('ajax_wordpess_notifications', 'language', true, 'core');
-add_handler('ajax_wordpess_notifications', 'message_list_type', true, 'core');
+add_handler('ajax_wordpess_notifications', 'wordpress_list_type', true, 'core');
 add_handler('ajax_wordpess_notifications', 'wp_notification_data',  true);
 add_handler('ajax_wordpess_notifications', 'date', true, 'core');
 add_handler('ajax_wordpess_notifications', 'http_headers', true, 'core');
 add_output('ajax_wordpess_notifications', 'filter_wp_notification_data', true);
+
+add_handler('ajax_wp_notice_display', 'login', false, 'core');
+add_handler('ajax_wp_notice_display', 'load_user_data', true, 'core');
+add_handler('ajax_wp_notice_display', 'language', true, 'core');
+add_handler('ajax_wp_notice_display', 'get_wp_notice_data', true);
+add_handler('ajax_wp_notice_display', 'close_session_early',  true, 'core');
+add_handler('ajax_wp_notice_display', 'date', true, 'core');
+add_handler('ajax_wp_notice_display', 'http_headers', true, 'core');
+add_output('ajax_wp_notice_display', 'filter_wp_notice_data', true);
+
+add_handler('ajax_wordpess_freshly_pressed', 'login', false, 'core');
+add_handler('ajax_wordpess_freshly_pressed', 'load_user_data', true, 'core');
+add_handler('ajax_wordpess_freshly_pressed', 'language', true, 'core');
+add_handler('ajax_wordpess_freshly_pressed', 'wordpress_list_type', true, 'core');
+add_handler('ajax_wordpess_freshly_pressed', 'wp_freshly_pressed_data',  true);
+add_handler('ajax_wordpess_freshly_pressed', 'date', true, 'core');
+add_handler('ajax_wordpess_freshly_pressed', 'http_headers', true, 'core');
+add_output('ajax_wordpess_freshly_pressed', 'filter_wp_freshly_pressed_data', true);
+
 return array(
     'allowed_pages' => array(
-        'wordpress_notifications',
-        'ajax_wordpess_notifications'
-    )
+        'wordpress',
+        'ajax_wordpess_freshly_pressed',
+        'ajax_wordpess_notifications',
+        'ajax_wp_notice_display',
+    ),
+    'allowed_output' => array(
+        'wp_notice_text' => array(FILTER_UNSAFE_RAW, false),
+        'wp_notice_headers' => array(FILTER_UNSAFE_RAW, false),
+    ),
 );
 
 ?>
