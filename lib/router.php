249fdd5561d21f0ba3a45a8fea7fc9b5d7747ba9
Jason Munro <jason@hastymail.org>
2015-02-28 16:15:16 -0600

    start to break up the router a bit. not final

--- lib/router.php
+++ lib/router.php
@@ -8,11 +8,184 @@
 if (!defined('DEBUG_MODE')) { die(); }
 
 /**
+ * Module management for the page request router
+ */
+trait Hm_Router_Modules {
+
+    /**
+     * Build a list of module properties
+     * @param object $config site config object
+     * @return array list of filters, input, and output modules
+     */
+    protected function process_module_setup($config, $debug_mode) {
+        if ($debug_mode) {
+            return $this->get_debug_modules($config);
+        }
+        else {
+            return $this->get_production_modules($config);
+        }
+    }
+
+    /**
+     * Get module data when in debug mode
+     * @param object $config site config object
+     * @return array list of filters, input, and output modules
+     */
+    protected function get_debug_modules($config) {
+        $filters = array();
+        $filters = array('allowed_output' => array(), 'allowed_get' => array(), 'allowed_cookie' => array(),
+            'allowed_post' => array(), 'allowed_server' => array(), 'allowed_pages' => array());
+        $modules = explode(',', $config->get('modules', ''));
+        foreach ($modules as $name) {
+            if (is_readable(sprintf(APP_PATH."modules/%s/setup.php", $name))) {
+                $filters = Hm_Router::merge_filters($filters, require sprintf(APP_PATH."modules/%s/setup.php", $name));
+            }
+        }
+        $handler_mods = array();
+        $output_mods = array();
+        return array($filters, $handler_mods, $output_mods);
+    }
+
+    /**
+     * Get module data when in production mode
+     * @param object $config site config object
+     * @return array list of filters, input, and output modules
+     */
+    public function get_production_modules($config) {
+        $filters = $config->get('input_filters', array());
+        $handler_mods = $config->get('handler_modules', array());
+        $output_mods = $config->get('output_modules', array());
+        return array($filters, $handler_mods, $output_mods);
+    }
+
+    /**
+     * Return the subset of active modules from a supplied list
+     * @param array $mod_list list of modules
+     * @return array filter list
+     */
+    public function get_active_mods($mod_list) {
+        return array_unique(array_values(array_map(function($v) { return $v[0]; }, $mod_list)));
+    }
+
+    /**
+     * Load modules into a module manager
+     * @param string $class name of the module manager to use
+     * @param array $module_sets list of modules by page
+     * @return void
+     */
+    public function load_modules($class, $module_sets) {
+        foreach ($module_sets as $page => $modlist) {
+            foreach ($modlist as $name => $vals) {
+                if ($this->page == $page) {
+                    $class::add($page, $name, $vals[1], false, 'after', true, $vals[0]);
+                }
+            }
+        }
+        $class::try_queued_modules();
+        $class::process_all_page_queue();
+    }
+
+    /**
+     * Load all module sets and include required modules.php files
+     * @param object $config site config object
+     * @param array $handlers list of handler modules
+     * @param array $output list of output modules
+     * @return void
+     */
+    protected function load_module_sets($config, $handlers=array(), $output=array()) {
+
+        $this->load_modules('Hm_Handler_Modules', $handlers);
+        $this->load_modules('Hm_Output_Modules', $output);
+        $active_mods = array_unique(array_merge($this->get_active_mods(Hm_Output_Modules::get_for_page($this->page)),
+            $this->get_active_mods(Hm_Handler_Modules::get_for_page($this->page))));
+        if (!count($active_mods)) {
+            Hm_Functions::cease('No module assignments found');
+        }
+        $mods = explode(',', $config->get('modules', '')); 
+        $this->load_module_set_files($mods, $active_mods);
+    }
+
+    /**
+     * Load module set definition files
+     * @param array $mods modules to load
+     * @param array $active_mods list of active modules
+     * @return void
+     */
+    public function load_module_set_files($mods, $active_mods) {
+        foreach ($mods as $name) {
+            if (in_array($name, $active_mods, true) && is_readable(sprintf(APP_PATH.'modules/%s/modules.php', $name))) {
+                require sprintf(APP_PATH.'modules/%s/modules.php', $name);
+            }
+        }
+    }
+
+    /**
+     * Process all the data handler modules for this page
+     * @param object $request request details
+     * @param object $session session interface
+     * @param object $config site config
+     * @return array combined handler module output
+     */
+    protected function process_page($request, $session, $config) {
+        $response = array();
+        $handler = new Hm_Request_Handler();
+        $modules = Hm_Handler_Modules::get_for_page($this->page);
+        $response = $handler->process_request($this->page, $request, $session, $config, $modules);
+        return $response;
+    }
+
+    /**
+     * Merge the combined response from the handler modules with some default values
+     * @param array $response combined result of the handler modules
+     * @param object $config site config
+     * @param object $request request details
+     * @param object $session session interface
+     * @return void
+     */
+    protected function merge_response($response, $config, $request, $session) {
+        return array_merge($response, array(
+            'router_page_name'    => $this->page,
+            'router_request_type' => $request->type,
+            'router_sapi_name'    => $request->sapi,
+            'router_format_name'  => $request->format,
+            'router_login_state'  => $session->is_active(),
+            'router_url_path'     => $request->path,
+            'router_module_list'  => $config->get('modules', ''),
+            'router_app_name'     => $config->get('app_name', 'HM3')
+        ));
+    }
+
+    /**
+     * Merge input filters from module sets
+     * @param array $existing already collected filters
+     * @param array $new new filters to merge
+     * @return array merged list
+     */
+    static public function merge_filters($existing, $new) {
+        foreach (array('allowed_output', 'allowed_get', 'allowed_cookie', 'allowed_post', 'allowed_server', 'allowed_pages') as $v) {
+            if (array_key_exists($v, $new)) {
+                if ($v == 'allowed_pages' || $v == 'allowed_output') {
+                    $existing[$v] = array_merge($existing[$v], $new[$v]);
+                }
+                else {
+                    $existing[$v] += $new[$v];
+                }
+            }
+        }
+        return $existing;
+    }
+
+}
+
+/**
  * Page request router. This class ties together everything needed to process
  * a request, initiate a session, run handler modules assigned to the request,
  * and possibly do an HTTP redirect
  */
 class Hm_Router {
+
+    /* module management */
+    use Hm_Router_Modules;
 
     /* request id */
     public $page = 'home';
@@ -55,7 +228,9 @@ class Hm_Router {
         $this->check_for_redirect($request, $session, $response_data);
 
         /* format and output the response data */
-        $this->render_output($this->format_response_content($response_data, $request->allowed_output), $response_data);
+        $formatter = new $response_data['router_format_name']();
+
+        $this->render_output($formatter->format_content($response_data, $request->allowed_output), $response_data);
 
         /* save any cached stuff */
         Hm_Page_Cache::save($session);
@@ -68,17 +243,6 @@ class Hm_Router {
     }
 
     /**
-     * Format result of output modules
-     * @param mixed $response_data data from output modules
-     * @param array $allowed_output filters applied to JSON formatted output
-     * @return mixed formatted content
-     */
-    private function format_response_content($response_data, $allowed_output) {
-        $formatter = new $response_data['router_format_name']();
-        return $formatter->format_content($response_data, $allowed_output);
-    }
-
-    /**
      * Send the formatted content to the user
      * @param mixed $output data to send to the user
      * @param mixed $response_data router details
@@ -87,52 +251,6 @@ class Hm_Router {
     private function render_output($output, $response_data) {
         $renderer = new Hm_Output_HTTP();
         $renderer->send_response($output, $response_data);
-    }
-
-    /**
-     * Build a list of module properties
-     * @param object $config site config object
-     * @return array list of filters, input, and output modules
-     */
-    private function process_module_setup($config, $debug_mode) {
-        if ($debug_mode) {
-            return $this->get_debug_modules($config);
-        }
-        else {
-            return $this->get_production_modules($config);
-        }
-    }
-
-    /**
-     * Get module data when in debug mode
-     * @param object $config site config object
-     * @return array list of filters, input, and output modules
-     */
-    private function get_debug_modules($config) {
-        $filters = array();
-        $filters = array('allowed_output' => array(), 'allowed_get' => array(), 'allowed_cookie' => array(),
-            'allowed_post' => array(), 'allowed_server' => array(), 'allowed_pages' => array());
-        $modules = explode(',', $config->get('modules', ''));
-        foreach ($modules as $name) {
-            if (is_readable(sprintf(APP_PATH."modules/%s/setup.php", $name))) {
-                $filters = Hm_Router::merge_filters($filters, require sprintf(APP_PATH."modules/%s/setup.php", $name));
-            }
-        }
-        $handler_mods = array();
-        $output_mods = array();
-        return array($filters, $handler_mods, $output_mods);
-    }
-
-    /**
-     * Get module data when in production mode
-     * @param object $config site config object
-     * @return array list of filters, input, and output modules
-     */
-    public function get_production_modules($config) {
-        $filters = $config->get('input_filters', array());
-        $handler_mods = $config->get('handler_modules', array());
-        $output_mods = $config->get('output_modules', array());
-        return array($filters, $handler_mods, $output_mods);
     }
 
     /**
@@ -177,67 +295,6 @@ class Hm_Router {
         Hm_Debug::add(sprintf('Using %s with %s', $session_class, $auth_class));
         $session = new $session_class($config, $auth_class);
         return $session;
-    }
-
-    /**
-     * Return the subset of active modules from a supplied list
-     * @param array $mod_list list of modules
-     * @return array filter list
-     */
-    public function get_active_mods($mod_list) {
-        return array_unique(array_values(array_map(function($v) { return $v[0]; }, $mod_list)));
-    }
-
-    /**
-     * Load modules into a module manager
-     * @param string $class name of the module manager to use
-     * @param array $module_sets list of modules by page
-     * @return void
-     */
-    public function load_modules($class, $module_sets) {
-        foreach ($module_sets as $page => $modlist) {
-            foreach ($modlist as $name => $vals) {
-                if ($this->page == $page) {
-                    $class::add($page, $name, $vals[1], false, 'after', true, $vals[0]);
-                }
-            }
-        }
-        $class::try_queued_modules();
-        $class::process_all_page_queue();
-    }
-
-    /**
-     * Load all module sets and include required modules.php files
-     * @param object $config site config object
-     * @param array $handlers list of handler modules
-     * @param array $output list of output modules
-     * @return void
-     */
-    private function load_module_sets($config, $handlers=array(), $output=array()) {
-
-        $this->load_modules('Hm_Handler_Modules', $handlers);
-        $this->load_modules('Hm_Output_Modules', $output);
-        $active_mods = array_unique(array_merge($this->get_active_mods(Hm_Output_Modules::get_for_page($this->page)),
-            $this->get_active_mods(Hm_Handler_Modules::get_for_page($this->page))));
-        if (!count($active_mods)) {
-            Hm_Functions::cease('No module assignments found');
-        }
-        $mods = explode(',', $config->get('modules', '')); 
-        $this->load_module_set_files($mods, $active_mods);
-    }
-
-    /**
-     * Load module set definition files
-     * @param array $mods modules to load
-     * @param array $active_mods list of active modules
-     * @return void
-     */
-    public function load_module_set_files($mods, $active_mods) {
-        foreach ($mods as $name) {
-            if (in_array($name, $active_mods, true) && is_readable(sprintf(APP_PATH.'modules/%s/modules.php', $name))) {
-                require sprintf(APP_PATH.'modules/%s/modules.php', $name);
-            }
-        }
     }
 
     /**
@@ -306,62 +363,6 @@ class Hm_Router {
         else {
             $this->page = 'notfound';
         }
-    }
-
-    /**
-     * Process all the data handler modules for this page
-     * @param object $request request details
-     * @param object $session session interface
-     * @param object $config site config
-     * @return array combined handler module output
-     */
-    private function process_page($request, $session, $config) {
-        $response = array();
-        $handler = new Hm_Request_Handler();
-        $modules = Hm_Handler_Modules::get_for_page($this->page);
-        $response = $handler->process_request($this->page, $request, $session, $config, $modules);
-        return $response;
-    }
-
-    /**
-     * Merge the combined response from the handler modules with some default values
-     * @param array $response combined result of the handler modules
-     * @param object $config site config
-     * @param object $request request details
-     * @param object $session session interface
-     * @return void
-     */
-    private function merge_response($response, $config, $request, $session) {
-        return array_merge($response, array(
-            'router_page_name'    => $this->page,
-            'router_request_type' => $request->type,
-            'router_sapi_name'    => $request->sapi,
-            'router_format_name'  => $request->format,
-            'router_login_state'  => $session->is_active(),
-            'router_url_path'     => $request->path,
-            'router_module_list'  => $config->get('modules', ''),
-            'router_app_name'     => $config->get('app_name', 'HM3')
-        ));
-    }
-
-    /**
-     * Merge input filters from module sets
-     * @param array $existing already collected filters
-     * @param array $new new filters to merge
-     * @return array merged list
-     */
-    static public function merge_filters($existing, $new) {
-        foreach (array('allowed_output', 'allowed_get', 'allowed_cookie', 'allowed_post', 'allowed_server', 'allowed_pages') as $v) {
-            if (array_key_exists($v, $new)) {
-                if ($v == 'allowed_pages' || $v == 'allowed_output') {
-                    $existing[$v] = array_merge($existing[$v], $new[$v]);
-                }
-                else {
-                    $existing[$v] += $new[$v];
-                }
-            }
-        }
-        return $existing;
     }
 
     /**
