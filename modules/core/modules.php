ac502c625b2664fe57e16f4961fdb0c1c0afc5bd
Jason Munro <jason@hastymail.org>
2015-03-04 10:02:55 -0600

    save + logout from the save settings page

--- modules/core/modules.php
+++ modules/core/modules.php
@@ -357,22 +357,40 @@ class Hm_Handler_process_save_form extends Hm_Handler_Module {
      * save any changes since login to permanent storage
      */
     public function process() {
-        list($success, $form) = $this->process_form(array('save_settings_permanently', 'password'));
+        list($success, $form) = $this->process_form(array('password'));
+        $save = false;
+        $logout = false;
         if ($success) {
-            $user = $this->session->get('username', false);
-            $path = $this->config->get('user_settings_dir', false);
+            if (array_key_exists('save_settings_permanently', $this->request->post)) {
+                $save = true;
+            }
+            elseif (array_key_exists('save_settings_permanently_then_logout', $this->request->post)) {
+                $save = true;
+                $logout = true;
+            }
+            if ($save) {
+                $user = $this->session->get('username', false);
+                $path = $this->config->get('user_settings_dir', false);
 
-            if ($this->session->auth($user, $form['password'])) {
-                $pass = $form['password'];
-            }
-            else {
-                Hm_Msgs::add('ERRIncorrect password, could not save settings to the server');
-                $pass = false;
-            }
-            if ($user && $path && $pass) {
-                $this->user_config->save($user, $pass);
-                $this->session->set('changed_settings', array());
-                Hm_Msgs::add('Settings saved');
+                if ($this->session->auth($user, $form['password'])) {
+                    $pass = $form['password'];
+                }
+                else {
+                    Hm_Msgs::add('ERRIncorrect password, could not save settings to the server');
+                    $pass = false;
+                }
+                if ($user && $path && $pass) {
+                    $this->user_config->save($user, $pass);
+                    $this->session->set('changed_settings', array());
+                    if ($logout) {
+                        $this->session->destroy($this->request);
+                        Hm_Msgs::add('Saved user data on logout');
+                        Hm_Msgs::add('Session destroyed on logout');
+                    }
+                    else {
+                        Hm_Msgs::add('Settings saved');
+                    }
+                }
             }
         }
     }
