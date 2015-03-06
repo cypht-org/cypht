fc3a9f7b7413a4fb222462316826dc2920059d98
Jason Munro <jason@hastymail.org>
2015-03-06 13:53:55 -0600

    split out service defs

--- modules/nux/modules.php
+++ modules/nux/modules.php
@@ -5,11 +5,11 @@
  * @package modules
  * @subpackage nux
  * @todo filter/disable features depending on imap/pop3 module sets
- * @todo add SMTP support to provider defs
- * @todo homepage dialog
  */
 
 if (!defined('DEBUG_MODE')) { die(); }
+
+require APP_PATH.'modules/nux/services.php';
 
 /**
  * @subpackage nux/handler
@@ -440,145 +440,5 @@ class Nux_Quick_Services {
         return array();
     }
 }
-
-/**
- * todo move the urls to the oauth2 file
- */
-Nux_Quick_Services::add('gmail', array(
-    'server' => 'imap.gmail.com',
-    'type' => 'imap',
-    'tls' => true,
-    'port' => 993,
-    'name' => 'Gmail',
-    'auth' => 'oauth2',
-    'scope' => ' https://mail.google.com/',
-    'smtp' => array(
-        'server' => 'smtp.gmail.com',
-        'port' => 465,
-        'tls' => true
-    )
-));
-
-Nux_Quick_Services::add('outlook', array(
-    'server' => 'imap.live.com',
-    'type' => 'imap',
-    'tls' => true,
-    'port' => 993,
-    'name' => 'Outlook',
-    'auth' => 'oauth2',
-    'scope' => 'wl.imap',
-));
-
-Nux_Quick_Services::add('yahoo', array(
-    'server' => 'imap.mail.yahoo.com',
-    'type' => 'imap',
-    'tls' => true,
-    'port' => 993,
-    'name' => 'Yahoo',
-    'auth' => 'login',
-    'smtp' => array(
-        'server' => 'smtp.mail.yahoo.com',
-        'port' => 587,
-        'tls' => true
-    )
-));
-
-Nux_Quick_Services::add('mailcom', array(
-    'server' => 'imap.mail.com',
-    'type' => 'imap',
-    'tls' => true,
-    'port' => 993,
-    'name' => 'Mail.com',
-    'auth' => 'login',
-    'smtp' => array(
-        'server' => 'smtp.mail.com',
-        'port' => 587,
-        'tls' => true
-    )
-));
-
-Nux_Quick_Services::add('aol', array(
-    'server' => 'imap.aol.com',
-    'type' => 'imap',
-    'tls' => true,
-    'port' => 993,
-    'name' => 'AOL',
-    'auth' => 'login',
-    'smtp' => array(
-        'server' => 'smtp.aol.com',
-        'port' => 587,
-        'tls' => true
-    )
-));
-
-Nux_Quick_Services::add('gmx', array(
-    'server' => 'imap.gmx.com',
-    'type' => 'imap',
-    'tls' => true,
-    'port' => 143,
-    'name' => 'GMX',
-    'auth' => 'login',
-    'smtp' => array(
-        'server' => 'smtp.gmx.com',
-        'port' => 465,
-        'tls' => true
-    )
-));
-
-Nux_Quick_Services::add('zoho', array(
-    'server' => 'imap.zoho.com',
-    'type' => 'imap',
-    'tls' => true,
-    'port' => 993,
-    'name' => 'Zoho',
-    'auth' => 'login',
-    'smtp' => array(
-        'server' => 'smtp.zoho.com',
-        'port' => 465,
-        'tls' => true
-    )
-));
-
-Nux_Quick_Services::add('fastmail', array(
-    'server' => 'mail.messagingengine.com',
-    'type' => 'imap',
-    'tls' => true,
-    'port' => 993,
-    'name' => 'Fastmail',
-    'auth' => 'login',
-    'smtp' => array(
-        'server' => 'mail.messagingengine.com',
-        'port' => 465,
-        'tls' => true
-    )
-));
-
-Nux_Quick_Services::add('yandex', array(
-    'server' => 'imap.yandex.com',
-    'type' => 'imap',
-    'tls' => true,
-    'port' => 993,
-    'name' => 'Yandex',
-    'auth' => 'login',
-    'smtp' => array(
-        'server' => 'smtp.yandex.com',
-        'port' => 465,
-        'tls' => true
-    )
-));
-
-Nux_Quick_Services::add('inbox', array(
-    'server' => 'imap.inbox.com',
-    'type' => 'imap',
-    'tls' => true,
-    'port' => 993,
-    'name' => 'Inbox.com',
-    'auth' => 'login',
-    'smtp' => array(
-        'server' => 'my.inbox.com',
-        'port' => 465,
-        'tls' => true
-    )
-));
 
 ?>
