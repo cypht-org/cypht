7dc06bde1ddeeba9a046a23a537fc13a16f1e462
Jason Munro <jason@hastymail.org>
2015-03-26 18:15:24 -0500

    typo fixes

--- modules/core/site.js
+++ modules/core/site.js
@@ -91,7 +91,7 @@ var Hm_Ajax_Request = function() { return {
     },
 
     fail: function() {
-        setTimeout(function() { Hm_Notices.show({0: 'An error occured communicating with the server'}); }, 1000);
+        setTimeout(function() { Hm_Notices.show({0: 'An error occurred communicating with the server'}); }, 1000);
     },
 
     always: function(res) {
