57b5aa96022432ccb3d24647acb9d1c6dc694abd
Jason Munro <jason@hastymail.org>
2015-04-29 13:57:02 -0500

    update unit tests for new uid cache trait

--- tests/cache.php
+++ tests/cache.php
@@ -99,53 +99,54 @@ class Hm_Test_Uid_Cache extends PHPUnit_Framework_TestCase {
 
     public function setUp() {
         require 'bootstrap.php';
-        Test_Uid_Cache::load(array('foo', 'bar'));
+        Test_Uid_Cache::load(array(array('foo', 'bar'),array()));
     }
     /**
      * @preserveGlobalState disabled
      * @runInSeparateProcess
      */
-    public function test_uid_is_present() {
-        $this->assertTrue(Test_Uid_Cache::is_present('foo'));
-        $this->assertTrue(Test_Uid_Cache::is_present('bar'));
+    public function test_uid_is_read() {
+        $this->assertTrue(Test_Uid_Cache::is_read('foo'));
+        $this->assertTrue(Test_Uid_Cache::is_read('bar'));
+    }
+    /**
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_uid_is_unread() {
+        $this->assertFalse(Test_Uid_Cache::is_unread('foo'));
+        Test_Uid_Cache::unread('bar');
+        $this->assertTrue(Test_Uid_Cache::is_unread('bar'));
+        $this->assertFalse(Test_Uid_Cache::is_read('bar'));
     }
     /**
      * @preserveGlobalState disabled
      * @runInSeparateProcess
      */
     public function test_uid_load() {
-        Test_Uid_Cache::load(array());
-        $this->assertFalse(Test_Uid_Cache::is_present('foobar'));
-        Test_Uid_Cache::load(array('foobar'));
-        $this->assertTrue(Test_Uid_Cache::is_present('foobar'));
+        Test_Uid_Cache::load(array(array(),array()));
+        $this->assertFalse(Test_Uid_Cache::is_read('foobar'));
+        Test_Uid_Cache::load(array(array('foobar'), array('foobar')));
+        $this->assertTrue(Test_Uid_Cache::is_read('foobar'));
     }
     /**
      * @preserveGlobalState disabled
      * @runInSeparateProcess
      */
     public function test_uid_dump() {
-        $this->assertEquals(array('foo', 'bar'), Test_Uid_Cache::dump());
+        $this->assertEquals(array(array('foo', 'bar'),array()), Test_Uid_Cache::dump());
     }
     /**
      * @preserveGlobalState disabled
      * @runInSeparateProcess
      */
-    public function test_uid_add() {
-        $this->assertEquals(false, Test_Uid_Cache::is_present('baz'));
-        Test_Uid_Cache::add('baz');
-        $this->assertTrue(Test_Uid_Cache::is_present('baz'));
-    }
-    /**
-     * @preserveGlobalState disabled
-     * @runInSeparateProcess
-     */
-    public function test_uid_remove() {
-        $this->assertTrue(Test_Uid_Cache::is_present('bar'));
-        Test_Uid_Cache::remove('bar');
-        $this->assertEquals(false, Test_Uid_Cache::is_present('bar'));
+    public function test_uid_read() {
+        $this->assertEquals(false, Test_Uid_Cache::is_read('baz'));
+        Test_Uid_Cache::unread('baz');
+        Test_Uid_Cache::read('baz');
+        $this->assertTrue(Test_Uid_Cache::is_read('baz'));
     }
     public function tearDown() {
-        Test_Uid_Cache::remove('foo');
-        Test_Uid_Cache::remove('bar');
+        Test_Uid_Cache::load(array(),array());
     }
 }
