aad4ec5af8c0bba0ff3c6eea67becc71f33ab48a
Jason Munro <jason@hastymail.org>
2015-05-04 12:43:35 -0500

    unit test update

--- tests/auth.php
+++ tests/auth.php
@@ -1,5 +1,15 @@
 <?php
 
+class Hm_IMAP {
+    private $connected = false;
+    public function get_state() { return $this->connected ? 'authenticated' : false; }
+    public function connect() { $this->connected = true; return true; }
+}
+
+class Hm_POP3 {
+    public function connect() { return true; }
+    public function auth() { return true; }
+}
 
 class Hm_Test_Auth extends PHPUnit_Framework_TestCase {
 
@@ -24,12 +34,36 @@ class Hm_Test_Auth extends PHPUnit_Framework_TestCase {
      * @runInSeparateProcess
      */
     public function test_check_credentials() {
+
+
+        $session = new Hm_Mock_Session();
         $auth = new Hm_Auth_DB($this->config);
         $this->assertFalse($auth->check_credentials('unittestuser', 'notthepass'));
         $this->assertTrue($auth->check_credentials('unittestuser', 'unittestpass'));
+
         $auth = new Hm_Auth_None($this->config);
         $this->assertTrue($auth->check_credentials('any', 'thing'));
         $this->assertTrue($auth->create('any', 'thing'));
+
+        $auth = new Hm_Auth_IMAP($this->config);
+        $this->assertFalse($auth->check_credentials('any', 'thing'));
+        $auth = new Hm_Auth_POP3($this->config);
+        $this->assertFalse($auth->check_credentials('any', 'thing'));
+
+
+        $this->config->set('imap_auth_server', 'test');
+        $this->config->set('imap_auth_port', 123);
+        $this->config->set('imap_auth_tls', false);
+        $auth = new Hm_Auth_IMAP($this->config);
+        $auth->save_auth_detail($session);
+        $this->assertTrue($auth->check_credentials('any', 'thing'));
+
+        $this->config->set('pop3_auth_server', 'test');
+        $this->config->set('pop3_auth_port', 123);
+        $this->config->set('pop3_auth_tls', false);
+        $auth = new Hm_Auth_POP3($this->config);
+        $auth->save_auth_detail($session);
+        $this->assertTrue($auth->check_credentials('any', 'thing'));
     }
     /**
      * @preserveGlobalState disabled
