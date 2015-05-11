4a903604bec94ec0ffe759f1e211ea93775cf457
Jason Munro <jason@hastymail.org>
2015-05-11 15:45:35 -0500

    fix missing include

--- modules/core/functions.php
+++ modules/core/functions.php
@@ -312,7 +312,7 @@ function format_msg_html($str, $external_resources=false) {
  * @return string
  */
 function convert_html_to_text($html) {
-    require 'third_party/Html2Text.php';
+    require APP_PATH.'third_party/Html2Text.php';
     $html = new \Html2Text\Html2Text($html, array('do_links' => 'table', 'width' => 0));
     return $html->getText();
 }
