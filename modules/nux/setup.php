303ed1ffcac31a4c632d3fbf0c5fff842e0ab038
Jason Munro <jason@hastymail.org>
2015-05-22 17:05:57 -0500

    nux fixes for smtp servers

--- modules/nux/setup.php
+++ modules/nux/setup.php
@@ -30,12 +30,14 @@ add_handler('ajax_nux_add_service', 'date', true, 'core');
 add_handler('ajax_nux_add_service', 'http_headers', true, 'core');
 
 add_handler('home', 'load_imap_servers_from_config', true, 'imap', 'load_user_data', 'after');
-add_handler('home', 'load_pop3_servers_from_config', true, 'pop3', 'load_imap_servers_from_config', 'after');
-add_handler('home', 'load_smtp_servers_from_config', true, 'smtp', 'load_imap_servers_from_config', 'after');
-add_handler('home', 'load_feeds_from_config', true, 'feeds', 'load_imap_servers_from_config', 'after');
-add_handler('home', 'process_oauth2_authorization', true, 'nux', 'load_feeds_from_config', 'after');
-add_handler('home', 'nux_homepage_data', true, 'nux', 'load_smtp_servers_from_config', 'after');
+add_handler('home', 'load_pop3_servers_from_config', true, 'pop3', 'load_user_data', 'after');
+add_handler('home', 'load_smtp_servers_from_config', true, 'smtp', 'load_user_data', 'after');
+add_handler('home', 'load_feeds_from_config', true, 'feeds', 'load_user_data', 'after');
+
+add_handler('home', 'process_oauth2_authorization', true, 'nux', 'message_list_type', 'after');
+add_handler('home', 'nux_homepage_data', true, 'nux', 'process_oauth2_authorization', 'after');
 add_handler('home', 'nux_dev_news', true, 'nux', 'nux_homepage_data', 'after');
+
 add_output('home', 'welcome_dialog', true, 'nux', 'home_heading', 'after');
 add_output('home', 'nux_help', true, 'nux', 'welcome_dialog', 'after');
 add_output('home', 'nux_dev_news', true, 'nux', 'nux_help', 'after');
