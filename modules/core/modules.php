8929d7d7042075a1433c608df3afd920877a00f3
Jason Munro <jason@hastymail.org>
2015-06-23 21:19:00 -0500

    more reshuffling

--- modules/core/modules.php
+++ modules/core/modules.php
@@ -12,8 +12,9 @@ define('MAX_PER_SOURCE', 100);
 define('DEFAULT_PER_SOURCE', 20);
 define('DEFAULT_SINCE', '-1 week');
 
-require APP_PATH.'modules/core/message_list_functions.php';
 require APP_PATH.'modules/core/functions.php';
+require APP_PATH.'modules/core/message_functions.php';
+require APP_PATH.'modules/core/message_list_functions.php';
 
 /**
  * Close the session before it's automatically closed at the end of page processing
