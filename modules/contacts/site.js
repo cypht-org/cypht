3898bd76b7473b61fd8604e8c62dea2ebd2697fa
Jason Munro <jason@hastymail.org>
2015-07-24 15:07:42 -0500

    add js for contacts

---
+++ modules/contacts/site.js
@@ -0,0 +1,23 @@
+var delete_contact = function(id) {
+    Hm_Ajax.request(
+        [{'name': 'hm_ajax_hook', 'value': 'ajax_delete_contact'},
+        {'name': 'contact_id', 'value': id}],
+        function(res) { $('.contact_row_'+id).remove(); }
+    );
+};
+
+var add_contact_from_message_view = function() {
+    var contact = $('#add_contact').val();
+    if (contact) {
+        Hm_Ajax.request(
+            [{'name': 'hm_ajax_hook', 'value': 'ajax_add_contact'},
+            {'name': 'contact_value', 'value': contact}]
+        );
+    }
+};
+
+if (hm_page_name() == 'contacts') {
+    $('.delete_contact').click(function() {
+        delete_contact($(this).data('id'));
+    });
+}
