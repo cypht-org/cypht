8ead411e0cd048b723de88c649b7470c83c94569
Jason Munro <jason@hastymail.org>
2015-09-10 12:14:00 -0500

    add gmail contacts to the contacts page

--- modules/contacts/setup.php
+++ modules/contacts/setup.php
@@ -10,11 +10,13 @@ output_source('contacts');
 setup_base_page('contacts', 'core');
 
 add_handler('contacts', 'load_contacts', true, 'contacts', 'load_user_data', 'after');
+add_handler('contacts', 'load_gmail_contacts', true, 'contacts', 'load_contacts', 'after');
 add_handler('contacts', 'process_add_contact', true, 'contacts', 'load_contacts', 'after');
 add_handler('contacts', 'process_edit_contact', true, 'contacts', 'load_contacts', 'after');
 add_output('contacts', 'contacts_content_start', true, 'contacts', 'content_section_start', 'after');
 add_output('contacts', 'contacts_content_add_form', true, 'contacts', 'contacts_content_start', 'after');
 add_output('contacts', 'contacts_list', true, 'contacts', 'contacts_content_add_form', 'after');
+add_output('contacts', 'gmail_contacts_list', true, 'contacts', 'contacts_list', 'after');
 add_output('contacts', 'contacts_content_end', true, 'contacts', 'contacts_content_add_form', 'after');
 
 add_output('ajax_hm_folders', 'contacts_page_link', true, 'contacts', 'settings_menu_end', 'before');
