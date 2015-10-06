c1241f1479906924f147d0051709a7effb81edbf
Jason Munro <jason@hastymail.org>
2015-10-06 12:08:51 -0500

    fix for recent message list row class changes

--- modules/core/site.js
+++ modules/core/site.js
@@ -899,6 +899,9 @@ var Hm_Utils = {
         if (!path_type) {
             path_type = Hm_Utils.get_path_type(path);
         }
+        if (path.indexOf(' ') != -1) {
+            path = path.split(' ')[0];
+        }
         var type = false;
         var server_id = false;
         var uid = false;
