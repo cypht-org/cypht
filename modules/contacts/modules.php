c5b4929fdbcbb3e3e67fa235fb434f071def84f4
Jason Munro <jason@hastymail.org>
2015-12-30 09:55:23 -0600

    label fix

--- modules/contacts/modules.php
+++ modules/contacts/modules.php
@@ -235,7 +235,7 @@ class Hm_Output_contacts_list extends Hm_Output_Module {
         $per_page = 25;
         $current_page = $this->get('contact_page', 1);
         $res = '<table class="contact_list">';
-        $res .= '<tr><td colspan="5" class="contact_list_title"><div class="server_title">'.$this->trans('Local Contacts').'</div></td></tr>';
+        $res .= '<tr><td colspan="5" class="contact_list_title"><div class="server_title">'.$this->trans('Contacts').'</div></td></tr>';
         $contacts = $this->get('contact_store');
         if ($contacts) {
             $total = count($contacts->dump());
