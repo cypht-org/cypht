f2c9420b2f91cac72188ee24cfedc2a0eea9c56c
Jason Munro <jason@hastymail.org>
2016-01-06 16:45:47 -0600

    unit test update for sqlite connection support

--- tests/db.php
+++ tests/db.php
@@ -13,6 +13,18 @@ class Hm_Test_DB extends PHPUnit_Framework_TestCase {
      * @preserveGlobalState disabled
      * @runInSeparateProcess
      */
+    public function test_build_dsn() {
+        setup_db($this->config);
+        $this->assertEquals('object', gettype(Hm_DB::connect($this->config)));
+        $this->assertEquals('mysql:host=127.0.0.1;dbname=test', Hm_DB::build_dsn());
+        $this->config->data['db_driver'] = 'sqlite';
+        $this->assertEquals('boolean', gettype(Hm_DB::connect($this->config)));
+        $this->assertEquals('sqlite:127.0.0.1', Hm_DB::build_dsn());
+    }
+    /**
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
     public function test_connect() {
         $this->assertFalse(Hm_DB::connect($this->config));
         setup_db($this->config);
