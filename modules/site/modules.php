ec09121999eb3c0ce6d2aaec4c332dc532ad5938
Jason Munro <jason@hastymail.org>
2016-01-25 12:40:19 -0600

    better site module set examples

--- modules/site/modules.php
+++ modules/site/modules.php
@@ -2,13 +2,16 @@
 
 if (!defined('DEBUG_MODE')) { die(); }
 
-/*
- * TODO: come up with new example code
- *  class Hm_Handler_site_http_headers extends Hm_Handler_Module {
- *      public function process() {
- *
- *      }
- *  }
- */
 
+class Hm_Handler_site_http_headers extends Hm_Handler_Module {
+    public function process() {
+        /* output custom headers here */
+    }
+}
+
+class Hm_Handler_disable_servers_page extends Hm_Handler_Module {
+    public function process() {
+        Hm_Dispatch::page_redirect('?page=home');
+    }
+}
 
