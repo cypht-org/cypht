9b5f7a0608cabb071f8d0143fd58c215ee60914f
Jason Munro <jason@hastymail.org>
2016-01-25 12:40:05 -0600

    better site module set examples

--- modules/site/setup.php
+++ modules/site/setup.php
@@ -9,7 +9,13 @@ output_source('site');
 //replace_module('handler', 'http_headers', 'site_http_headers', 'home');
 
 /* replace module on all pages */
-//replace_module('handler', 'http_headers', 'site_http_headers', 'home');
+//replace_module('handler', 'http_headers', 'site_http_headers');
+
+/* disable the "servers" link in the settings section of the folder list */
+//replace_module('output', 'settings_servers_link', false, 'ajax_hm_folders');
+
+/* redirect request to the servers page to the home page instead */
+//add_handler('servers', 'disable_servers_page', true, 'site', 'login', 'after');
 
 /* allowed input */
 return array(
