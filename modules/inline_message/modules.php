eb271e0425e749d611c1809d6ba5df36cbde29bd
Jason Munro <jason@hastymail.org>
2016-02-16 12:11:26 -0600

    new module set to open messages inline. imap and rss only for now

---
+++ modules/inline_message/modules.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * inline message modules
+ * @package modules
+ * @subpackage inline_message
+ */
+
+if (!defined('DEBUG_MODE')) { die(); }
+
+/**
+ * @subpackage inline_message/handler
+ */
+class Hm_Handler_get_inline_message_setting extends Hm_Handler_Module {
+    public function process() {
+        $this->out('inline_message_setting', $this->user_config->get('inline_message_setting', 0));
+    }
+}
+
+/**
+ * @subpackage inline_message/handler
+ */
+class Hm_Handler_process_inline_message_setting extends Hm_Handler_Module {
+    public function process() {
+        function inline_message_callback($val) { return $val; }
+        process_site_setting('inline_message', $this, 'inline_message_callback', false, true);
+    }
+}
+
+
+/**
+ * @subpackage inline_message/output
+ */
+class Hm_Output_inline_message_flag extends Hm_Output_Module {
+    protected function output() {
+        return '<script type="text/javascript">var inline_msg = function() { return '.
+            ($this->get('inline_message_setting', 0) ? 'true' : 'false').
+            ';};</script>';
+    }
+}
+
+/**
+ * @subpackage inline_message/output
+ */
+class Hm_Output_inline_message_setting extends Hm_Output_Module {
+    protected function output() {
+        $inline = false;
+        $settings = $this->get('user_settings', array());
+        if (array_key_exists('inline_message', $settings)) {
+            $inline = $settings['inline_message'];
+        }
+        $res = '<tr class="general_setting"><td>'.$this->trans('Show messages inline').'</td><td><input value="1" type="checkbox" name="inline_message"';
+        if ($inline) {
+            $res .= ' checked="checked"';
+        }
+        $res .= '></td></tr>';
+        return $res;
+    }
+}
+
---
+++ modules/inline_message/setup.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!defined('DEBUG_MODE')) { die(); }
+
+handler_source('inline_message');
+output_source('inline_message');
+
+add_handler('settings', 'process_inline_message_setting', true, 'inline_message', 'save_user_settings', 'before');
+add_output('settings', 'inline_message_setting', true, 'inline_message', 'start_general_settings', 'after');
+
+add_handler('message_list', 'get_inline_message_setting', true, 'inline_message', 'load_user_data', 'after');
+add_handler('search', 'get_inline_message_setting', true, 'inline_message', 'load_user_data', 'after');
+add_output('message_list', 'inline_message_flag', true, 'inline_message', 'header_start', 'after');
+add_output('search', 'inline_message_flag', true, 'inline_message', 'header_start', 'after');
+
+return array(
+    'allowed_pages' => array(),
+    'allowed_output' => array(),
+    'allowed_get' => array(
+        'profile_id' => FILTER_VALIDATE_INT,
+    ),
+    'allowed_post' => array(
+        'inline_message' => FILTER_VALIDATE_INT
+    )
+);
