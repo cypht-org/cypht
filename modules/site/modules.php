83df94d6a3c65a404ddb5cb4dbbd1cd81d3215cc
Jason Munro <jason@hastymail.org>
2016-02-16 12:33:04 -0600

    comment update for the site module set

--- modules/site/modules.php
+++ modules/site/modules.php
@@ -1,14 +1,25 @@
 <?php
 
+/**
+ * example site modules
+ * @package modules
+ * @subpackage site
+ */
+
 if (!defined('DEBUG_MODE')) { die(); }
 
-
+/**
+ * @subpackage site/handler
+ */
 class Hm_Handler_site_http_headers extends Hm_Handler_Module {
     public function process() {
         /* output custom headers here */
     }
 }
 
+/**
+ * @subpackage site/handler
+ */
 class Hm_Handler_disable_servers_page extends Hm_Handler_Module {
     public function process() {
         Hm_Dispatch::page_redirect('?page=home');
