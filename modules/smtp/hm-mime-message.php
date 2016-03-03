7f7fbda0811db25b56132e0ec6b5cf2f0a397665
Jason Munro <jason@hastymail.org>
2016-03-03 11:59:22 -0600

    improve the MIME message class to set the reply-to and From display name

--- modules/smtp/hm-mime-message.php
+++ modules/smtp/hm-mime-message.php
@@ -21,16 +21,26 @@ class Hm_MIME_Msg {
     private $allow_unqualified_addresses = false;
 
     /* build mime message data */
-    function __construct($to, $subject, $body, $from, $html=false, $cc='', $bcc='', $reply_to_id='') {
+    function __construct($to, $subject, $body, $from, $html=false, $cc='', $bcc='', $in_reply_to_id='', $from_name='', $reply_to='') {
         if ($cc) {
             $this->headers['Cc'] = $cc;
         }
-        if ($reply_to_id) {
-            $this->headers['In-Reply-To'] = $reply_to_id;
+        if ($in_reply_to_id) {
+            $this->headers['In-Reply-To'] = $in_reply_to_id;
         }
         $this->bcc = $bcc;
-        $this->headers['From'] = $from;
-        $this->headers['Reply-To'] = $from;
+        if ($from_name) {
+            $this->headers['From'] = sprintf('"%s" <%s>', $from_name, $from);
+        }
+        else {
+            $this->headers['From'] = $from;
+        }
+        if ($reply_to) {
+            $this->headers['Reply-To'] = $reply_to;
+        }
+        else {
+            $this->headers['Reply-To'] = $from;
+        }
         $this->headers['To'] = $this->encode_header_fld($to);
         $this->headers['Subject'] = $this->encode_header_fld($subject);
         $this->headers['Date'] = date('r');
