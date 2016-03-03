a76653780415abeb8734cc2662600338facfe150
Jason Munro <jason@hastymail.org>
2016-03-03 12:44:53 -0600

    oops, add missing profile js file

---
+++ modules/profiles/site.js
@@ -0,0 +1,31 @@
+if (hm_page_name() == 'compose') {
+    $('.compose_sign').click(function() {
+        var server_id = $('.compose_server').val();
+        if (profile_signatures[server_id]) {
+            var ta = $('.ke-content', $('iframe').contents());
+            if (ta.length) {
+                ta.html(ta.html() + profile_signatures[server_id].replace(/\n/g, '<br />'));
+            }
+            else {
+                ta = $('#compose_body');
+                insert_sig(ta[0], profile_signatures[server_id]);
+            }
+        }
+    });
+}
+
+var insert_sig = function(textarea, sig) {
+    if (document.selection) {
+        textarea.focus();
+        sel = document.selection.createRange();
+        sel.text = sig;
+    }
+    else if (textarea.selectionStart || textarea.selectionStart == '0') {
+        var startPos = textarea.selectionStart;
+        var endPos = textarea.selectionEnd;
+        textarea.value = textarea.value.substring(0, startPos) + sig + textarea.value.substring(endPos, textarea.value.length);
+    }
+    else {
+        textarea.value += textarea;
+    }
+};
