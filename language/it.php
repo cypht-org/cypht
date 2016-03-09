81bd129b4ae7b7be3768101416adddfcb49ff90d
Jason Munro <jason@hastymail.org>
2016-03-09 14:46:06 -0600

    more language cleanup

--- language/it.php
+++ language/it.php
@@ -164,6 +164,7 @@ return array(
     'Poison Mist' => 'Veleno Mist',
     'A Bunch Of Browns' => 'Un Mazzo Di Browns',
     'VT100' => 'VT100',
+    'Hacker News' => false,
     'Calendar' => 'Calendario',
     'More info' => 'Maggiori informazioni',
     'Sunday' => 'Domenica',
@@ -321,10 +322,15 @@ return array(
     'Send To' => false,
     'local' => false,
     'Add Local Contact' => false,
-    'Delete search',
-    'Delete saved search',
-    'Save search',
-    'Github repo',
+    'Delete search' => false,
+    'Delete saved search' => false,
+    'Save search' => false,
+    'Github repo' => false,
+    'Github' => false,
+    'Accounts' => false,
+    'NASA' => false,
+    'You have unsaved changes' => false,
+    'APOD' => false,
 );
 
 ?>
