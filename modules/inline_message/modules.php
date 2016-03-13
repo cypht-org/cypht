245a5a3696e3be158f94e7dfb33c01138c25137f
Jason Munro <jason@hastymail.org>
2016-03-13 13:18:02 -0500

    type fixes

--- modules/inline_message/modules.php
+++ modules/inline_message/modules.php
@@ -13,7 +13,7 @@ if (!defined('DEBUG_MODE')) { die(); }
  */
 class Hm_Handler_get_inline_message_setting extends Hm_Handler_Module {
     public function process() {
-        $this->out('inline_message_setting', $this->user_config->get('inline_message_setting', 0));
+        $this->out('inline_message_setting', $this->user_config->get('inline_message_setting', false));
     }
 }
 
@@ -34,7 +34,7 @@ class Hm_Handler_process_inline_message_setting extends Hm_Handler_Module {
 class Hm_Output_inline_message_flag extends Hm_Output_Module {
     protected function output() {
         return '<script type="text/javascript">var inline_msg = function() { return '.
-            ($this->get('inline_message_setting', 0) && !$this->get('is_mobile', false) ? 'true' : 'false').
+            ($this->get('inline_message_setting', false) && !$this->get('is_mobile', false) ? 'true' : 'false').
             ';};</script>';
     }
 }
