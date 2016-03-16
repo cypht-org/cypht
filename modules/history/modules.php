01b18b3700cf2c8157ada96ce974b41ea36d38f1
Jason Munro <jason@hastymail.org>
2016-03-16 14:32:29 -0500

    more history page clean up

--- modules/history/modules.php
+++ modules/history/modules.php
@@ -12,7 +12,8 @@
 class Hm_Output_history_page_link extends Hm_Output_Module {
     protected function output() {
         $res = '<li class="menu_history"><a class="unread_link" href="?page=history">'.
-            '<img class="account_icon" src="'.$this->html_safe(Hm_Image_Sources::$env_closed).'" alt="" width="16" height="16" /> '.$this->trans('History').'</a></li>';
+            '<img class="account_icon" src="'.$this->html_safe(Hm_Image_Sources::$env_closed).
+            '" alt="" width="16" height="16" /> '.$this->trans('History').'</a></li>';
         if ($this->format == 'HTML5') {
             return $res;
         }
@@ -26,7 +27,9 @@ class Hm_Output_history_page_link extends Hm_Output_Module {
 class Hm_Output_history_heading  extends Hm_Output_Module {
     protected function output() {
         $res = '<div class="content_title">'.$this->trans('Message history').'</div>'.
-               '<div class="history_content"><table class="history_links"></table></div>';
+            '<div class="history_content"><table class="message_table"><colgroup><col class="source_col">'.
+            '<col class="from_col"><col class="subject_col"><col class="date_col"><col class="icon_col">'.
+            '</colgroup><thead></thead><tbody></tbody></table></div>';
         return $res;
     }
 }
