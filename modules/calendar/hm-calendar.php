8972f8f2451c836f7df6fbd6f938a37e9cfdbb85
Jason Munro <jason@hastymail.org>
2016-03-20 18:38:19 -0500

    moar calendar event stuff

--- modules/calendar/hm-calendar.php
+++ modules/calendar/hm-calendar.php
@@ -24,6 +24,7 @@ class Hm_Cal_Output {
         $this->output_mod = $output_mod;
         $this->today = date('Y-m-d');
     }
+
     public function output($data, $date, $format) {
         $this->date = $date;
         $this->year = date('Y', strtotime($date));
@@ -38,6 +39,7 @@ class Hm_Cal_Output {
             die('Invalid output format');
         }
     }
+
     private function output_day($day) {
         $res = '<td class="';
         if ($day == $this->today) {
@@ -49,6 +51,7 @@ class Hm_Cal_Output {
         $res .= '">'.date('d', strtotime($day)).'</td>';
         return $res;
     }
+
     private function output_week($week) {
         $res = '';
         if ($this->format == 'week') {
@@ -66,32 +69,7 @@ class Hm_Cal_Output {
         }
         return $res;
     }
-    private function prev_next_week() {
-        return array(sprintf('<a href="?page=calendar&date=%s">&lt;</a>', date('Y-m-d', strtotime("-1 week", strtotime($this->date)))),
-            sprintf('<a href="?page=calendar&date=%s">&gt;</a>', date('Y-m-d', strtotime("+1 week", strtotime($this->date)))));
-    }
-    private function prev_next_month() {
-        return array(sprintf('<a href="?page=calendar&date=%s">&lt;</a>', date('Y-m', strtotime("-1 month", strtotime($this->year.'-'.$this->month)))),
-            sprintf('<a href="?page=calendar&date=%s">&gt;</a>', date('Y-m', strtotime("+1 month", strtotime($this->year.'-'.$this->month)))));
-    }
-    private function prev_next_year() {
-        return array(sprintf('<a href="?page=calendar&date=%s">&lt;</a>', date('Y', strtotime("-1 year", strtotime($this->date)))),
-            sprintf('<a href="?page=calendar&date=%s">&gt;</a>', date('Y', strtotime("+1 year", strtotime($this->date)))));
-    }
-    private function prev_next() {
-        if (method_exists($this, 'prev_next_'.$this->format)) {
-            return $this->{'prev_next_'.$this->format}();
-        }
-        die('invalid format');
-    }
-    private function title() {
-        list($prev, $next) = $this->prev_next();
-        $year = date('Y', strtotime($this->year.'-'.$this->month));
-        $month = date('F', strtotime($this->year.'-'.$this->month));
-        $title = sprintf('%s, %%s', $month);
-        $title = sprintf($this->output_mod->trans($title), $year);
-        return '<div class="month_label">'.$prev.' '.$title.' '.$next.'</div>';
-    }
+
     private function output_month($month) {
         $res = $this->title();
         $res .= '<table class="calendar_month">';
@@ -102,6 +80,7 @@ class Hm_Cal_Output {
         $res .= '</table>';
         return $res;
     }
+
     private function output_year($year) {
         $res = '<div class="calendar_year">';
         foreach ($year as $id => $month) {
@@ -111,6 +90,38 @@ class Hm_Cal_Output {
         $res .= '</div>';
         return $res;
     }
+
+    private function prev_next_week() {
+        return array(sprintf('<a href="?page=calendar&date=%s">&lt;</a>', date('Y-m-d', strtotime("-1 week", strtotime($this->date)))),
+            sprintf('<a href="?page=calendar&date=%s">&gt;</a>', date('Y-m-d', strtotime("+1 week", strtotime($this->date)))));
+    }
+
+    private function prev_next_month() {
+        return array(sprintf('<a href="?page=calendar&date=%s">&lt;</a>', date('Y-m', strtotime("-1 month", strtotime($this->year.'-'.$this->month)))),
+            sprintf('<a href="?page=calendar&date=%s">&gt;</a>', date('Y-m', strtotime("+1 month", strtotime($this->year.'-'.$this->month)))));
+    }
+
+    private function prev_next_year() {
+        return array(sprintf('<a href="?page=calendar&date=%s">&lt;</a>', date('Y', strtotime("-1 year", strtotime($this->date)))),
+            sprintf('<a href="?page=calendar&date=%s">&gt;</a>', date('Y', strtotime("+1 year", strtotime($this->date)))));
+    }
+
+    private function prev_next() {
+        if (method_exists($this, 'prev_next_'.$this->format)) {
+            return $this->{'prev_next_'.$this->format}();
+        }
+        die('invalid format');
+    }
+
+    private function title() {
+        list($prev, $next) = $this->prev_next();
+        $year = date('Y', strtotime($this->year.'-'.$this->month));
+        $month = date('F', strtotime($this->year.'-'.$this->month));
+        $title = sprintf('%s, %%s', $month);
+        $title = sprintf($this->output_mod->trans($title), $year);
+        return '<div class="month_label">'.$prev.' '.$title.' '.$next.'</div>';
+    }
+
     private function output_heading() {
         $days = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
         return '<tr>'.implode('', array_map(function($v) {
@@ -126,6 +137,7 @@ class Hm_Cal_Event_Store {
 
     public function __construct() {
     }
+
     public function load($data) {
         foreach ($data as $event) {
             $event = new Hm_Cal_Event($event);
@@ -134,13 +146,25 @@ class Hm_Cal_Event_Store {
             }
         }
     }
+
     public function import($data) {
     }
+
     public function export() {
     }
+
     public function save($session) {
     }
+
     public function in_date_range($start, $end) {
+        $events = array();
+        foreach ($this->data as $event) {
+            $event_data = $event->in_date_range($start, $end);
+            if (count($event_data)) {
+                $events = array_merge($events, $event_data);
+            }
+        }
+        return $events;
     }
 }
 
@@ -148,12 +172,14 @@ class Hm_Cal_Event_Store {
  * @subpackage calendar/lib
  */
 class Hm_Cal_Event {
+
     private $data = array(
         'title' => NULL,
         'description' => NULL,
         'date' => NULL,
         'repeat_interval' => NULL,
     );
+
     public function __construct($data) {
         if (is_array($data)) {
             foreach ($this->data as $name => $value) {
@@ -163,15 +189,18 @@ class Hm_Cal_Event {
             }
         }
     }
+
     public function is_valid() {
         return $this->get('date');
     }
+
     private function get($name, $default=false) {
         if (array_key_exists($name, $this->data)) {
             return $this->data[$name];
         }
         return $default;
     }
+
     private function check_start_date($start, $end) {
         $ts = false;
         if ($this->get('date')) {
@@ -182,12 +211,14 @@ class Hm_Cal_Event {
         }
         return $ts;
     }
+
     private function fast_forward_repeat_event($ts, $interval, $limit) {
         while ($ts < $limit) {
             $ts = strtotime('+1 '.$interval, $ts);
         }
         return $ts;
     }
+
     private function collect_repeat_events($ts, $interval, $limit) {
         $times = array();
         while ($ts < $limit) {
@@ -196,6 +227,7 @@ class Hm_Cal_Event {
         }
         return $times;
     }
+
     private function check_repeat($start, $end, $ts) {
         $times = array();
         if ($this->get('repeat_interval')) {
@@ -208,11 +240,12 @@ class Hm_Cal_Event {
         }
         return $times;
     }
+
     public function in_date_range($start_date, $end_date) {
         $start = strtotime($start_date);
         $end = strtotime($end_date);
         $times = array();
-
+        $results = array();
         if ($start && $end) {
             $ts = $this->check_start_date($start, $end);
             if ($ts) {
@@ -222,7 +255,21 @@ class Hm_Cal_Event {
                 $times = array_merge($times, $this->check_repeat($start, $end, $ts));
             }
         }
-        return array_unique($times);
+        $times = array_unique($times);
+        return $this->fill_event_details($times);
+    }
+
+    private function fill_event_details($times) {
+        $results = array();
+        foreach ($times as $time) {
+            $result_item = array();
+            foreach ($this->data as $name => $val) {
+                $result_item[$name] = $val;
+            }
+            $result_item['ts'] = $time;
+            $results[] = $result_item;
+        }
+        return $results;
     }
 }
 
@@ -239,12 +286,7 @@ class Hm_Cal_Data {
     public function __construct() {
         $this->today = time();
     }
-    public function output($str_time, $format='month') {
-        $this->check_input_params($str_time, $format);
-        if (method_exists($this, $format)) {
-            return $this->$format();
-        }
-    }
+
     private function check_input_params($str_time, $format) {
         $this->ts = strtotime($str_time);
         if ($this->ts === false || $this->ts === -1) {
@@ -252,6 +294,14 @@ class Hm_Cal_Data {
         }
         $this->set_start_ts($format);
     }
+
+    public function output($str_time, $format='month') {
+        $this->check_input_params($str_time, $format);
+        if (method_exists($this, $format)) {
+            return $this->$format();
+        }
+    }
+
     private function set_start_ts($format) {
         if (method_exists($this, 'start_'.$format)) {
             $this->{'start_'.$format}($this->ts);
@@ -259,26 +309,31 @@ class Hm_Cal_Data {
         else {
             die('Invalid output format');
         }
-    } 
+    }
+
     private function start_week($ts) {
         $offset = date('w', $ts);
         $this->day_pos = strtotime(sprintf('-%d day', $offset), $ts);
     }
+
     private function start_month($ts) {
         $first_day = strtotime(date('Y-m-01', $ts));
         $this->month_pos = (int) date('n', $ts);
         $offset = date('w', $first_day);
         $this->day_pos = strtotime(sprintf('-%d day', $offset), $first_day);
     }
+ 
     private function start_year($ts) {
         $first_day = strtotime(date('Y-01-01', $ts));
         $offset = date('w', $first_day);
         $this->day_pos = strtotime(sprintf('-%d day', $offset), $first_day);
         $this->month_pos = 1;
     }
+
     private function increment_day() {
         $this->day_pos = strtotime('+1 day', $this->day_pos);
     }
+
     private function week() {
         $res = array();
         for ($i = 0; $i < 7; $i++) {
@@ -286,11 +341,13 @@ class Hm_Cal_Data {
         }
         return $res;
     }
+
     private function day() {
         $day = date('Y-m-d', $this->day_pos);
         $this->increment_day();
         return $day;
     }
+
     private function month() {
         $res = array();
         for ($i = 0; $i < 6; $i++) {
@@ -301,6 +358,7 @@ class Hm_Cal_Data {
         }
         return $res;
     }
+
     private function year() {
         $res = array();
         for ($i = 0; $i < 12; $i++) {
