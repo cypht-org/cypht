63b183731bd1598c0cfab625ef31c63bbb7d0f20
Jason Munro <jason@hastymail.org>
2016-03-22 13:39:02 -0500

    more work on calendar events.

--- modules/calendar/site.js
+++ modules/calendar/site.js
@@ -1,5 +1,8 @@
 $(function() {
     if (hm_page_name() == 'calendar') {
+        $('.event_delete').click(function() {
+            $(this).parent().submit();
+        });
         $('.cal_title').click(function() {
             $('.event_details').hide();
             $('.event_details', $(this).parent()).show();
