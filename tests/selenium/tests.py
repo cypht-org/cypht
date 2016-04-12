4c27448178fdb43fdc73bb4ab3ab4714c6ce256e
Jason Munro <jason@hastymail.org>
2016-04-12 11:54:10 -0500

    more selenium test refactoring

--- tests/selenium/tests.py
+++ tests/selenium/tests.py
@@ -6,128 +6,123 @@ from runner import test_runner
 USER = 'testuser'
 PASS = 'testuser'
 
-class Test:
-
-    browser = None
-
-    def __init__(self):
-        self.browser = WebTest()
+class Test(WebTest):
 
     def test_bad_login(self):
-        self.browser.login('asdf', 'asdf')
-        self.browser.rest()
-        assert self.browser.by_class('err').text == 'Invalid username or password'
+        self.login('asdf', 'asdf')
+        self.rest()
+        assert self.by_class('err').text == 'Invalid username or password'
 
     def test_good_login(self):
-        self.browser.load()
-        self.browser.login(USER, PASS)
-        self.browser.rest()
-        assert self.browser.by_class('err') == None
+        self.load()
+        self.login(USER, PASS)
+        self.rest()
+        assert self.by_class('err') == None
 
     def test_folder_list(self):
-        assert self.browser.by_class('main_menu').text == 'Main'
-        self.browser.by_class('update_message_list').click()
-        self.browser.rest()
-        assert self.browser.by_class('main_menu').text == 'Main'
+        assert self.by_class('main_menu').text == 'Main'
+        self.by_class('update_message_list').click()
+        self.rest()
+        assert self.by_class('main_menu').text == 'Main'
 
     def test_search(self):
-        list_item = self.browser.by_class('menu_search')
+        list_item = self.by_class('menu_search')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('content_title').text[0:6] == 'Search'
+        self.rest()
+        assert self.by_class('content_title').text[0:6] == 'Search'
 
     def test_unread(self):
-        list_item = self.browser.by_class('menu_unread')
+        list_item = self.by_class('menu_unread')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('mailbox_list_title').text == 'Unread'
+        self.rest()
+        assert self.by_class('mailbox_list_title').text == 'Unread'
 
     def test_combined_inbox(self):
-        list_item = self.browser.by_class('menu_combined_inbox')
+        list_item = self.by_class('menu_combined_inbox')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('mailbox_list_title').text == 'Everything'
+        self.rest()
+        assert self.by_class('mailbox_list_title').text == 'Everything'
 
     def test_flagged(self):
-        list_item = self.browser.by_class('menu_flagged')
+        list_item = self.by_class('menu_flagged')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('mailbox_list_title').text == 'Flagged'
+        self.rest()
+        assert self.by_class('mailbox_list_title').text == 'Flagged'
 
     def test_contacts(self):
-        list_item = self.browser.by_class('menu_contacts')
+        list_item = self.by_class('menu_contacts')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('content_title').text == 'Contacts'
+        self.rest()
+        assert self.by_class('content_title').text == 'Contacts'
 
     def test_compose(self):
-        list_item = self.browser.by_class('menu_compose')
+        list_item = self.by_class('menu_compose')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('content_title').text == 'Compose'
+        self.rest()
+        assert self.by_class('content_title').text == 'Compose'
 
     def test_calendar(self):
-        list_item = self.browser.by_class('menu_calendar')
+        list_item = self.by_class('menu_calendar')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('content_title').text == 'Calendar'
+        self.rest()
+        assert self.by_class('content_title').text == 'Calendar'
 
     def test_history(self):
-        list_item = self.browser.by_class('menu_history')
+        list_item = self.by_class('menu_history')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('content_title').text == 'Message history'
+        self.rest()
+        assert self.by_class('content_title').text == 'Message history'
 
     def test_home(self):
-        self.browser.by_css('[data-source=".settings"]').click()
-        list_item = self.browser.by_class('menu_home')
+        self.by_css('[data-source=".settings"]').click()
+        list_item = self.by_class('menu_home')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('content_title').text == 'Home'
+        self.rest()
+        assert self.by_class('content_title').text == 'Home'
 
     def test_servers(self):
-        list_item = self.browser.by_class('menu_servers')
+        list_item = self.by_class('menu_servers')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('content_title').text == 'Servers'
+        self.rest()
+        assert self.by_class('content_title').text == 'Servers'
 
     def test_site(self):
-        list_item = self.browser.by_class('menu_settings')
+        list_item = self.by_class('menu_settings')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('content_title').text == 'Site Settings'
+        self.rest()
+        assert self.by_class('content_title').text == 'Site Settings'
 
     def test_general_settings(self):
-        self.browser.by_css('[data-target=".general_setting"]').click()
-        self.browser.by_name('smtp_auto_bcc').click()
-        self.browser.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        self.browser.by_name('save_settings').click()
-        self.browser.rest()
-        self.browser.by_css('[data-source=".settings"]').click()
-        assert self.browser.by_class('sys_messages').text == 'Settings saved'
+        self.by_css('[data-target=".general_setting"]').click()
+        self.by_name('smtp_auto_bcc').click()
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        self.by_name('save_settings').click()
+        self.rest()
+        self.by_css('[data-source=".settings"]').click()
+        assert self.by_class('sys_messages').text == 'Settings saved'
 
     def test_save(self):
-        list_item = self.browser.by_class('menu_save')
+        list_item = self.by_class('menu_save')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('content_title').text == 'Save Settings'
+        self.rest()
+        assert self.by_class('content_title').text == 'Save Settings'
 
     def test_password(self):
-        list_item = self.browser.by_class('menu_change_password')
+        list_item = self.by_class('menu_change_password')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('content_title').text == 'Change Password'
+        self.rest()
+        assert self.by_class('content_title').text == 'Change Password'
 
     def test_profiles(self):
-        list_item = self.browser.by_class('menu_profiles')
+        list_item = self.by_class('menu_profiles')
         list_item.find_element_by_tag_name('a').click()
-        self.browser.rest()
-        assert self.browser.by_class('content_title').text == 'Profiles'
+        self.rest()
+        assert self.by_class('content_title').text == 'Profiles'
 
     def test_logout(self):
-        self.browser.logout()
-        self.browser.rest()
-        assert self.browser.by_class('sys_messages').text == 'Session destroyed on logout'
+        self.logout()
+        self.rest()
+        assert self.by_class('sys_messages').text == 'Session destroyed on logout'
 
 
 if __name__ == '__main__':
