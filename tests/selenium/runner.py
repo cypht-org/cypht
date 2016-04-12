a1c34d354a1fafbd5c18d902b3f375f8e5f339d0
Jason Munro <jason@hastymail.org>
2016-04-12 11:23:01 -0500

    selenium test improvements

--- tests/selenium/runner.py
+++ tests/selenium/runner.py
@@ -7,14 +7,20 @@ RED = '\033[91m'
 END = '\033[0m'
 
 def test_runner(obj, tests):
+    passed = 0
     print
     for name in tests:
         func = getattr(obj, name)
         try:
             func()
-            print name+' '+GREEN+'PASSED'+END
+            print '%s %sPASSED%s' % (name, GREEN, END)
+            passed += 1
         except Exception:
-            print name+' '+RED+'FAILED'+END
+            print '%s %sFAILED%s' % (name, RED, END)
             print exc_info()
+    print
+    print '%s%s of %s PASSED%s' % (GREEN, passed, len(tests), END)
+    if (len(tests) > passed):
+        print '%s%s of %s FAILED%s' % (RED, (len(tests) - passed), len(tests), RED)
     print
     obj.browser.end()
