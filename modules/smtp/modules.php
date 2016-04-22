2bd537c8eac89174b5d7935473b4a21276504bed
Jason Munro <jason@hastymail.org>
2016-04-22 12:34:40 -0500

    add a few icons

--- modules/smtp/modules.php
+++ modules/smtp/modules.php
@@ -488,7 +488,7 @@ class Hm_Output_sent_folder_link extends Hm_Output_Module {
     protected function output() {
         if ($this->get('auto_bcc_enabled', 0)) {
             $res = '<li class="menu_sent"><a class="unread_link" href="?page=message_list&amp;list_path=sent">'.
-                '<img class="account_icon" src="'.$this->html_safe(Hm_Image_Sources::$env_closed).'" alt="" width="16" height="16" /> '.$this->trans('Sent').'</a></li>';
+                '<img class="account_icon" src="'.$this->html_safe(Hm_Image_Sources::$sent).'" alt="" width="16" height="16" /> '.$this->trans('Sent').'</a></li>';
             $this->concat('formatted_folder_list', $res);
         }
     }
