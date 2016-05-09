211b172f3eb1211bc455a3623b2f57862079441e
Jason Munro <jason@hastymail.org>
2016-05-09 13:37:01 -0500

    more prep work toward imap folder functions (create/rename/delete)

--- modules/imap_folders/modules.php
+++ modules/imap_folders/modules.php
@@ -8,10 +8,20 @@
 
 if (!defined('DEBUG_MODE')) { die(); }
 
+
+/**
+ * @subpackage imap_folders/handler
+ */
+class Hm_Handler_imap_folder_check extends Hm_Handler_Module {
+    public function process() {
+        $this->out('imap_support', $this->module_is_supported('imap'));
+    }
+}
+
 /**
  * @subpackage imap_folders/output
  */
-class Hm_Output_folders_content extends Hm_Output_Module {
+class Hm_Output_folders_content_start extends Hm_Output_Module {
     protected function output() {
         $res = '<div class="content_title">'.$this->trans('Folders').'</div>';
         return $res;
@@ -23,12 +33,14 @@ class Hm_Output_folders_content extends Hm_Output_Module {
  */
 class Hm_Output_folders_page_link extends Hm_Output_Module {
     protected function output() {
-        $res = '<li class="menu_folders"><a class="unread_link" href="?page=folders">'.
-            '<img class="account_icon" src="'.$this->html_safe(Hm_Image_Sources::$folder).
-            '" alt="" width="16" height="16" /> '.$this->trans('Folders').'</a></li>';
-        if ($this->format == 'HTML5') {
-            return $res;
+        if ($this->get('imap_support')) {
+            $res = '<li class="menu_folders"><a class="unread_link" href="?page=folders">'.
+                '<img class="account_icon" src="'.$this->html_safe(Hm_Image_Sources::$folder).
+                '" alt="" width="16" height="16" /> '.$this->trans('Folders').'</a></li>';
+            if ($this->format == 'HTML5') {
+                return $res;
+            }
+            $this->concat('formatted_folder_list', $res);
         }
-        $this->concat('formatted_folder_list', $res);
     }
 }
