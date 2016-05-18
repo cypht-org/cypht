60b596d59c373d4a0699d0e9c340c4d80ac4799e
Jason Munro <jason@hastymail.org>
2016-05-18 17:15:33 -0500

    fix missing translation calls for a few strings

--- modules/contacts/modules.php
+++ modules/contacts/modules.php
@@ -248,10 +248,10 @@ class Hm_Output_contacts_list extends Hm_Output_Module {
                     '<td>'.$this->html_safe($contact->value('phone_number')).'</td>'.
                     '<td class="contact_controls">';
                 if (!$contact->value('source')) {
-                    $res .= '<a data-id="'.$this->html_safe($id).'" class="delete_contact" title="Delete"><img alt="'.$this->trans('Delete').'" width="16" height="16" src="'.Hm_Image_Sources::$circle_x.'" /></a>'.
-                        '<a href="?page=contacts&amp;contact_id='.$this->html_safe($id).'&amp;contact_page='.$current_page.'" class="delete_contact" title="Edit"><img alt="'.$this->trans('Edit').'" width="16" height="16" src="'.Hm_Image_Sources::$cog.'" /></a>';
+                    $res .= '<a data-id="'.$this->html_safe($id).'" href="" class="delete_contact" title="'.$this->trans('Delete').'"><img alt="'.$this->trans('Delete').'" width="16" height="16" src="'.Hm_Image_Sources::$circle_x.'" /></a>'.
+                        '<a href="?page=contacts&amp;contact_id='.$this->html_safe($id).'&amp;contact_page='.$current_page.'" class="delete_contact" title="'.$this->trans('Edit').'"><img alt="'.$this->trans('Edit').'" width="16" height="16" src="'.Hm_Image_Sources::$cog.'" /></a>';
                 }
-                $res .= '<a href="?page=compose&amp;contact_id='.$this->html_safe($id).'" class="send_to_contact" title="Send to"><img alt="'.$this->trans('Send To').'" width="16" height="16" src="'.Hm_Image_Sources::$doc.'" /></a>'.
+                $res .= '<a href="?page=compose&amp;contact_id='.$this->html_safe($id).'" class="send_to_contact" title="'.$this->trans('Send To').'"><img alt="'.$this->trans('Send To').'" width="16" height="16" src="'.Hm_Image_Sources::$doc.'" /></a>'.
                     '</td></tr>';
             }
             $res .= '<tr><td class="contact_pages" colspan="5">';
