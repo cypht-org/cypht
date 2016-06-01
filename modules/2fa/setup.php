71b0801b9b58246bbda0da58e650b9813a31d9a3
Jason Munro <jason@hastymail.org>
2016-06-01 00:40:15 -0500

    more work on 2fa module support

--- modules/2fa/setup.php
+++ modules/2fa/setup.php
@@ -11,4 +11,10 @@ output_source('2fa');
 add_module_to_all_pages('handler', '2fa_check', true, '2fa', 'save_user_data', 'after');
 add_module_to_all_pages('output', '2fa_dialog', true, '2fa', 'header_start', 'before');
 
-return array( 'allowed_post' => array( '2fa_code' => FILTER_SANITIZE_STRING));
+add_handler('settings', 'process_enable_2fa', true, '2fa', 'save_user_settings', 'before');
+add_output('settings', 'enable_2fa_setting', true, '2fa', 'start_general_settings', 'after');
+
+return array( 'allowed_post' => array(
+    '2fa_code' => FILTER_SANITIZE_STRING,
+    'enable_2fa' => FILTER_VALIDATE_INT
+));
