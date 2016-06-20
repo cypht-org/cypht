5afb5eaa30f5b7a647faa7b38a87db6797084cf2
Jason Munro <jason@hastymail.org>
2016-06-20 14:29:07 -0500

    move 2fa setting to it's own section and out of the general settings

--- modules/2fa/setup.php
+++ modules/2fa/setup.php
@@ -12,7 +12,7 @@ add_module_to_all_pages('handler', '2fa_check', true, '2fa', 'save_user_data', '
 add_module_to_all_pages('output', '2fa_dialog', true, '2fa', 'header_start', 'before');
 
 add_handler('settings', 'process_enable_2fa', true, '2fa', 'save_user_settings', 'before');
-add_output('settings', 'enable_2fa_setting', true, '2fa', 'start_general_settings', 'after');
+add_output('settings', 'enable_2fa_setting', true, '2fa', 'end_settings_form', 'before');
 
 return array( 'allowed_post' => array(
     '2fa_code' => FILTER_SANITIZE_STRING,
