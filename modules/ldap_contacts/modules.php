3b1ca3224cbf178306b270c3e0350b43b9678aee
Jason Munro <jason@hastymail.org>
2016-07-26 14:25:41 -0500

    add read_write option for the ldap.ini file

--- modules/ldap_contacts/modules.php
+++ modules/ldap_contacts/modules.php
@@ -221,9 +221,11 @@ class Hm_Handler_process_add_ldap_contact extends Hm_Handler_Module {
 class Hm_Handler_load_ldap_contacts extends Hm_Handler_Module {
     public function process() {
         $contacts = $this->get('contact_store');
-        $contacts = fetch_ldap_contacts($this->config, $contacts);
+        list($contacts, $ldap_config) = fetch_ldap_contacts($this->config, $contacts);
         $this->append('contact_sources', 'ldap');
-        $this->append('contact_edit', 'ldap');
+        if (is_array($ldap_config) && array_key_exists('read_write', $ldap_config) && $ldap_config['read_write']) {
+            $this->append('contact_edit', 'ldap');
+        }
         $this->out('contact_store', $contacts, false);
     }
 }
@@ -250,6 +252,9 @@ class Hm_Handler_load_edit_ldap_contact extends Hm_Handler_Module {
  */
 class Hm_Output_ldap_contact_form_end extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         return '</div></form></div>';
     }
 }
@@ -259,6 +264,9 @@ class Hm_Output_ldap_contact_form_end extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_first_name extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $name = get_ldap_value('givenname', $this);
         return '<label class="screen_reader" for="ldap_first_name">'.$this->trans('First Name').'</label>'.
             '<input required placeholder="'.$this->trans('First Name').'" id="ldap_first_name" type="text" name="ldap_first_name" '.
@@ -271,6 +279,9 @@ class Hm_Output_ldap_form_first_name extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_submit extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $label = 'Add';
         $name = 'add_ldap_contact';
         if ($this->get('current_ldap_contact')) {
@@ -287,6 +298,9 @@ class Hm_Output_ldap_form_submit extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_last_name extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $name = get_ldap_value('sn', $this);
         return '<label class="screen_reader" for="ldap_last_name">'.$this->trans('Last Name').'</label>'.
             '<input required placeholder="'.$this->trans('Last Name').'" id="ldap_last_name" type="text" name="ldap_last_name" '.
@@ -299,6 +313,9 @@ class Hm_Output_ldap_form_last_name extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_title extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $title = get_ldap_value('title', $this);
         return '<label class="screen_reader" for="ldap_title">'.$this->trans('Title').'</label>'.
             '<input placeholder="'.$this->trans('Title').'" id="ldap_title" type="text" name="ldap_title" '.
@@ -311,7 +328,9 @@ class Hm_Output_ldap_form_title extends Hm_Output_Module {
  */
 class Hm_Output_ldap_contact_form_start extends Hm_Output_Module {
     protected function output() {
-
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $title = $this->trans('Add LDAP Contact');
         $form_class='contact_form';
         if ($this->get('current_ldap_contact')) {
@@ -332,6 +351,9 @@ class Hm_Output_ldap_contact_form_start extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_displayname extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('displayname', $this);
         return '<label class="screen_reader" for="ldap_displayname">'.$this->trans('Display Name').'</label>'.
             '<input placeholder="'.$this->trans('Display Name').'" id="ldap_displayname" type="text" name="ldap_displayname" '.
@@ -344,6 +366,9 @@ class Hm_Output_ldap_form_displayname extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_mail extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('email_address', $this);
         return '<label class="screen_reader" for="ldap_mail">'.$this->trans('E-mail Address').'</label>'.
             '<input required placeholder="'.$this->trans('E-mail Address').'" id="ldap_mail" type="email" name="ldap_mail" '.
@@ -356,6 +381,9 @@ class Hm_Output_ldap_form_mail extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_phone extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('phone_number', $this);
         return '<label class="screen_reader" for="ldap_phone">'.$this->trans('Telephone Number').'</label>'.
             '<input placeholder="'.$this->trans('Telephone Number').'" id="ldap_phone" type="text" name="ldap_phone" '.
@@ -368,6 +396,9 @@ class Hm_Output_ldap_form_phone extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_fax extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('facsimiletelephonenumber', $this);
         return '<label class="screen_reader" for="ldap_fax">'.$this->trans('Fax Number').'</label>'.
             '<input placeholder="'.$this->trans('Fax Number').'" id="ldap_fax" type="text" name="ldap_fax" '.
@@ -380,6 +411,9 @@ class Hm_Output_ldap_form_fax extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_mobile extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('mobile', $this);
         return '<label class="screen_reader" for="ldap_mobile">'.$this->trans('Mobile Number').'</label>'.
             '<input placeholder="'.$this->trans('Mobile Number').'" id="ldap_mobile" type="text" name="ldap_mobile" '.
@@ -392,6 +426,9 @@ class Hm_Output_ldap_form_mobile extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_room extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('roomnumber', $this);
         return '<label class="screen_reader" for="ldap_room">'.$this->trans('Room Number').'</label>'.
             '<input placeholder="'.$this->trans('Room Number').'" id="ldap_room" type="text" name="ldap_room" '.
@@ -404,6 +441,9 @@ class Hm_Output_ldap_form_room extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_car extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('carlicense', $this);
         return '<label class="screen_reader" for="ldap_car">'.$this->trans('License Plate Number').'</label>'.
             '<input placeholder="'.$this->trans('License Plate Number').'" id="ldap_car" type="text" name="ldap_car" '.
@@ -416,6 +456,9 @@ class Hm_Output_ldap_form_car extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_org extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('o', $this);
         return '<label class="screen_reader" for="ldap_org">'.$this->trans('Organization').'</label>'.
             '<input placeholder="'.$this->trans('Organization').'" id="ldap_org" type="text" name="ldap_org" '.
@@ -428,6 +471,9 @@ class Hm_Output_ldap_form_org extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_org_unit extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('ou', $this);
         return '<label class="screen_reader" for="ldap_org_unit">'.$this->trans('Organization Unit').'</label>'.
             '<input placeholder="'.$this->trans('Organization Unit').'" id="ldap_org_unit" type="text" name="ldap_org_unit" '.
@@ -440,6 +486,9 @@ class Hm_Output_ldap_form_org_unit extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_org_dpt extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('departmentnumber', $this);
         return '<label class="screen_reader" for="ldap_org_dpt">'.$this->trans('Department Number').'</label>'.
             '<input placeholder="'.$this->trans('Department Number').'" id="ldap_org_dpt" type="text" name="ldap_org_dpt" '.
@@ -452,6 +501,9 @@ class Hm_Output_ldap_form_org_dpt extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_emp_num extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('employeenumber', $this);
         return '<label class="screen_reader" for="ldap_emp_num">'.$this->trans('Employee Number').'</label>'.
             '<input placeholder="'.$this->trans('Employee Number').'" id="ldap_emp_num" type="text" name="ldap_emp_num" '.
@@ -464,6 +516,9 @@ class Hm_Output_ldap_form_emp_num extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_emp_type extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('employeetype', $this);
         return '<label class="screen_reader" for="ldap_emp_type">'.$this->trans('Employment Type').'</label>'.
             '<input placeholder="'.$this->trans('Employment Type').'" id="ldap_emp_type" type="text" name="ldap_emp_type" '.
@@ -476,6 +531,9 @@ class Hm_Output_ldap_form_emp_type extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_lang extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('preferredlanguage', $this);
         return '<label class="screen_reader" for="ldap_lang">'.$this->trans('Language').'</label>'.
             '<input placeholder="'.$this->trans('Language').'" id="ldap_lang" type="text" name="ldap_lang" '.
@@ -488,6 +546,9 @@ class Hm_Output_ldap_form_lang extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_uri extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('labeleduri', $this);
         return '<label class="screen_reader" for="ldap_uri">'.$this->trans('Website').'</label>'.
             '<input placeholder="'.$this->trans('Website').'" id="ldap_uri" type="text" name="ldap_uri" '.
@@ -500,6 +561,9 @@ class Hm_Output_ldap_form_uri extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_locality extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('l', $this);
         return '<label class="screen_reader" for="ldap_locality">'.$this->trans('Locality').'</label>'.
             '<input placeholder="'.$this->trans('Locality').'" id="ldap_locality" type="text" name="ldap_locality" '.
@@ -512,6 +576,9 @@ class Hm_Output_ldap_form_locality extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_street extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('street', $this);
         return '<label class="screen_reader" for="ldap_street">'.$this->trans('Street').'</label>'.
             '<input placeholder="'.$this->trans('Street').'" id="ldap_street" type="text" name="ldap_street" '.
@@ -524,6 +591,9 @@ class Hm_Output_ldap_form_street extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_state extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('st', $this);
         return '<label class="screen_reader" for="ldap_state">'.$this->trans('State').'</label>'.
             '<input placeholder="'.$this->trans('State').'" id="ldap_state" type="text" name="ldap_state" '.
@@ -536,6 +606,9 @@ class Hm_Output_ldap_form_state extends Hm_Output_Module {
  */
 class Hm_Output_ldap_form_postalcode extends Hm_Output_Module {
     protected function output() {
+        if (!in_array('ldap', $this->get('contact_edit', array()), true)) {
+            return;
+        }
         $val = get_ldap_value('postalcode', $this);
         return '<label class="screen_reader" for="ldap_postalcode">'.$this->trans('Postal Code').'</label>'.
             '<input placeholder="'.$this->trans('Postal Code').'" id="ldap_postalcode" type="text" name="ldap_postalcode" '.
@@ -574,7 +647,7 @@ function fetch_ldap_contacts($config, $contact_store, $session=false) {
             }
         }
     }
-    return $contact_store;
+    return array($contact_store, $ldap_config);
 }
 
 /**
