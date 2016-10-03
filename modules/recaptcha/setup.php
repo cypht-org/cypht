73800b8de55164730c1030039729f230ca06825a
Jason Munro <jason@hastymail.org>
2016-10-03 09:56:13 -0500

    recaptcha does not work with dynamic login yet

--- modules/recaptcha/setup.php
+++ modules/recaptcha/setup.php
@@ -5,7 +5,6 @@ handler_source('recaptcha');
 output_source('recaptcha');
 
 add_module_to_all_pages('handler', 'process_recaptcha', false, 'recaptcha', 'login', 'before');
-add_module_to_all_pages('handler', 'process_recaptcha', false, 'recaptcha', 'dynamic_login', 'before');
 add_module_to_all_pages('output', 'recaptcha_form', false, 'recaptcha', 'login_end', 'before');
 add_module_to_all_pages('output', 'recaptcha_script', false, 'recaptcha', 'header_end', 'before');
 
