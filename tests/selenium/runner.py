4ab1a818e483f3c8c27e6415b2d6f0b3efb8a2ff
Jason Munro <jason@hastymail.org>
2016-10-12 09:46:26 -0500

    selenium update for multiple passes and test auto-discovery

--- tests/selenium/runner.py
+++ tests/selenium/runner.py
@@ -2,12 +2,13 @@
 
 from sys import exc_info
 from traceback import print_exception
+from creds import DESIRED_CAP
 
 GREEN = '\033[92m'
 RED = '\033[91m'
 END = '\033[0m'
 
-def test_runner(obj, tests):
+def run_tests(obj, tests):
     passed = 0
     print
     for name in tests:
@@ -26,3 +27,19 @@ def test_runner(obj, tests):
         print '%s%s of %s FAILED%s' % (RED, (len(tests) - passed), len(tests), END)
     print
     obj.end()
+
+def get_tests(class_name):
+    res = []
+    for method in class_name.__dict__:
+        if not method.startswith('__'):
+            res.append(method)
+    return res
+
+def test_runner(class_name, tests=None):
+    if not tests:
+        tests = get_tests(class_name)
+    if isinstance(DESIRED_CAP, list):
+        for cap in DESIRED_CAP:
+            run_tests(class_name(cap), tests)
+    else:
+        run_tests(class_name(), tests)
