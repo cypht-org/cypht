e9e4655eb47574a4fbb7b93c6ceca8dbd7e5b300
Jason Munro <jason@hastymail.org>
2017-03-02 14:17:40 -0600

    prepping a release candidate

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * GIT VERSION: 4040
+ * RELEASE VERSION 1.0.0-pre1
  *
  * Some of the following constants are automatically filled in when
  * the build process is run. If you change them in site/index.php
--- tests/selenium/runall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for suite in *
-do
-    pyc=`echo $suite | grep 'pyc$'`
-    if [ -z "$pyc" ] && [ "$suite" != "runner.py" ] && [ "$suite" != "base.py" ] && [ "$suite" != "runall.sh" ]; then
-        python ./$suite
-    fi
-done
