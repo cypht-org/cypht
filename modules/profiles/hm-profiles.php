0ab84da4d837fb5604872767fe37da308a914794
Jason Munro <jason@hastymail.org>
2017-03-28 10:18:25 -0500

    more work on improving profiles

--- modules/profiles/hm-profiles.php
+++ modules/profiles/hm-profiles.php
@@ -15,48 +15,83 @@ class Hm_Profiles {
 
     private $data = array();
 
-    public function __construct() {
+    public function __construct($hmod) {
+        $this->load($hmod);
     }
 
-    public function load($session) {
+    public function load($hmod) {
+        $this->data = $hmod->user_config->get('profiles', array());
+        if (count($this->data) == 0) {
+            $this->load_legacy($hmod);
+        }
     }
 
-    public function save($session) {
+    public function save($user_config) {
+        $user_config->set('profiles', $this->data);
     }
 
     public function add($data) {
+        $this->data[] = $data;
     }
 
     public function load_legacy($hmod) {
         $profiles = array();
         if ($hmod->module_is_supported('imap')) {
             foreach (Hm_IMAP_List::dump() as $id => $server) {
-                $details = $hmod->user_config->get('profile_imap_'.$server['server'].'_'.$server['user'], array(
-                    'default' => false, 'name' => '', 'address' => '', 'replyto' => '', 'smtp_id' => '', 'sig' => ''));
-                $details['type'] = 'imap';
-                $details['user'] = $server['user'];
-                $details['server'] = $server['server'];
-                $profiles[] = $details;
+                $profile = $hmod->user_config->get('profile_imap_'.$server['server'].'_'.$server['user'], array(
+                    'profile_default' => false, 'profile_name' => '', 'profile_address' => '',
+                    'profile_replyto' => '', 'profile_smtp' => '', 'profile_sig' => ''));
+                $profiles[] = array(
+                    'default' => $profile['profile_default'],
+                    'name' => $profile['profile_name'],
+                    'address' => $profile['profile_address'],
+                    'replyto' => $profile['profile_replyto'],
+                    'smtp_id' => $profile['profile_smtp'],
+                    'sig' => $profile['profile_sig'],
+                    'type' => 'imap',
+                    'user' => $server['user'],
+                    'server' => $server['server'],
+                );
             }
         }
         if ($hmod->module_is_supported('pop3')) {
             foreach (Hm_POP3_List::dump() as $id => $server) {
-                $details = $hmod->user_config->get('profile_pop3_'.$server['server'].'_'.$server['user'], array(
-                    'default' => false, 'name' => '', 'address' => '', 'replyto' => '', 'smtp_id' => '', 'sig' => ''));
-                $details['type'] = 'pop3';
-                $details['user'] = $server['user'];
-                $details['server'] = $server['server'];
-                $profiles[] = $details;
+                $profile = $hmod->user_config->get('profile_pop3_'.$server['server'].'_'.$server['user'], array(
+                    'profile_default' => false, 'profile_name' => '', 'profile_address' => '',
+                    'profile_replyto' => '', 'profile_smtp' => '', 'profile_sig' => ''));
+                $profiles[] = array(
+                    'default' => $profile['profile_default'],
+                    'name' => $profile['profile_name'],
+                    'address' => $profile['profile_address'],
+                    'replyto' => $profile['profile_replyto'],
+                    'smtp_id' => $profile['profile_smtp'],
+                    'sig' => $profile['profile_sig'],
+                    'type' => 'pop3',
+                    'user' => $server['user'],
+                    'server' => $server['server'],
+                );
             }
         }
-        foreach ($profiles as $p) {
-            elog(Hm_IMAP_List::fetch($p['user'], $p['server']));
-        }
+        $this->data = $profiles;
     }
 
     public function edit($id, $data) {
+        if (array_key_exists($id, $this->data)) {
+            $this->data[$id] = $data;
+            return true;
+        }
+        return false;
     }
 
-    public function delete($id) {
+    public function del($id) {
+        if (array_key_exists($id, $this->data)) {
+            unset($this->data[$id]);
+            return true;
+        }
+        return false;
+
+    }
+    public function list() {
+        return $this->data;
     }
 }
