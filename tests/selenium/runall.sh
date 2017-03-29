162bde85b679156651fe8210ce18bb48afc6f654
Jason Munro <jason@hastymail.org>
2017-03-29 16:06:28 -0500

    selenium test tweaks

--- tests/selenium/runall.sh
+++ tests/selenium/runall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-FILE=`realpath $0`
-PATH=`dirname $FILE`
-cd $PATH
 
 for suite in *
 do
     pyc=`echo $suite | /bin/grep 'pyc$'`
     if [ -z "$pyc" ] && [ "$suite" != "runner.py" ] && [ "$suite" != "base.py" ] && [ "$suite" != "runall.sh" ]; then
         /usr/bin/python ./$suite
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
     fi
 done
