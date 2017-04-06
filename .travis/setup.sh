122fabf815418527b1120610225219809d129ab5
Jason Munro <jason@hastymail.org>
2017-04-05 23:11:06 -0500

    testing some travis ci changes

---
+++ .travis/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+setup_ldap() {
+    if [[ ${TRAVIS_PHP_VERSION:0:3} == "7.0" ]]; then
+        echo 'extension=ldap.so' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini;
+    fi
+    if [[ ${TRAVIS_PHP_VERSION:0:3} == "7.1" ]]; then
+        echo 'extension=ldap.so' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini;
+    fi
+}
+setup_ldap
