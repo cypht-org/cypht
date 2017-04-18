c929fb0b007600def5460bde7a162e72ceb7b355
Jason Munro <jason@hastymail.org>
2017-04-18 17:01:18 -0500

    misc fixes to the new (not yet complete) desktop notice module set

--- modules/desktop_notifications/site.js
+++ modules/desktop_notifications/site.js
@@ -3,7 +3,11 @@
 
 $(function() {
     $('body').on('new_message', function() {
-        if (document.hidden && hm_page_name() == 'unread') {
+        var unread_page = false;
+        if (hm_page_name() == 'message_list' && hm_list_path() == 'unread') {
+            unread_page = true;
+        }
+        if (!document.hidden && unread_page) {
             return;
         }
         var current = $('.total_unread_count').text()*1;
@@ -12,16 +16,15 @@ $(function() {
             return;
         }
         var content;
-        if (hm_page_name() == 'unread') {
-            content = Hm_Message_List.just_inserted.join("\n");
+        if (unread) {
+            content = Hm_Message_List.just_inserted.reverse().join("\n");
         }
         else {
-            content = globals.Hm_Background_Unread.just_inserted.join("\n");
+            content = globals.Hm_Background_Unread.just_inserted.reverse().join("\n");
         }
         Push.create("New Message", {
             body: content,
             timeout: 10000,
-            link: '?page=unread',
             icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEwAACxMBAJqcGAAAAIBJREFUWIXtlkEOgCAMBEdfpj/Hl+FB8GBiWoqEg7sJt7YzTXoAlL9nAfJMgXUmXAJVYAeOCeyjsO9sQOI6ypEvFdZrRomY4FEiJtgqiIp45zY3fAWu9d0Devu6N4mCTYHw9TrBboFWES+4WcASaQWHBZ4iUXAGsv4DEpCABBTlBOkR5VdJRFCfAAAAAElFTkSuQmCC',
             onClick: function () { window.focus(); this.close(); }
         });
