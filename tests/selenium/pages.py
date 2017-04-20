bc14e35367c20604a6fe2e7fad791736ab87b1a1
Jason Munro <jason@hastymail.org>
2017-04-20 16:11:17 -0500

    fix for edge in a selenium test (I hope)

--- tests/selenium/pages.py
+++ tests/selenium/pages.py
@@ -84,7 +84,7 @@ class PageTests(WebTest):
         list_item.find_element_by_tag_name('a').click()
         self.rest()
         print ':{0}:'.format(self.by_class('content_title').text)
-        assert self.by_class('content_title').text == 'Servers'
+        assert self.by_class('content_title').text.strip() == 'Servers'
 
     def site(self):
         list_item = self.by_class('menu_settings')
