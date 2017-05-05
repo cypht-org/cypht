34f542875d43d970849faf1abb8b7e3d9809091d
Jason Munro <jason@hastymail.org>
2017-05-05 13:32:09 -0500

    attempt to fix bad cert issue with dovecot on travis ci

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -32,6 +32,29 @@ install_dovecot() {
     sleep 10
     sudo stop dovecot
     echo "mail_location = maildir:/home/%u/Maildir" | sudo tee --append /etc/dovecot/conf.d/10-mail.conf
+    STARTPATH=`pwd`
+    SSL_CERT=/etc/ssl/certs/dovecot.pem
+    SSL_KEY=/etc/ssl/private/dovecot.pem
+    cd /etc/ssl/certs
+    PATH=$PATH:/usr/bin/ssl
+    FQDN=cypht-test.org
+    MAILNAME=cypht-test.org
+    (openssl req -new -x509 -days 365 -nodes -out $SSL_CERT -keyout $SSL_KEY > /dev/null 2>&1 <<+
+    .
+    .
+    .
+    Dovecot mail server
+    $FQDN
+    $FQDN
+    root@$MAILNAME
+    +
+    ) || echo "Warning : Bad SSL config, can't generate certificate."
+    chown root $SSL_CERT || true
+    chgrp dovecot $SSL_CERT || true
+    chmod 0644 $SSL_CERT || true
+    chown root $SSL_KEY || true
+    chgrp dovecot $SSL_KEY || true
+    chmod 0600 $SSL_KEY || true
     sudo start dovecot
     netstat -lnt
 }
