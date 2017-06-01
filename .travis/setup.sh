6095647339ff696cea5ce8abdb4c81fdec509e1b
Jason Munro <jason@hastymail.org>
2017-05-31 22:54:10 -0500

    even more travis tweaking

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -100,7 +100,7 @@ install_selenium() {
 
 # install and configure Apache and PHP-FPM
 install_apache() {
-    sudo apt-get install apache2 libapache2-mod-fastcgi
+    sudo apt-get install apache2-mpm-prefork libapache2-mod-fastcgi
     sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
     sudo a2enmod rewrite actions fastcgi alias
     echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
