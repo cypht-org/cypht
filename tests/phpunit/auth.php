50e08e1c1666ef946d7a47f0365cb5f8b7d0e005
Jason Munro <jason@hastymail.org>
2017-06-04 14:16:04 -0500

    try to fix a unit test broken with postgresql

--- tests/phpunit/auth.php
+++ tests/phpunit/auth.php
@@ -16,11 +16,13 @@ class Hm_Test_Auth extends PHPUnit_Framework_TestCase {
         $auth->delete('unittestuser');
         $this->assertEquals(2, $auth->create('unittestuser', 'unittestpass'));
         $this->assertEquals(1, $auth->create('unittestuser', 'unittestpass'));
+        Hm_DB::close($auth->dbh);
 
         $this->config->set('db_pass', 'asdf');
         $this->config->set('db_socket', '');
         $auth = new Hm_Auth_DB($this->config);
         $this->assertEquals(0, $auth->create('unittestuser', 'unittestpass'));
+        Hm_DB::close($auth->dbh);
 
         $this->config->set('db_pass', 'asdf');
         $this->config->set('db_socket', '/root/cantgetthere.db');
