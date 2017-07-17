016afe56bb1b750e8e5d2adad92221a284b341a0
Jason Munro <jason@hastymail.org>
2017-07-17 15:44:11 +0000

    more travis testing

--- tests/selenium/runall.sh
+++ tests/selenium/runall.sh
@@ -5,7 +5,7 @@ do
     pyc=`echo $suite | /bin/grep 'pyc$'`
     if [ -z "$pyc" ] && [ "$suite" != "runner.py" ] && [ "$suite" != "base.py" ] && [ "$suite" != "runall.sh" ]; then
         export TEST_SUITE="$suite"
-        /usr/bin/python -u -v ./$suite
+        /usr/bin/python ./$suite
         if [ $? -ne 0 ]; then
             exit 1
         fi
