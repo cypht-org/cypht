38844bfb4b4a398ca2fa214e5dce88f7a0ea10f1
Jason Munro <jason@hastymail.org>
2017-07-17 14:27:23 +0000

    debugging travis PHP 7.1 ldap issue

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -19,6 +19,7 @@ setup_ldap() {
     fi
     if [ "$TRAVIS_PHP_VERSION" = "7.1" ]; then
         sudo apt-get install php7.1-ldap
+        ls -R /usr/lib/php
         sudo ln -s /usr/lib/php/20160303/ldap.so /home/travis/.phpenv/versions/7.1.5/lib/php/extensions/no-debug-zts-20160303/ldap.so
         echo 'extension=ldap.so' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
     fi
