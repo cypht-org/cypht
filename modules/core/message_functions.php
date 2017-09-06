2fc185722786c574e8e9507d5e1d95dac0c8a05b
Jason Munro <jason@hastymail.org>
2017-09-06 17:10:23 +0000

    fix for link formatting in text message parts (again)

--- modules/core/message_functions.php
+++ modules/core/message_functions.php
@@ -59,7 +59,7 @@ function format_msg_image($str, $mime_type) {
 function format_msg_text($str, $output_mod, $links=true) {
     $str = str_replace("\t", '    ', $str);
     $str = nl2br(str_replace(' ', '<wbr>', ($output_mod->html_safe($str))));
-    $str = preg_replace("/(&[^;]+;)/", " $1", $str);
+    $str = preg_replace("/(&(?!amp)[^;]+;)/", " $1", $str);
     if ($links) {
         $link_regex = "/((http|ftp|rtsp)s?:\/\/(%[[:digit:]A-Fa-f][[:digit:]A-Fa-f]|[-_\.!~\*';\/\?#:@&=\+$,\[\]%[:alnum:]])+)/m";
         $str = preg_replace($link_regex, "<a target=\"_blank\" href=\"$1\">$1</a>", $str);
