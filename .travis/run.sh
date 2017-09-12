fa7f5d9214403b0d9c83769428b13e29b48a11c5
Jason Munro <jason@hastymail.org>
2017-09-12 11:01:43 -0500

    more travis tweaking

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$DB" = "postgresql" ] && [ "$TRAVIS_PHP_VERSION" = "nightly" ]; then
-    cd tests/phpunit/ && usr/local/bin/phpunit && cd ../selenium && sh ./runall.sh && cd ../../
+    cd tests/phpunit/ && /usr/local/bin/phpunit && cd ../selenium && sh ./runall.sh && cd ../../
 else
     cd tests/phpunit && /usr/local/bin/phpunit && cd ../../
 fi
