617439aae2d040c4825ad0ac82b7910f9b068032
Jason Munro <jason@hastymail.org>
2017-09-25 16:07:54 -0500

    version bump

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * GIT VERSION: 10478
+ * GIT VERSION: 10484
  *
  * Some of the following constants are automatically filled in when
  * the build process is run. If you change them in site/index.php
