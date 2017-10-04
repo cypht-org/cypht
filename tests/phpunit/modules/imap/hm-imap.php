763b5c5af75a6f7f1a3fe39985ee1ea6b1e0b7e9
Jason Munro <jason@hastymail.org>
2017-10-03 22:43:28 -0500

    IMAP unit test updates

--- tests/phpunit/modules/imap/hm-imap.php
+++ tests/phpunit/modules/imap/hm-imap.php
@@ -24,24 +24,107 @@ class Hm_Test_Hm_IMAP extends PHPUnit_Framework_TestCase {
      * @preserveGlobalState disabled
      * @runInSeparateProcess
      */
-    public function test_connect() {
-        /* normal connect */
+    public function test_connect_working() {
         $this->connect();
-        $this->imap->show_debug();
+        $res = $this->imap->show_debug(true, true);
+        print_r($res);
         $this->disconnect();
-        /* tls connect */
+    }
+    /**
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_connect_tls() {
         $this->config['tls'] = true;
         $this->connect();
-        $this->imap->show_debug();
+        $res = $this->imap->show_debug(true, true);
+        print_r($res);
         $this->disconnect();
-        /* failed connection */
+    }
+    /**
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_connect_failed() {
         Hm_Functions::$no_stream = true;
         $this->connect();
-        $this->imap->show_debug();
-        /* bad args */
+        $res = $this->imap->show_debug(true, true);
+        print_r($res);
+    }
+    /**
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_connect_bad_args() {
         unset($this->config['username']);
         $this->connect();
-        $this->imap->show_debug();
+        $res = $this->imap->show_debug(true, true);
+        print_r($res);
+    }
+    /**
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_authenticate_login() {
+        $this->connect();
+        $res = $this->imap->show_debug(true, true);
+        print_r($res);
+        $this->disconnect();
+    }
+    /**
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_authenticate_cram() {
+        $this->config['auth'] = 'cram-md5';
+        $this->connect();
+        $res = $this->imap->show_debug(true, true);
+        print_r($res);
+        $this->disconnect();
+    }
+    /**
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_authenticate_oauth() {
+        $this->config['auth'] = 'xoauth2';
+        $this->connect();
+        $res = $this->imap->show_debug(true, true);
+        print_r($res);
+        $this->disconnect();
+    }
+    /**
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_get_special_use_mailboxes() {
+        $this->connect();
+        $this->imap->get_special_use_mailboxes();
+        $this->imap->get_special_use_mailboxes('sent');
+        $res = $this->imap->show_debug(true, true);
+        print_r($res);
+    }
+    /**
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_get_namespaces() {
+        $this->connect();
+        $this->imap->get_namespaces();
+        $res = $this->imap->show_debug(true, true);
+        print_r($res);
+    }
+    /**
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_get_mailbox_list() {
+        $this->connect();
+        $this->imap->get_mailbox_list();
+        $this->imap->get_mailbox_list(true);
+        $this->imap->get_mailbox_list();
+        $res = $this->imap->show_debug(true, true);
+        print_r($res);
     }
     /**
      * @preserveGlobalState disabled
@@ -50,7 +133,8 @@ class Hm_Test_Hm_IMAP extends PHPUnit_Framework_TestCase {
     public function test_disconnect() {
         /* normal connect */
         $this->connect();
-        $this->imap->show_debug();
+        $res = $this->imap->show_debug(true, true);
+        print_r($res);
         $this->disconnect();
     }
     public function tearDown() {
