85f4ef45cfa244d1e333d6b00882a778be83ea50
Jason Munro <jason@hastymail.org>
2018-07-23 12:24:18 -0500

    add class for easier styling of imap message part description

--- modules/imap/modules.php
+++ modules/imap/modules.php
@@ -2680,7 +2680,7 @@ function format_msg_part_row($id, $vals, $output_mod, $level, $part, $dl_args, $
         $res .= '</td><td class="part_encoding">'.(isset($vals['encoding']) ? $output_mod->html_safe(strtolower($vals['encoding'])) : '').
             '</td><td class="part_charset">'.(isset($vals['attributes']['charset']) && trim($vals['attributes']['charset']) ? $output_mod->html_safe(strtolower($vals['attributes']['charset'])) : '');
     }
-    $res .= '</td><td>'.$output_mod->html_safe(decode_fld($desc)).'</td>';
+    $res .= '</td><td class="part_desc">'.$output_mod->html_safe(decode_fld($desc)).'</td>';
     $res .= '<td class="download_link"><a href="?'.$dl_args.'&amp;imap_msg_part='.$output_mod->html_safe($id).'">'.$output_mod->trans('Download').'</a></td></tr>';
     return $res;
 }}
