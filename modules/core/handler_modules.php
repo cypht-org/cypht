0588967c5fd245bd08a9089198393451b054c872
Jason Munro <jason@hastymail.org>
2018-08-06 17:10:57 -0500

    allow sites to enable external image sources

--- modules/core/handler_modules.php
+++ modules/core/handler_modules.php
@@ -172,12 +172,16 @@ class Hm_Handler_http_headers extends Hm_Handler_Module {
         if ($this->request->tls) {
             $headers['Strict-Transport-Security'] = 'max-age=31536000';
         }
+        $img_src = "'self'";
+        if ($this->config->get('allow_external_image_sources', false)) {
+            $img_src = '*';
+        }
         $headers['X-Frame-Options'] = 'SAMEORIGIN';
         $headers['X-XSS-Protection'] = '1; mode=block';
         $headers['X-Content-Type-Options'] = 'nosniff';
         $headers['Expires'] = gmdate('D, d M Y H:i:s \G\M\T', strtotime('-1 year'));
         $headers['Content-Security-Policy'] = "default-src 'none'; script-src 'self' 'unsafe-inline'; ".
-            "connect-src 'self'; font-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline';";
+            "connect-src 'self'; font-src 'self'; img-src ".$img_src." data:; style-src 'self' 'unsafe-inline';";
         if ($this->request->type == 'AJAX') {
             $headers['Content-Type'] = 'application/json';
         }
