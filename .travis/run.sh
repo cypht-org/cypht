099068cb7b5b0ba093fe9d87ab89fc0a239fcb95
Jason Munro <jason@hastymail.org>
2018-08-22 12:40:30 -0500

    limited selenium tests worked, lets try all instances in travis!

--- .travis/run.sh
+++ .travis/run.sh
@@ -8,21 +8,4 @@ selenium_tests() {
     cd tests/selenium/ && sh ./runall.sh && cd ../../
 }
 
-BUILD="$DB$TRAVIS_PHP_VERSION"
-case "$BUILD" in
-    mysql5.4)
-        phpunit_tests && selenium_tests
-    ;;
-    postgresql5.4)
-        phpunit_tests && selenium_tests
-    ;;
-    postgresqlnightly)
-        phpunit_tests && selenium_tests
-    ;;
-    mysqlnightly)
-        phpunit_tests && selenium_tests
-    ;;
-    *)
-        phpunit_tests
-    ;;
-esac
+phpunit_tests && selenium_tests
