dee9dafd0c86f1a7f8525b275079e6b28ea8c93b
Jason Munro <jason@hastymail.org>
2018-08-23 13:12:52 -0500

    limit which build run selenium, add some more browsers

--- .travis/run.sh
+++ .travis/run.sh
@@ -8,4 +8,21 @@ selenium_tests() {
     cd tests/selenium/ && sh ./runall.sh && cd ../../
 }
 
-phpunit_tests && selenium_tests
+BUILD="$DB$TRAVIS_PHP_VERSION"
+case "$BUILD" in
+    postgresql5.6)
+        phpunit_tests && selenium_tests
+    ;;
+    postgresql7.0)
+        phpunit_tests && selenium_tests
+    ;;
+    postgresql7.1)
+        phpunit_tests && selenium_tests
+    ;;
+    postgresql7.2)
+        phpunit_tests && selenium_tests
+    ;;
+    *)
+        phpunit_tests
+    ;;
+esac
