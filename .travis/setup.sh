01a4a974aad40d3428b03884bdc4031e21360fa9
Jason Munro <jason@hastymail.org>
2018-08-23 14:27:21 -0500

    move the coverage report again

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -168,7 +168,7 @@ setup_db() {
         mysql -u root -e "insert into hm_user values('unittestuser', 'sha512:86000:xfEgf7NIUQ2XkeU5tnIcA+HsN8pUllMVdzpJxCSwmbsZAE8Hze3Zs+MeIqepwocYteJ92vhq7pjOfrVThg/p1voELkDdPenU8i2PgG9UTI0IJTGhMN7rsUILgT6XlMAKLp/u2OD13sukUFcQNTdZNFqMsuTVTYw/Me2tAnFwgO4=:rfyUhYsWBCknx6EmbeswN0fy0hAC0N3puXzwWyDRquA=');" test
         mysql -u root -e "insert into hm_user_settings values('testuser', 'sFpVPU/hPvmfeiEKUBs4w1EizmbW/Ze2BALZf6kdJrIU3KVZrsqIhKaWTNNFRm3p51ssRAH2mpbxBMhsdpOAqIZMXFHjLttRu9t5WZWOkN7qwEh2LRq6imbkMkfqXg//K294QDLyWjE0Lsc/HSGqnguBF0YUVLVmWmdeqq7/OrXUo4HNbU88i4s2gkukKobJA2hjcOEq/rLOXr3t4LnLlcISnUbt4ptalSbeRrOnx4ehZV8hweQf1E+ID7s/a+8HHx1Qo713JDzReoLEKUsxRQ==');" test
     fi
-     if [ "$TRAVIS_PHP_VERSION" = "7.1" ] && [ "$DB" = "sqlite" ]; then
+     if [ "$TRAVIS_PHP_VERSION" = "7.1" ] && [ "$DB" = "mysql" ]; then
          cp -vf .travis/phpunit.xml tests/phpunit/phpunit.xml
      fi
 }
