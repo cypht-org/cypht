01e2947f66e57c53e3a9ad2fb3f9d183693aaf31
Jason Munro <jason@hastymail.org>
2018-10-09 15:43:27 -0500

    don't double up on loading animation speed if we overlap

--- modules/core/site.js
+++ modules/core/site.js
@@ -37,6 +37,9 @@ var Hm_Ajax = {
     },
 
     show_loading_icon: function() {
+        if (Hm_Ajax.icon_loading_id) {
+            self.stop_loading_icon(Hm_Ajax.icon_loading_id);
+        }
         var hm_loading_pos = $('.loading_icon').width()/40;
         $('.loading_icon').show();
         function move_background_image() {
