ff8e257e566641d36d68ec3708b2eba6836f72eb
Jason Munro <jason@hastymail.org>
2019-07-01 23:19:19 -0500

    changelog script update

--- scripts/release_changes.sh
+++ scripts/release_changes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 CYPHT_DIR="/home/jason/cypht"
-VERSION="v1.1.0-rc4"
-SINCE="2017-05-11"
+VERSION="v1.1.0"
+SINCE="2018-11-11"
 
 cd "$CYPHT_DIR"
 git log --pretty=format:'%h%  - %s [%aD]' \
