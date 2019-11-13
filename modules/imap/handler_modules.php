c5dc2b96d5cfd1a6a8f15d85dbd0920b16fe325c
Jason Munro <jason@hastymail.org>
2019-11-13 16:19:31 -0600

    follow up work on the new archive message feature issue #228

--- modules/imap/handler_modules.php
+++ modules/imap/handler_modules.php
@@ -697,68 +697,64 @@ class Hm_Handler_imap_archive_message extends Hm_Handler_Module {
     public function process() {
         list($success, $form) = $this->process_form(array('imap_msg_uid', 'imap_server_id', 'folder'));
 
-        if ($success) {
-            $cache = Hm_IMAP_List::get_cache($this->cache, $form['imap_server_id']);
-            $imap = Hm_IMAP_List::connect($form['imap_server_id'], $cache);
-            $archive_folder = false;
-
-            $specials = $this->user_config->get('special_imap_folders', array());
-            if (array_key_exists($form['imap_server_id'], $specials)) {
-                if (array_key_exists('archive', $specials[$form['imap_server_id']])) {
-                    if ($specials[$form['imap_server_id']]['archive']) {
-                        $archive_folder = $specials[$form['imap_server_id']]['archive'];
-                    }
-                }
-            }
-
-            if(!$archive_folder) {
-                $archive_folder = "Archive";
-            }
-
-            if (imap_authed($imap)) {
-                $delimiter = $imap->get_namespaces()[0]['delim'];
-                $new_folder = $archive_folder . $delimiter . hex2bin($form['folder']);
-                
-                if(count($imap->get_mailbox_status($archive_folder)) == 0) {
-                    if($imap->create_mailbox($archive_folder)) {
-                        if ($imap->create_mailbox($new_folder)) {
-                            if ($imap->select_mailbox(hex2bin($form['folder']))) {
-                                $imap->message_action('MOVE', array($form['imap_msg_uid']), $new_folder);
-                                Hm_Msgs::add("Message archived");
-                            } else {
-                                Hm_Msgs::add('ERRAn error occurred archiving the message');
-                            }
-                        } else {
-                            Hm_Msgs::add('ERRAn error occurred archiving the message');
-                        }
-                    } else {
-                        Hm_Msgs::add('ERRAn error occurred archiving the message');
-                    }
-                } else {
-                    if(count($imap->get_mailbox_status($new_folder)) == 0 ){
-                        if ($imap->create_mailbox($new_folder)) {
-                            if ($imap->select_mailbox(hex2bin($form['folder']))) {
-                                $imap->message_action('MOVE', array($form['imap_msg_uid']), $new_folder);
-                                Hm_Msgs::add("Message archived");
-                            } else {
-                                Hm_Msgs::add('ERRAn error occurred archiving the message');
-                            }
-                        } else {
-                            Hm_Msgs::add('ERRAn error occurred archiving the message');
-                        }
-                    } else {
-                        if ($imap->select_mailbox(hex2bin($form['folder']))) {
-                            $imap->message_action('MOVE', array($form['imap_msg_uid']), $new_folder);
-                            Hm_Msgs::add("Message archived");
-                        }
-                    }
-                }
-            }
-
-            $msgs = Hm_Msgs::get();
-            Hm_Msgs::flush();
-            $this->session->secure_cookie($this->request, 'hm_msgs', base64_encode(json_encode($msgs)));
+        if (!$success) {
+            return;
         }
+        $cache = Hm_IMAP_List::get_cache($this->cache, $form['imap_server_id']);
+        $imap = Hm_IMAP_List::connect($form['imap_server_id'], $cache);
+        $archive_folder = false;
+
+        $specials = $this->user_config->get('special_imap_folders', array());
+        if (array_key_exists($form['imap_server_id'], $specials)) {
+            if (array_key_exists('archive', $specials[$form['imap_server_id']])) {
+                if ($specials[$form['imap_server_id']]['archive']) {
+                    $archive_folder = $specials[$form['imap_server_id']]['archive'];
+                }
+            }
+        }
+
+        if(!$archive_folder) {
+            $archive_folder = "Archive";
+        }
+
+        if (imap_authed($imap)) {
+            $new_folder = prep_folder_name($imap, hex2bin($form['folder']), false, $archive_folder);
+            $archive_exists = count($imap->get_mailbox_status($archive_folder));
+            $folder_exists = count($imap->get_mailbox_status($new_folder));
+            $error = false;
+
+            /* if archive folders don't exist try to create them */
+            if (!$archive_exists && !$imap->create_mailbox($archive_folder)) {
+                $error = true;
+            }
+            if (!$error && !$folder_exists && !$imap->create_mailbox($new_folder)) {
+                $error = true;
+            }
+
+            /* select source folder */
+            if (!$error && !$imap->select_mailbox(hex2bin($form['folder']))) {
+                $error = true;
+            }
+
+            /* try to move the message */
+            if (!$error && $imap->message_action('MOVE', array($form['imap_msg_uid']), $new_folder)) {
+                Hm_Msgs::add("Message archived");
+
+                /* bust imap cache if we created a new folder */
+                if (!$archive_exists || !$folder_exists) {
+                    $this->session->secure_cookie($this->request, 'hm_reload_folders', '1');
+                    $this->cache->del('imap'.$form['imap_server_id']);
+                    $this->cache->del('imap_folders_imap_'.$form['imap_server_id'].'_'.bin2hex($archive_folder));
+                }
+            }
+            else {
+                Hm_Msgs::add('ERRAn error occurred archiving the message');
+            }
+        }
+
+        $msgs = Hm_Msgs::get();
+        Hm_Msgs::flush();
+        $this->session->secure_cookie($this->request, 'hm_msgs', base64_encode(json_encode($msgs)));
     }
 }
 
