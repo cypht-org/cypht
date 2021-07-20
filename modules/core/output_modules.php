aab263c239cc8552a11fa5400b005073882e40f8
revisto <walt.shabani@gmail.com>
2021-07-20 15:41:47 +0430

    add hm_language_direction function

--- modules/core/output_modules.php
+++ modules/core/output_modules.php
@@ -544,6 +544,7 @@ class Hm_Output_js_data extends Hm_Output_Module {
             'var hm_debug = function() { return "'.(DEBUG_MODE ? '1' : '0').'"; };'.
             'var hm_mailto = function() { return '.($this->get('mailto_handler') ? '1' : '0').'; };'.
             'var hm_page_name = function() { return "'.$this->html_safe($this->get('router_page_name')).'"; };'.
+            'var hm_language_direction = function() { return "'.$this->html_safe($this->dir).'"; };'.
             'var hm_list_path = function() { return "'.$this->html_safe($this->get('list_path', '')).'"; };'.
             'var hm_list_parent = function() { return "'.$this->html_safe($this->get('list_parent', '')).'"; };'.
             'var hm_msg_uid = function() { return Hm_Utils.get_from_global("msg_uid", "'.$this->html_safe($this->get('uid', '')).'"); };'.
