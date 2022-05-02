b88e277680e0f152b0f2d88254bba6908ae3bb48
ElvisAns <ansimapersic@gmail.com>
2022-05-02 18:42:56 +0200

    [UPD] Import the new namespaces

--- modules/2fa/modules.php
+++ modules/2fa/modules.php
@@ -1,6 +1,12 @@
 <?php
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
 
-use CodeItNow\BarcodeBundle\Utils\QrCode;
 
 /**
  * 2FA modules
