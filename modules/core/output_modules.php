6390c7a9f0d7ec383fa529e9ab7732c1a06f264c
Henrique Borba <henrique.borba.dev@gmail.com>
2022-08-11 08:06:53 -0300

    Remove litegraph from debug mode

--- modules/core/output_modules.php
+++ modules/core/output_modules.php
@@ -485,7 +485,6 @@ class Hm_Output_page_js extends Hm_Output_Module {
             $js_lib = '<script type="text/javascript" src="third_party/cash.min.js"></script>';
             $js_lib .= '<script type="text/javascript" src="third_party/resumable.min.js"></script>';
             $js_lib .= '<script type="text/javascript" src="third_party/tingle.min.js"></script>';
-            $js_lib .= '<script type="text/javascript" src="third_party/litegraph.min.js"></script>';
             if ($this->get('encrypt_ajax_requests', '') || $this->get('encrypt_local_storage', '')) {
                 $js_lib .= '<script type="text/javascript" src="third_party/forge.min.js"></script>';
             }
