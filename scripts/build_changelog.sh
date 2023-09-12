7aa6a18671593623d1534ae5c01e09bb7241369c
Marc Laporte <marclaporte@gmail.com>
2023-09-12 16:22:07 -0400

    Update build_changelog.sh

--- scripts/build_changelog.sh
+++ scripts/build_changelog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This was last used for Cypht 1.3.0, and will no longer be used. Anyone interested can simply ready the Git commit log.
+# Should we continue to use this?
 
 
 CYPHT_DIR="/home/jason/cypht"
