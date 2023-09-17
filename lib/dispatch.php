ca9e4cba670fad10640feb6972aa6733640eea63
Yannick  nsenga romeo <Yanromeo99@gmail.com>
2023-09-17 21:54:10 +0200

    Setting minimum requirement of php version to 7.4

--- lib/dispatch.php
+++ lib/dispatch.php
@@ -358,7 +358,7 @@ class Hm_Dispatch {
      */
     static public function is_php_setup() {
         return
-            version_compare(PHP_VERSION, '5.4', '>=') &&
+            version_compare(PHP_VERSION, '7.4', '>=') &&
             Hm_Functions::function_exists('mb_strpos') &&
             Hm_Functions::function_exists('curl_exec') &&
             Hm_Functions::function_exists('openssl_random_pseudo_bytes') &&
