ef42d9cf8f43154790cdc8c4839c5a2b7a0cd632
ElvisAns <ansimapersic@gmail.com>
2023-09-19 06:38:19 +0200

    follow naming convention

--- modules/core/output_modules.php
+++ modules/core/output_modules.php
@@ -226,8 +226,8 @@ class Hm_Output_login_start extends Hm_Output_Module {
                 'width:300px;}.login_form input{clear:both;float:left;padding:4px;'.
                 'margin-top:10px;margin-bottom:10px;}#username,#password{width:200px; height:25px;} .err{color:red !important;}.long_session'.
                 '{float:left;}.long_session input{padding:0px;float:none;font-size:18px;}.mobile .long_session{float:left;clear:both;} @media screen and (min-width:400px){.login_form{width:400px;}}'.
-                '.user-icon__signin{display:block; background-color:white; border-radius:100%; padding:10px; height:40px; margin-top:-120px; box-shadow: #6eb549 .4px 2.4px 6.2px; }'.
-                '.label__signin{width:210px; margin:0px 0px -18px 0px;color:#fff;opacity:0.7;}
+                '.user-icon_signin{display:block; background-color:white; border-radius:100%; padding:10px; height:40px; margin-top:-120px; box-shadow: #6eb549 .4px 2.4px 6.2px; }'.
+                '.label_signin{width:210px; margin:0px 0px -18px 0px;color:#fff;opacity:0.7;}
                 </style>';
 
             return $css.'<div class="form-container"><form class="login_form" method="POST">';
@@ -264,13 +264,13 @@ class Hm_Output_login extends Hm_Output_Module {
                     $stay_logged_in.' <input style="cursor:pointer;" type="submit" id="login" value="'.$this->trans('Login').'" />';
             }
 
-            return '<svg class="user-icon__signin" viewBox="0 0 20 20"><path d="M12.075,10.812c1.358-0.853,2.242-2.507,2.242-4.037c0-2.181-1.795-4.618-4.198-4.618S5.921,4.594,5.921,6.775c0,1.53,0.884,3.185,2.242,4.037c-3.222,0.865-5.6,3.807-5.6,7.298c0,0.23,0.189,0.42,0.42,0.42h14.273c0.23,0,0.42-0.189,0.42-0.42C17.676,14.619,15.297,11.677,12.075,10.812 M6.761,6.775c0-2.162,1.773-3.778,3.358-3.778s3.359,1.616,3.359,3.778c0,2.162-1.774,3.778-3.359,3.778S6.761,8.937,6.761,6.775 M3.415,17.69c0.218-3.51,3.142-6.297,6.704-6.297c3.562,0,6.486,2.787,6.705,6.297H3.415z"></path></svg>
+            return '<svg class="user-icon_signin" viewBox="0 0 20 20"><path d="M12.075,10.812c1.358-0.853,2.242-2.507,2.242-4.037c0-2.181-1.795-4.618-4.198-4.618S5.921,4.594,5.921,6.775c0,1.53,0.884,3.185,2.242,4.037c-3.222,0.865-5.6,3.807-5.6,7.298c0,0.23,0.189,0.42,0.42,0.42h14.273c0.23,0,0.42-0.189,0.42-0.42C17.676,14.619,15.297,11.677,12.075,10.812 M6.761,6.775c0-2.162,1.773-3.778,3.358-3.778s3.359,1.616,3.359,3.778c0,2.162-1.774,3.778-3.359,3.778S6.761,8.937,6.761,6.775 M3.415,17.69c0.218-3.51,3.142-6.297,6.704-6.297c3.562,0,6.486,2.787,6.705,6.297H3.415z"></path></svg>
                 <img src="'.WEB_ROOT. 'modules/core/assets/images/logo.svg" style="height:90px;">'.
                 '<!--h1 class="title">'.$this->html_safe($this->get('router_app_name', '')).'</h1-->'.
                 ' <input type="hidden" name="hm_page_key" value="'.Hm_Request_Key::generate().'" />'.
-                ' <label class="label__signin" for="username">'.$this->trans('Username').'</label>'.
+                ' <label class="label_signin" for="username">'.$this->trans('Username').'</label>'.
                 '<input autofocus required type="text" placeholder="'.$this->trans('Username').'" id="username" name="username" value="">'.
-                ' <label class="label__signin" for="password">'.$this->trans('Password').'</label>'.
+                ' <label class="label_signin" for="password">'.$this->trans('Password').'</label>'.
                 '<input required type="password" id="password" placeholder="'.$this->trans('Password').'" name="password">'.
                 $stay_logged_in.' <input style="cursor:pointer; display:block; width:50%; background-color:#6eb549; color:white; height:40px;" type="submit" id="login" value="'.$this->trans('Login').'" />';
         }
