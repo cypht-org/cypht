c47a99b2bd4282dfb462f2fdec1322837de2d01f
Yannick  nsenga romeo <Yanromeo99@gmail.com>
2023-11-03 16:14:03 +0200

    Make sure the user has configured Google Authenticator before enabling 2FA authentication

--- modules/2fa/modules.php
+++ modules/2fa/modules.php
@@ -110,6 +110,33 @@ class Hm_Handler_2fa_check extends Hm_Handler_Module {
 }
 
 /**
+ * Verify 2fa code is paired with Authenticator app before enabling 2fa
+ * @subpackage 2fa/handler
+ */
+class Hm_Handler_2fa_setup_check extends Hm_Handler_Module {
+    public function process() {
+
+        list($secret, $simple) = get_2fa_key($this->config);
+        if (!$secret) {
+            Hm_Debug::add('2FA module set enabled, but no shared secret configured');
+            return;
+        }
+
+        $verified = false;
+        $len = $simple ? 15 : 64;
+
+        $username = $this->session->get('username', false);
+        $secret = create_secret($secret, $username, $len);
+
+        if (check_2fa_pin($this->request->post['2fa_code'], $secret)) {
+            $verified = true;
+        }
+        
+        $this->out('ajax_2fa_verified', $verified);
+    }
+}
+
+/**
  * @subpackage 2fa/output
  */
 class Hm_Output_enable_2fa_setting extends Hm_Output_Module {
@@ -131,7 +158,7 @@ class Hm_Output_enable_2fa_setting extends Hm_Output_Module {
         $res .= '></td></tr>';
         $svg = $this->get('2fa_svg');
         if ($svg) {
-            $qr_code = '<tr class="tfa_setting"><td></td><td>';
+            $qr_code = '<tr class="tfa_setting"><td class="hidden-xs"></td><td>';
             if (!$enabled) {
                 $qr_code .= '<div class="err settings_wrap_text">'.
                     $this->trans('Configure your authentication app using the barcode below BEFORE enabling 2 factor authentication.').'</div>';
@@ -142,18 +169,41 @@ class Hm_Output_enable_2fa_setting extends Hm_Output_Module {
 
             $qr_code .= $svg;
             $qr_code .= '</td></tr>';
-            $qr_code .= '<tr class="tfa_setting"><td></td><td>'.$this->trans('If you can\'t use the QR code, you can enter the code below manually (no line breaks)').'</td></tr>';
-            $qr_code .= '<tr class="tfa_setting"><td></td><td>'.wordwrap($this->html_safe($this->get('2fa_secret', '')), 60, '<br />', true).'</td></tr>';
+            $qr_code .= '<tr class="tfa_setting"><td class="hidden-xs"></td><td>'.$this->trans('If you can\'t use the QR code, you can enter the code below manually (no line breaks)').'</td></tr>';
+            $qr_code .= '<tr class="tfa_setting"><td class="hidden-xs"></td><td>'.wordwrap($this->html_safe($this->get('2fa_secret', '')), 60, '<br />', true).'</td></tr>';
         }
         else {
             $qr_code = '<tr class="tfa_setting"><td></td><td class="err">'.$this->trans('Unable to generate 2 factor authentication QR code').'</td></tr>';
         }
         $res .= $qr_code;
-        $res .= '<tr class="tfa_setting"><td></td><td>'.$this->trans('The following backup codes can be used to access your account if you lose your device').'<br /><br />';
+        $res .= '<tr class="tfa_setting"><td class="hidden-xs"></td><td>'.$this->trans('The following backup codes can be used to access your account if you lose your device').'<br /><br />';
         foreach ($backup_codes as $val) {
             $res .= ' '.$val.'<input type="hidden" name="2fa_backup_codes[]" value="'.$val.'" /></br >';
         }
         $res .= '</td></tr>';
+
+        $res .= '<tr class="tfa_setting">
+        <td class="hidden-xs"></td>
+        <td>
+            <fieldset class="tfa_confirmation_fieldset">
+                <legend>Enter the confirmation code</legend>
+                <div class="tfa_confirmation_wrapper">
+                    <div class="tfa_confirmation_form">
+                        <div class="tfa_confirmation_input_digits">
+                            <input class="tfa_confirmation_input_digit" type="number" aria-label="Digit 0" aria-required="true">
+                            <input class="tfa_confirmation_input_digit" type="number" aria-label="Digit 1" aria-required="true">
+                            <input class="tfa_confirmation_input_digit" type="number" aria-label="Digit 2" aria-required="true">
+                            <input class="tfa_confirmation_input_digit" type="number" aria-label="Digit 3" aria-required="true">
+                            <input class="tfa_confirmation_input_digit" type="number" aria-label="Digit 4" aria-required="true">
+                            <input class="tfa_confirmation_input_digit" type="number" aria-label="Digit 5" aria-required="true">
+                        </div>
+                        <button id="tfaConfirmationBtn" type="submit" class="tfa_confirmation_input_button">'.$this->trans('Verify code').'</button>
+                    </div>
+                    <div class="tfa_confirmation_hint"> '.$this->trans('Enter the 6 digit code from your Authenticator application').'</div>
+                </div>
+            </fieldset>
+        </td>
+    </tr>';
         return $res;
     }
 }
---
+++ modules/2fa/site.js
@@ -0,0 +1,175 @@
+$(function () {
+    function tFaToast(message, timer = 3000) {
+        Hm_Notices.show([message]);
+        const tm = setTimeout(function () {
+            Hm_Notices.hide(true);
+            clearTimeout(tm);
+        }, timer);
+    }
+
+    function focusElement(elem) {
+        elem.focus();
+        elem.select();
+    }
+
+    function validateInput(input) {
+        if (isNaN(Number(input.value)) || input.value === "") {
+            input.classList.add("invalid");
+            return false;
+        }
+        input.classList.remove("invalid");
+        return input.value.length > 1 ? input.value[0] : input.value;
+    }
+
+    function focusNextInput(currentInput) {
+        if (currentInput.nextElementSibling) {
+            focusElement(currentInput.nextElementSibling);
+        }
+    }
+
+    function focusPrevInput(currentInput) {
+        if (currentInput.previousElementSibling) {
+            focusElement(currentInput.previousElementSibling);
+        }
+    }
+
+    function handleArrowKeys(e) {
+        if (e.key == "ArrowRight") {
+            e.preventDefault();
+            focusNextInput(e.target);
+        }
+        if (e.key == "ArrowLeft") {
+            e.preventDefault();
+            focusPrevInput(e.target);
+        }
+    }
+
+    function handleBackspace(e) {
+        if (e.key == "Backspace") {
+            e.target.value = "";
+            e.target.classList.remove("invalid");
+            focusPrevInput(e.target);
+        }
+    }
+
+    function getInputCode() {
+        let inputCode = "";
+        for (let input of $(".tfa_confirmation_input_digit")) {
+            input.classList.remove("invalid");
+            if (validateInput(input) === false) {
+                return null;
+            }
+            inputCode += input.value;
+            input.blur();
+        }
+        return inputCode;
+    }
+
+    function tFaInit() {
+        let verified = false;
+
+        const formInput = $('input[name="2fa_enable"]');
+        const tfaEnabled = formInput.is(":checked");
+        const confirmationBtn = $("#tfaConfirmationBtn");
+        const formContainer = $(".tfa_confirmation_form");
+
+        if (!tfaEnabled) {
+            formInput.on("change", function () {
+                const checked = $(this).is(":checked");
+
+                if (checked && !verified) {
+                    $(this).prop("checked", false);
+                    tFaToast("ERRYou need to verify your 2 factor authentication code before processing");
+                    return;
+                }
+            });
+        }
+
+        formContainer.on("input", function (e) {
+            const value = validateInput(e.target);
+            if (value !== false) {
+                e.target.value = value;
+                focusNextInput(e.target);
+            } else {
+                e.target.value = "";
+            }
+        });
+
+        formContainer.on("click", function (e) {
+            if (e.target.tagName == "INPUT") {
+                focusElement(e.target);
+            }
+        });
+
+        formContainer.on("keydown", ".tfa_confirmation_input_digit", function (e) {
+            handleArrowKeys(e);
+            handleBackspace(e);
+        });
+
+        formContainer.on("paste", function (e) {
+            const pastedData = e.originalEvent.clipboardData.getData("text");
+
+            if (pastedData === "") {
+                return;
+            }
+            const payloadData = pastedData.split("");
+
+            const inputs = $(".tfa_confirmation_input_digit");
+
+            inputs.each(function (i, input) {
+                $(input).val(payloadData[i]).focus();
+            });
+
+            const tm = setTimeout(() => {
+                for (let input of inputs) {
+                    if (validateInput(input) === false) {
+                        focusElement(input);
+                        break;
+                    }
+                }
+                clearTimeout(tm);
+            }, 0);
+        });
+
+        confirmationBtn.on("click", function (e) {
+            e.preventDefault();
+            $(this).removeClass("invalid").removeClass("shake");
+            $(".tfa_confirmation_input_digit").removeClass("invalid");
+            var code = getInputCode();
+
+            if (!code) {
+                tFaToast("ERRYou need to enter the verification code");
+                var tm = setTimeout(function () {
+                    Hm_Notices.hide(true);
+                    clearTimeout(tm);
+                }, 2000);
+                return;
+            }
+
+            $(this).text("Processing").addClass("loading");
+            Hm_Ajax.request(
+                [
+                    { name: "hm_ajax_hook", value: "ajax_2fa_setup_check" },
+                    { name: "2fa_code", value: code },
+                ],
+                function (response) {
+                    if (response && response.ajax_2fa_verified) {
+                        verified = true;
+                        formInput.prop("checked", true);
+                        confirmationBtn.addClass("valid");
+                        tFaToast("2 factor authentication enabled");
+                    } else {
+                        verified = false;
+                        formInput.prop("checked", false);
+                        $(".tfa_confirmation_input_digit").addClass("invalid");
+                        confirmationBtn.addClass("invalid").addClass("shake");
+                        tFaToast("ERR2 factor authentication code does not match");
+                    }
+                    confirmationBtn.text("Verify code").removeClass("loading");
+                }
+            );
+        });
+    }
+
+    tFaInit();
+});
