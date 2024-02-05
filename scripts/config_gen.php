361a846103bc50481bc6b34857bac4cc2c8268ca
Steven Ngesera <muhngesteven@gmail.com>
2024-02-05 20:01:51 +0200

    Add SortableJs to  in config_gen to combine_includes

--- scripts/config_gen.php
+++ scripts/config_gen.php
@@ -252,6 +252,7 @@ function combine_includes($js, $js_compress, $css, $css_compress, $settings) {
         $js_lib .= file_get_contents("third_party/resumable.min.js");
         $js_lib .= file_get_contents("third_party/ays-beforeunload-shim.js");
         $js_lib .= file_get_contents("third_party/jquery.are-you-sure.js");
+        $js_lib .= file_get_contents("third_party/sortable.min.js");
         file_put_contents('tmp.js', $js);
         $js_out = $js_lib.compress($js, $js_compress, 'tmp.js');
         $js_hash = build_integrity_hash($js_out);
