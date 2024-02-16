48d5abef118dff67c24d98931c7bdc54a62da54f
Victor Emanouilov <kroky6@gmail.com>
2024-02-16 10:16:06 +0200

    make local contacts use repository pattern and integrate with remote contacts

--- lib/repository.php
+++ lib/repository.php
@@ -7,14 +7,18 @@ trait Hm_Repository {
     protected static $session;
     protected static $entities;
 
-    protected static function initRepo($name, $user_config, $session, &$entities) {
+    protected static function initRepo($name, $user_config, $session, &$entities, callable $init = null) {
         self::$name = $name;
         self::$user_config = $user_config;
         self::$session = $session;
         self::$entities = &$entities;
         $initial = self::$user_config->get(self::$name, []);
-        foreach ($initial as $entity) {
-            self::add($entity, false);
+        if ($init) {
+            $init($initial);
+        } else {
+            foreach ($initial as $entity) {
+                self::add($entity, false);
+            }
         }
     }
 
@@ -28,14 +32,24 @@ trait Hm_Repository {
     }
 
     public static function add($entity, $save = true) {
-        if (! array_key_exists('id', $entity)) {
-            $entity['id'] = self::generateId();
+        if (is_array($entity)) {
+            if (! array_key_exists('id', $entity)) {
+                $entity['id'] = self::generateId();
+            }
+            $id = $entity['id'];
+        } elseif (method_exists($entity, 'value')) {
+            if (! $entity->value('id')) {
+                $entity->update('id', self::generateId());
+            }
+            $id = $entity->value('id');
+        } else {
+            throw new Exception('Unrecognized entity found in the repository.');
         }
-        self::$entities[$entity['id']] = $entity;
+        self::$entities[$id] = $entity;
         if ($save) {
             self::save();
         }
-        return $entity['id'];
+        return $id;
     }
 
     public static function edit($id, $entity) {
