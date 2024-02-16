d708645eaca96e9715aa938a621c31c30511611e
Victor Emanouilov <kroky6@gmail.com>
2024-02-16 10:48:09 +0200

    repository IDs migration script

--- lib/repository.php
+++ lib/repository.php
@@ -12,6 +12,7 @@ trait Hm_Repository {
         self::$user_config = $user_config;
         self::$session = $session;
         self::$entities = &$entities;
+        self::migrateFromIntegerIds();
         $initial = self::$user_config->get(self::$name, []);
         if ($init) {
             $init($initial);
@@ -84,5 +85,63 @@ trait Hm_Repository {
 
     public static function count() {
         return count(self::$entities);
+    }
+
+    protected static function migrateFromIntegerIds() {
+        $config = self::$user_config->dump();
+        $replacements = [
+            'imap' => [],
+            'smtp' => [],
+        ];
+        $changed = false;
+        if (! empty($config['imap_servers'])) {
+            $replacements['imap'] = self::replaceIntegerIds($config['imap_servers']);
+            $changed = $changed || ! empty($replacements['imap']);
+        }
+        if (! empty($config['smtp_servers'])) {
+            $replacements['smtp'] = self::replaceIntegerIds($config['smtp_servers']);
+            $changed = $changed || ! empty($replacements['smtp']);
+        }
+        if (! empty($config['feeds'])) {
+            $result = self::replaceIntegerIds($config['feeds']);
+            $changed = $changed || ! empty($result);
+        }
+        if (! empty($config['profiles'])) {
+            $result = self::replaceIntegerIds($config['profiles']);
+            $changed = $changed || ! empty($result);
+            foreach ($config['profiles'] as $id => $profile) {
+                if (isset($profile['smtp_id']) && is_numeric($profile['smtp_id']) && isset($replacements['smtp'][$profile['smtp_id']])) {
+                    $config['profiles'][$id]['smtp_id'] = $replacements['smtp'][$profile['smtp_id']];
+                    $changed = true;
+                }
+            }
+        }
+        if (! empty($config['special_imap_folders'])) {
+            foreach ($config['special_imap_folders'] as $id => $special) {
+                if (is_numeric($id) && isset($replacements['imap'][$id])) {
+                    $config['special_imap_folders'][$replacements['imap'][$id]] = $special;
+                    unset($config['special_imap_folders'][$id]);
+                    $changed = true;
+                }
+            }
+        }
+        if ($changed) {
+            self::$user_config->reload($config, self::$session->get('username'));
+            self::$session->set('user_data', self::$user_config->dump());
+        }
+    }
+
+    protected static function replaceIntegerIds(&$list) {
+        $replacements = [];
+        foreach ($list as $id => $server) {
+            if (is_numeric($id)) {
+                $new_id = self::generateId();
+                $server['id'] = $new_id;
+                $list[$new_id] = $server;
+                unset($list[$id]);
+                $replacements[$id] = $new_id;
+            }
+        }
+        return $replacements;
     }
 }
