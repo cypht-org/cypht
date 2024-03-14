5d3bc7ade253bd7c3f70374d04517cb4e7f87f17
Josaphat Imani <josaphat.imani97@gmail.com>
2024-03-14 07:49:20 +0200

    Added license to are you sure files

--- third_party/jquery.are-you-sure.js
+++ third_party/jquery.are-you-sure.js
@@ -1,3 +1,15 @@
+/*!
+ * jQuery Plugin: Are-You-Sure (Dirty Form Detection)
+ * https://github.com/codedance/jquery.AreYouSure/
+ *
+ * Copyright (c) 2012-2014, Chris Dance and PaperCut Software http://www.papercut.com/
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ *
+ * Author:  chris.dance@papercut.com
+ * Version: 1.9.0
+ * Date:    13th August 2014
+ */
 $.fn.areYouSure = function(options) {
       
   var settings = $.extend(
