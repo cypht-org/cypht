f8165bfd8b6c3db8a30e455f1dbfba3e4d6fe468
Steven Ngesera <muhngesteven@gmail.com>
2024-05-06 18:58:00 +0300

    add try to navigate_msg_test in case we don't have email_folders

--- tests/selenium/inline_msg.py
+++ tests/selenium/inline_msg.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 from base import WebTest, USER, PASS
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from runner import test_runner
 from settings import SettingsHelpers
 
@@ -16,18 +17,22 @@ class InlineMsgTests(SettingsHelpers):
         self.checkbox_test('general_setting', 'inline_message', False, 'inline_message')
         self.dropdown_test('email_setting', 'all_email_since', '-1 week', '-5 years')
 
-    # def navigate_msg_test(self):
-        # self.by_css('[data-source=".email_folders"]').click()
-        # allmsgs = self.by_class('menu_email')
-        # allmsgs.find_element(By.TAG_NAME, 'a').click()
-        # self.wait_on_class('checkbox_cell')
-        # body = self.by_class('message_table_body')
-        # subject = body.find_element(By.CLASS_NAME, 'subject')
-        # subject.find_element(By.TAG_NAME, 'a').click()
-        # self.wait_on_class('header_subject')
-        # detail_subject = self.by_class('header_subject')
-        # header = detail_subject.find_element(By.TAG_NAME, 'th')
-        # assert header.text.startswith('recent')
+    def navigate_msg_test(self):
+        try:
+            self.by_css('[data-source=".email_folders"]').click()
+        except NoSuchElementException:
+            pass
+        else:
+            allmsgs = self.by_class('menu_email')
+            allmsgs.find_element(By.TAG_NAME, 'a').click()
+            self.wait_on_class('checkbox_cell')
+            body = self.by_class('message_table_body')
+            subject = body.find_element(By.CLASS_NAME, 'subject')
+            subject.find_element(By.TAG_NAME, 'a').click()
+            self.wait_on_class('header_subject')
+            detail_subject = self.by_class('header_subject')
+            header = detail_subject.find_element(By.TAG_NAME, 'th')
+            assert header.text.startswith('recent')
 
 
 if __name__ == '__main__':
@@ -36,6 +41,6 @@ if __name__ == '__main__':
     test_runner(InlineMsgTests, [
 
         'set_inline_message_test',
-        # 'navigate_msg_test',
+        'navigate_msg_test',
         'logout'
     ])
