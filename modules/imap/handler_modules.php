eac34f6b61806d63d696682e0f55d0b3c3656f1f
Josaphat Imani <josaphat.imani97@gmail.com>
2024-06-21 11:21:23 +0200

    Fixed undefined variable when no IMAP auth

--- modules/imap/handler_modules.php
+++ modules/imap/handler_modules.php
@@ -1564,12 +1564,12 @@ class Hm_Handler_load_imap_servers_from_config extends Hm_Handler_Module {
             if (! empty($auth_server['sieve_config_host'])) {
                 $imap_details['sieve_config_host'] = $auth_server['sieve_config_host'];
             }
-        }
-        if (!$default_server_id) {
-            Hm_IMAP_List::add($imap_details);
-        } else {
-            // Perhaps something as changed
-            Hm_IMAP_List::edit($default_server_id, $imap_details);
+            if (!$default_server_id) {
+                Hm_IMAP_List::add($imap_details);
+            } else {
+                // Perhaps something as changed
+                Hm_IMAP_List::edit($default_server_id, $imap_details);
+            }
         }
     }
 }
