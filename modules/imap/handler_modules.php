b4a721d6941ebfbf4d9a13eff492c3a533e59d12
christer kahasha <62720246+christer77@users.noreply.github.com>
2024-06-30 16:57:32 +0200

    [FIX]Displaying multiple messages with the same content for a given action

--- modules/imap/handler_modules.php
+++ modules/imap/handler_modules.php
@@ -280,9 +280,6 @@ class Hm_Handler_imap_process_move extends Hm_Handler_Module {
             elseif (count($moved) == 0) {
                 Hm_Msgs::add('ERRUnable to move/copy selected messages');
             }
-            if ($form['imap_move_action'] == 'move' && $form['imap_move_page'] == 'message') {
-                $this->save_hm_msgs();
-            }
             $this->out('move_count', $moved);
         }
     }
@@ -875,7 +872,6 @@ class Hm_Handler_imap_delete_message extends Hm_Handler_Module {
                 Hm_Msgs::add('Message deleted');
                 $this->out('imap_delete_error', false);
             }
-            $this->save_hm_msgs();
         }
     }
 }
@@ -940,7 +936,6 @@ class Hm_Handler_imap_archive_message extends Hm_Handler_Module {
                 Hm_Msgs::add('ERRAn error occurred archiving the message');
             }
         }
-        $this->save_hm_msgs();
     }
 }
 
@@ -1020,7 +1015,6 @@ class Hm_Handler_imap_snooze_message extends Hm_Handler_Module {
             $msg = 'ERRFailed to snooze selected messages';
         }
         Hm_Msgs::add($msg);
-        $this->save_hm_msgs();
     }
 }
 
