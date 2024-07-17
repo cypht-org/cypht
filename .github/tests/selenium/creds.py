3ef02bdf213f6c0fd9a7d5e4f595732b9028fd8b
Josaphat Imani <josaphat.imani97@gmail.com>
2024-07-17 08:08:49 +0200

    Fix pages.py tests

--- .github/tests/selenium/creds.py
+++ .github/tests/selenium/creds.py
@@ -8,6 +8,7 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.BinaryLocation = "/usr/bin/google-chrome"
+chrome_options.add_argument("--window-size=2200,2800")
 
 RECIP='testuser@localhost.org'
 IMAP_ID='0'
--- tests/selenium/runall.sh
+++ tests/selenium/runall.sh
@@ -5,7 +5,8 @@ rm -rf __pycache__/
 
 #for suite in login.py folder_list.py pages.py profiles.py settings.py servers.py send.py inline_msg.py search.py keyboard_shortcuts.py
 #for suite in login.py folder_list.py pages.py profiles.py settings.py servers.py send.py inline_msg.py search.py
-for suite in login.py folder_list.py pages.py servers.py settings.py send.py inline_msg.py search.py
+# for suite in login.py folder_list.py pages.py servers.py settings.py send.py inline_msg.py search.py
+for suite in pages.py servers.py settings.py send.py inline_msg.py search.py
 do
     export TEST_SUITE="$suite"
     "$PYTHON" -u ./$suite
