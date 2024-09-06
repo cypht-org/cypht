2e34d01f0e27c0d26938d8766a8222ad07568620
indridieinarsson <indridi.einarsson@gmail.com>
2024-09-06 13:21:42 +0000

    mess with tests

--- tests/selenium/folder_list.py
+++ tests/selenium/folder_list.py
@@ -22,12 +22,12 @@ class FolderListTests(WebTest):
         self.safari_workaround(3)
         #self.wait.until.staleness_of(main_menu)
         #self.wait.until(ExpectedConditions.refreshed(main_menu));
-        # WebDriverWait(self.driver, 20).until(EC.stalenessOf(main_menu))
-        WebDriverWait(self.driver, 20).until(EC.refreshed(main_menu))
-        #ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,)
-        #main_menu = WebDriverWait(self.driver, 10,ignored_exceptions=ignored_exceptions).until(
-        #EC.presence_of_element_located((By.CLASS_NAME, 'main_menu'))
-        #)
+        WebDriverWait(self.driver, 20).until(EC.staleness_of(main_menu))
+        # WebDriverWait(self.driver, 20).until(EC.refreshed(main_menu))
+        ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,)
+        main_menu = WebDriverWait(self.driver, 10,ignored_exceptions=ignored_exceptions).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'main_menu'))
+        )
         main_menu = self.by_class('main_menu')
         assert main_menu.text.startswith('Main')
 
