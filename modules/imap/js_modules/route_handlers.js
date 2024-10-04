ef9876377c9706d78ad639cf62a8a60ecb399a24
Merci Jacob <mercihabam@gmail.com>
2024-10-04 15:46:25 +0200

    Fix refresh links

--- modules/imap/js_modules/route_handlers.js
+++ modules/imap/js_modules/route_handlers.js
@@ -1,6 +1,8 @@
 function applyImapMessageListPageHandlers(routeParams) {
     setup_imap_folder_page(routeParams.list_path); // Imap is OK, but other types don't work with this handler yet
     // TODO: - Support folders from other sources (Combined, Feeds, and JMAP as well maybe.)
+
+    $('.refresh_link').on("click", function() { return Hm_Message_List.load_sources(); });
 }
 
 function applyImapMessageContentPageHandlers(routeParams) {
