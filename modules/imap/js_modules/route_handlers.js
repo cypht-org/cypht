d1f050db628eb63e204475cb7bd21adac0f855ab
Merci Jacob <mercihabam@gmail.com>
2024-10-08 15:35:27 +0200

    Add a process to refresh the active message list in the background

--- modules/imap/js_modules/route_handlers.js
+++ modules/imap/js_modules/route_handlers.js
@@ -1,6 +1,9 @@
 function applyImapMessageListPageHandlers(routeParams) {
-    setup_imap_folder_page(routeParams.list_path); // Imap is OK, but other types don't work with this handler yet
-    // TODO: - Support folders from other sources (Combined, Feeds, and JMAP as well maybe.)
+    const refreshInterval = setup_imap_folder_page(routeParams.list_path);
+    return async function() {
+        const refreshIntervalId = await refreshInterval;
+        clearInterval(refreshIntervalId);
+    }
 }
 
 function applyImapMessageContentPageHandlers(routeParams) {
