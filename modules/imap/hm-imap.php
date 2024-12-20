93e6b928ba41dfed1b4dd9281e8e94b3cc1535d6
christer kahasha <62720246+christer77@users.noreply.github.com>
2024-12-20 15:21:32 +0100

    fix(other): gmail messages not shown

--- modules/imap/hm-imap.php
+++ modules/imap/hm-imap.php
@@ -6,6 +6,8 @@
  * @subpackage imap
  */
 
+use ZBateson\MailMimeParser\MailMimeParser;
+
 require_once('hm-imap-base.php');
 require_once('hm-imap-parser.php');
 require_once('hm-imap-cache.php');
@@ -901,7 +903,7 @@ if (!class_exists('Hm_IMAP')) {
             }
             $command .= "BODY.PEEK[HEADER.FIELDS (SUBJECT X-AUTO-BCC FROM DATE CONTENT-TYPE X-PRIORITY TO LIST-ARCHIVE REFERENCES MESSAGE-ID X-SNOOZED)]";
             if ($include_content_body) {
-                $command .= " BODY.PEEK[0.1]";
+                $command .= " BODY.PEEK[TEXT]<0.500>";
             }
             $command .= ")\r\n";
             $cache_command = $command.(string)$raw;
@@ -938,9 +940,11 @@ if (!class_exists('Hm_IMAP')) {
                     $x_auto_bcc = '';
                     $x_snoozed = '';
                     $count = count($vals);
+                    $header_founded = false;
+                    $body_founded = false;
                     for ($i=0;$i<$count;$i++) {
-                        if ($vals[$i] == 'BODY[HEADER.FIELDS') {
-                                
+                        if ($vals[$i] == 'BODY[HEADER.FIELDS' && !$header_founded) {
+                            $header_founded = true;
                             $i++;
                             while(isset($vals[$i]) && in_array(mb_strtoupper($vals[$i]), $junk)) {
                                 $i++;
@@ -958,7 +962,8 @@ if (!class_exists('Hm_IMAP')) {
                                 }
                             }
                         }
-                        elseif ($vals[$i] == 'BODY[0.1') {
+                        elseif ($vals[$i] == 'BODY[TEXT' && !$body_founded) {
+                            $body_founded = true;
                             $content = '';
                             $i++;
                             $i++;
@@ -967,7 +972,18 @@ if (!class_exists('Hm_IMAP')) {
                                 $i++;
                             }
                             $i++;
+                            if (! empty($content)) {
+                                if (substr($content, 0, 3) == "<0>") {
+                                    $content = substr($content, 3);
+                                }
+                                $parser = new MailMimeParser();
+                                $str_parser = $parser->parse($content, false);
+                                $content = $str_parser->getTextContent();
+                            }
                             $flds['body'] = $content;
+                            if (!$header_founded) {
+                                $i = 0;
+                            }
                         }
                         elseif (isset($tags[mb_strtoupper($vals[$i])])) {
                             if (isset($vals[($i + 1)])) {
@@ -986,7 +1002,6 @@ if (!class_exists('Hm_IMAP')) {
                             }
                         }
                     }
-
                     if ($uid) {
                         $cset = '';
                         if (mb_stristr($content_type, 'charset=')) {
