1b0101e9e2d85d8df5c92fea60cca58e08b51673
Indriði Einarsson <indridi@indridi.net>
2025-02-20 23:47:59 +0000

    revert first attempt. Ensure type property is saved to config on account create

--- modules/nux/modules.php
+++ modules/nux/modules.php
@@ -122,7 +122,8 @@ class Hm_Handler_process_oauth2_authorization extends Hm_Handler_Module {
                         'pass' => $result['access_token'],
                         'expiration' => strtotime(sprintf("+%d seconds", $result['expires_in'])),
                         'refresh_token' => $result['refresh_token'],
-                        'auth' => 'xoauth2'
+			'auth' => 'xoauth2',
+			'type' => $details['type']
                     ));
                     if (isset($details['smtp'])) {
                         Hm_SMTP_List::add(array(
@@ -134,7 +135,8 @@ class Hm_Handler_process_oauth2_authorization extends Hm_Handler_Module {
                             'user' => $details['email'],
                             'pass' => $result['access_token'],
                             'expiration' => strtotime(sprintf("+%d seconds", $result['expires_in'])),
-                            'refresh_token' => $result['refresh_token']
+			    'refresh_token' => $result['refresh_token'],
+			    'type' => 'smtp'
                         ));
                         $this->session->record_unsaved('SMTP server added');
                     }
@@ -182,6 +184,7 @@ class Hm_Handler_process_nux_add_service extends Hm_Handler_Module {
                     'tls' => $details['tls'],
                     'user' => $form['nux_email'],
                     'pass' => $form['nux_pass'],
+		    'type' => $details['type']
                 );
                 if ($details['sieve'] && $this->module_is_supported('sievefilters') && $this->user_config->get('enable_sieve_filter_setting', DEFAULT_ENABLE_SIEVE_FILTER)) {
                     $imap_list['sieve_config_host'] = $details['sieve']['host'].':'.$details['sieve']['port'];
@@ -200,7 +203,8 @@ class Hm_Handler_process_nux_add_service extends Hm_Handler_Module {
                             'port' => $details['smtp']['port'],
                             'tls' => $details['smtp']['tls'],
                             'user' => $form['nux_email'],
-                            'pass' => $form['nux_pass']
+			    'pass' => $form['nux_pass'],
+			    'type' => 'smtp'
                         ));
                         if (can_save_last_added_server('Hm_SMTP_List', $form['nux_email'])) {
                             $this->session->record_unsaved('SMTP server added');
@@ -386,6 +390,7 @@ class Hm_Handler_process_import_accouts_servers extends Hm_Handler_Module
                             $server['username'],
                             $server['password'],
                             $server['smtp']['tls'],
+			    $server['type'],
                             false
                         );
                         if (! $smtp_server_id) {
