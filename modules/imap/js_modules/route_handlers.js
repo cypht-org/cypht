77a095006f649c24cdbee9ef9b1d9ff2845f0b8d
Merci Jacob <mercihabam@gmail.com>
2025-06-05 12:36:53 +0200

    refactor(frontend): emit a 'message-loaded' event when the message view page setup is complete

--- modules/imap/js_modules/route_handlers.js
+++ modules/imap/js_modules/route_handlers.js
@@ -20,6 +20,7 @@ function applyImapMessageContentPageHandlers(routeParams) {
     imap_setup_message_view_page(routeParams.uid, null, routeParams.list_path, routeParams.list_parent, () => {
         imap_setup_tags();
         imap_setup_snooze();
+        window.dispatchEvent(new CustomEvent('message-loaded'));
     });
 
     const messages = new Hm_MessagesStore(routeParams.list_path, routeParams.list_page, `${routeParams.keyword}_${routeParams.filter}`);
