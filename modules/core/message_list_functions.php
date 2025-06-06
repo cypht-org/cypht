41e99f7c0fea3ea080babb2837980c49dbb37efa
Steven Ngesera <muhngesteven@gmail.com>
2025-06-07 00:50:54 +0300

    feat(backend): add utility to skip module execution based on user config

--- modules/core/message_list_functions.php
+++ modules/core/message_list_functions.php
@@ -122,9 +122,9 @@ function message_list_meta($input, $output_mod) {
     $date = sprintf('%s', mb_strtolower($output_mod->trans($times[$since])));
     $max = sprintf($output_mod->trans('sources@%d each'), $limit);
 
-    return '<div class="list_meta fs-6">'.
+    return '<div class="list_meta d-flex align-items-center fs-6">'.
         $date.
-        '<b> -</b>'.
+        '<b>-</b>'.
         '<span class="src_count"></span> '.$max.
         '<b>-</b>'.
         '<span class="total"></span> '.$output_mod->trans('total').'</div>';
