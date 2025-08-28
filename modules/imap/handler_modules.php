3a02c1ee79a71def86e26ad1554b002aba1b9187
Steven Ngesera <muhngesteven@gmail.com>
2025-08-29 00:10:16 +0300

    fix(frontend): add success message on ews server connect

--- modules/imap/handler_modules.php
+++ modules/imap/handler_modules.php
@@ -1603,6 +1603,7 @@ class Hm_Handler_save_ews_server extends Hm_Handler_Module {
                 ];
                 $this->user_config->set('special_imap_folders', $specials);
             }
+            Hm_Msgs::add("EWS server saved. To preserve these settings after logout, please go to <a class='alert-link' href='/?page=save'>Save Settings</a>.");
             $this->session->record_unsaved('EWS server added');
             $this->session->secure_cookie($this->request, 'hm_reload_folders', '1');
         }
