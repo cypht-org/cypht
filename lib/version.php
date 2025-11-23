b2bf1c75af4a60a161be15a780dee5842d1111bb
Merci Jacob <mercihabam@gmail.com>
2025-11-23 17:10:16 +0200

    fetch releases list from remote file

--- lib/version.php
+++ lib/version.php
@@ -2,8 +2,7 @@
 
 define('CYPHT_VERSION', '2.5.0');
 
-// $releases = json_decode(file_get_contents('https://github.com/cypht-org/cypht/blob/master/releases.json?raw=true'), true);
-$releases = json_decode(file_get_contents(APP_PATH.'releases.json'), true); // TODO: This should be replaced by the line above in production
+$releases = json_decode(file_get_contents('https://github.com/cypht-org/cypht/blob/master/releases.json?raw=true'), true);
 $latestRelease = end($releases['release']);
 
 if (version_compare(CYPHT_VERSION, $latestRelease['version'], '<')) {
