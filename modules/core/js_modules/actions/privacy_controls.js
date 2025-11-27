728d23f66c3f444f86c10a42eaabfbe2c801bd4d
Merci Jacob <mercihabam@gmail.com>
2025-11-27 20:31:01 +0200

    enh(frontend/backend): improve the external resources alert UI, and make it possible to block resources from a sender

--- modules/core/js_modules/actions/privacy_controls.js
+++ modules/core/js_modules/actions/privacy_controls.js
@@ -13,3 +13,19 @@ async function addSenderToImagesWhitelist(email) {
         });
     });
 }
+
+async function addSenderToImagesBlackList(email) {
+    return new Promise((resolve, reject) => {
+        Hm_Ajax.request([
+            { name: "hm_ajax_hook", value: "ajax_privacy_settings" },
+            { name: "images_blacklist", value: email },
+            { name: "save_settings", value: true },
+            { name: "update", value: true }
+        ], (response) => {
+            resolve(response);
+        }, [], false, undefined, () => {
+            Hm_Notices.show('An error occurred while adding the sender to the blacklist', 'danger');
+            reject();
+        });
+    });
+}
