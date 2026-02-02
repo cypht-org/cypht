643c71705f02d1bc7a2f9c5bd3883952501c1f5d
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2026-02-02 15:21:35 +0200

    feat(spam_reporting): add initial implementation of spam reporting module with core functions and adapters

---
+++ modules/spam_reporting/setup.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('DEBUG_MODE')) { die(); }
+
+handler_source('spam_reporting');
+output_source('spam_reporting');
+
+return array(
+    'allowed_pages' => array(
+    ),
+    'allowed_output' => array(
+    ),
+    'allowed_post' => array(
+    ),
+    'allowed_get' => array(
+    )
+);
