c6998f923cbf04365e3f31e48f6bb772ae7781ce
Merci Jacob <mercihabam@gmail.com>
2026-04-30 16:43:47 +0200

    avoid launching core message action on rescheduling

--- modules/core/functions.php
+++ modules/core/functions.php
@@ -850,7 +850,7 @@ function schedule_dropdown($output, $send_now = false, $list_control = false) {
     $txt = '';
     if ($send_now) {
         $txt .= '<div class="dropdown d-inline-block">
-                <a class="hlink text-decoration-none dropdown-toggle' . ($list_control ? ' core_msg_control border btn btn-sm btn-light': ''). '" id="dropdownMenuNexterDate" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">'.$output->trans('Reschedule').'</a>';
+                <a class="hlink text-decoration-none dropdown-toggle' . ($list_control ? ' border btn btn-sm btn-light': ''). '" id="dropdownMenuNexterDate" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">'.$output->trans('Reschedule').'</a>';
     }
     $txt .= '<ul class="dropdown-menu nexter_dropdown schedule_dropdown" aria-labelledby="dropdownMenuNexterDate">';
     foreach ($values as $format) {
