becb58a8efb74ff5fd2ca73cc97717c72c5a2d02
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2026-05-16 12:49:27 +0200

    Add tests for the flags junk

--- tests/phpunit/modules/imap/HandlerImapNotJunkTest.php
+++ tests/phpunit/modules/imap/HandlerImapNotJunkTest.php
@@ -6,7 +6,24 @@ class Hm_Test_Mock_Mailbox_NotJunk {
     /** @var array<int, array{0: string, 1: string, 2: array, 3: string|false}> */
     public $moves = array();
 
+    /** @var array<int, array{0: string, 1: string, 2: array}> */
+    public $not_junk_flags = array();
+
+    /** @var bool */
+    public $imap = true;
+
+    /** @var bool */
+    public $not_junk_succeeds = true;
+
+    public function is_imap() {
+        return $this->imap;
+    }
+
     public function message_action($folder, $action, $uids, $dest = false) {
+        if ($action === 'NOT_JUNK') {
+            $this->not_junk_flags[] = array($folder, $action, $uids);
+            return array('status' => $this->not_junk_succeeds, 'responses' => array());
+        }
         if ($action === 'MOVE') {
             $this->moves[] = array($folder, $action, $uids, $dest);
             return array('status' => true, 'responses' => array());
@@ -93,6 +110,10 @@ class HandlerImapNotJunkTest extends TestCase {
         $server_details = array('id' => 'srv', 'name' => 'Test');
         $result = $pm->invoke($handler, $mb, 'not_junk', $uids, $junk_hex, $specials, $server_details);
         $this->assertFalse($result['error']);
+        $this->assertCount(1, $mb->not_junk_flags);
+        $this->assertSame('Spam', $mb->not_junk_flags[0][0]);
+        $this->assertSame('NOT_JUNK', $mb->not_junk_flags[0][1]);
+        $this->assertSame(array('42'), $mb->not_junk_flags[0][2]);
         $this->assertCount(1, $mb->moves);
         $this->assertSame('Spam', $mb->moves[0][0]);
         $this->assertSame('MOVE', $mb->moves[0][1]);
@@ -116,7 +137,72 @@ class HandlerImapNotJunkTest extends TestCase {
         $server_details = array('id' => 'srv', 'name' => 'Test');
         $result = $pm->invoke($handler, $mb, 'not_junk', array('7'), $junk_hex, $specials, $server_details);
         $this->assertFalse($result['error']);
+        $this->assertCount(1, $mb->not_junk_flags);
         $this->assertSame('Bulk', $mb->moves[0][3]);
+    }
+
+    /**
+     * NOT_JUNK is attempted before MOVE (source-folder UIDs).
+     *
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_perform_action_not_junk_clears_junk_flag_before_move() {
+        $parent = build_parent_mock();
+        $parent->user_config->set('original_folder_setting', false);
+        $handler = new Hm_Handler_imap_message_action($parent, 'home');
+        $pm = new ReflectionMethod($handler, 'perform_action');
+        $pm->setAccessible(true);
+        $mb = new Hm_Test_Mock_Mailbox_NotJunk();
+        $junk_hex = bin2hex('Spam');
+        $server_details = array('id' => 'srv', 'name' => 'Test');
+        $pm->invoke($handler, $mb, 'not_junk', array('9'), $junk_hex, array(), $server_details);
+        $this->assertNotEmpty($mb->not_junk_flags);
+        $this->assertNotEmpty($mb->moves);
+    }
+
+    /**
+     * Fail-soft: MOVE still runs when -FLAGS (\Junk) fails.
+     *
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_perform_action_not_junk_move_succeeds_when_clear_junk_flag_fails() {
+        $parent = build_parent_mock();
+        $parent->user_config->set('original_folder_setting', false);
+        $handler = new Hm_Handler_imap_message_action($parent, 'home');
+        $pm = new ReflectionMethod($handler, 'perform_action');
+        $pm->setAccessible(true);
+        $mb = new Hm_Test_Mock_Mailbox_NotJunk();
+        $mb->not_junk_succeeds = false;
+        $junk_hex = bin2hex('Spam');
+        $server_details = array('id' => 'srv', 'name' => 'Test');
+        $result = $pm->invoke($handler, $mb, 'not_junk', array('3'), $junk_hex, array(), $server_details);
+        $this->assertFalse($result['error']);
+        $this->assertCount(1, $mb->not_junk_flags);
+        $this->assertCount(1, $mb->moves);
+    }
+
+    /**
+     * NOT_JUNK is skipped for non-IMAP mailboxes.
+     *
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function test_perform_action_not_junk_skips_flag_clear_for_non_imap() {
+        $parent = build_parent_mock();
+        $parent->user_config->set('original_folder_setting', false);
+        $handler = new Hm_Handler_imap_message_action($parent, 'home');
+        $pm = new ReflectionMethod($handler, 'perform_action');
+        $pm->setAccessible(true);
+        $mb = new Hm_Test_Mock_Mailbox_NotJunk();
+        $mb->imap = false;
+        $junk_hex = bin2hex('Spam');
+        $server_details = array('id' => 'srv', 'name' => 'Test');
+        $result = $pm->invoke($handler, $mb, 'not_junk', array('5'), $junk_hex, array(), $server_details);
+        $this->assertFalse($result['error']);
+        $this->assertCount(0, $mb->not_junk_flags);
+        $this->assertCount(1, $mb->moves);
     }
 
     /**
