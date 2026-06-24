7e2d804be69eabeadaa022345f0b4922e6ad2e42
Alfred Syatsukwa <44422833+IrAlfred@users.noreply.github.com>
2026-06-24 14:52:46 +0200

    Revert "refactor(tests): remove bootstrap.php inclusion from setUp methods in unit tests"
    This reverts commit 2a8806bd59298f90fbeab1e3fbccad708fa63e92.

--- tests/phpunit/lib/api_curl.php
+++ tests/phpunit/lib/api_curl.php
@@ -10,6 +10,7 @@ use PHPUnit\Framework\TestCase;
 class Hm_Test_API_Curl extends TestCase {
 
     public function setUp(): void {
+        require_once __DIR__.'/../bootstrap.php';
         // Reset Hm_Functions statics to known defaults before each test
         Hm_Functions::$exec_res  = '{"unit":"test"}';
         Hm_Functions::$curl_fail = false;
--- tests/phpunit/lib/searchable.php
+++ tests/phpunit/lib/searchable.php
@@ -18,6 +18,7 @@ class Hm_Test_Searchable extends TestCase {
     ];
 
     public function setUp(): void {
+        require_once __DIR__.'/../bootstrap.php';
         Searchable_Wrapper::load(self::$default_data);
     }
 
